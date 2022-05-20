ec9071d7b099939c4b52f817adbd1163bd2d16db
Johnson Kwok <johnson@mage.ai>
2022-05-20 13:36:04 -0700

    [jk] Add border constants

---
+++ src/frontend/oracle/styles/units/borders.ts
@@ -0,0 +1,11 @@
+import { GRAY_LINES, DARK_GRAY_LINES, NAVY_LINES } from '../colors/main';
+
+export const BORDER_WIDTH: number = 1;
+export const BORDER_STYLE = 'solid';
+
+export const BORDER_RADIUS: number = 8;
+export const BORDER_RADIUS_SMALL: number = 4;
+
+export const BORDER_COLOR = GRAY_LINES;
+export const BORDER_COLOR_DARK = DARK_GRAY_LINES;
+export const BORDER_COLOR_NAVY = NAVY_LINES;
