06e9eab733ba6f432ac909219bef5b98303c7e01
Johnson Kwok <johnson@mage.ai>
2022-05-20 13:24:50 -0700

    [jk] Add colors

---
+++ src/frontend/oracle/styles/colors/main.ts
@@ -0,0 +1,15 @@
+export const WHITE = '#FFFFFF';
+export const LIGHT = '#FBFCFD';
+export const SILVER = '#F9FAFC';
+export const GRAY = '#C9CEDA';
+export const BLACK = '#060606';
+export const BLUE = '#195AFF';
+export const PURPLE = '#7F4FEB';
+export const RED = '#FF0505';
+export const GRAY_LINES = '#D8DCE3';
+export const DARK_GRAY_LINES = '#B9BFCA';
+export const NAVY_LINES = '#003BD0';
+
+// CSS background
+export const PURPLE_GRADIENT = 'linear-gradient(180deg, rgba(127, 79, 235, 0.3) 0%, rgba(127, 79, 235, 0) 100%);';
+export const RED_GRADIENT = 'linear-gradient(180deg, rgba(255, 5, 5, 0.3) 0%, rgba(255, 5, 5, 0) 100%);';
