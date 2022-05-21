7a3892c6e71b26f8197e15222ec45b552fa95ed0
Johnson Kwok <78053898+johnson-mage@users.noreply.github.com>
2022-05-20 17:24:23 -0700

    [jk] Allow builds to complete without certain type errors (#12)

--- mage_ai/frontend/next.config.js
+++ mage_ai/frontend/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
-
-module.exports = nextConfig
