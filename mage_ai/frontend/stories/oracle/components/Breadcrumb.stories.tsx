b2ac7ff38011a39dff0e97b3dfc64be3356c1e01
Nathaniel Tjandra <nathaniel@mage.ai>
2022-05-23 09:45:41 -0700

    [nt] Add storybook testing for breadcrumbs
    default, large, disabled/muted, and selected/bold

---
+++ mage_ai/frontend/stories/oracle/components/Breadcrumb.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import Breadcrumb, { BreadcrumbType } from '@oracle/components/Breadcrumbs/Breadcrumb';
+import ThemeBlock from 'stories/ThemeBlock';
+
+export default {
+  component: Breadcrumb,
+  title: 'Oracle/Components/Breadcrumbs',
+} as Meta;
+
+const TemplateWithTheme = ({ ...props }) => (
+  <ThemeBlock>
+    <Breadcrumb label={'Storybook'} {...props} />
+  </ThemeBlock>
+);
+
+const Template: Story<BreadcrumbType> = (args) => <TemplateWithTheme {...args} />;
+
+export const Regular = Template.bind({});
+Regular.args = {};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Regular.args,
+  disabled: true,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  ...Regular.args,
+  large: true,
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+  ...Regular.args,
+  selected: true,
+};
