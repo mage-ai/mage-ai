d7748a3620cdfd76d267d6330bf22165724b6570
shrey-mage <shrey@mage.ai>
2022-05-23 19:10:52 -0700

    [sp] remove radius.ts

--- mage_ai/frontend/oracle/styles/units/radius.ts
+++
@@ -1,4 +0,0 @@
-export const BORDER_RADIUS: number = 4;
-export const BORDER_RADIUS_SMALL: number = 2;
-export const BORDER_RADIUS_XLARGE: number = 12;
-export const BORDER_RADIUS_XXXLARGE: number = 20;
