7012fe2645a84bdba06edcab831bec2d85524a7a
skunichetty <sachchit@mage.ai>
2022-05-25 16:38:50 -0700

    [sk] Stopped clean columns from invalidating suggestions

--- mage_ai/data_cleaner/pipelines/base.py
+++ mage_ai/data_cleaner/pipelines/base.py
@@ -11,15 +11,16 @@ from data_cleaner.cleaning_rules.remove_columns_with_single_value \
 from data_cleaner.cleaning_rules.remove_duplicate_rows \
     import RemoveDuplicateRows
 from data_cleaner.transformer_actions.base import BaseAction
+from data_cleaner.transformer_actions.constants import ActionType
 
 DEFAULT_RULES = [
     CleanColumnNames,
-    ImputeValues,
     ReformatValues,
+    ImputeValues,
     RemoveCollinearColumns,
     RemoveColumnsWithHighEmptyRate,
     RemoveColumnsWithSingleValue,
-    RemoveDuplicateRows,
+    RemoveDuplicateRows
 ]
 
 
@@ -29,6 +30,8 @@ class BasePipeline():
         self.rules = DEFAULT_RULES
 
     def create_actions(self, df, column_types, statistics):
+        self.column_types = column_types
+        self.statistics = statistics
         all_suggestions = []
         for rule in self.rules:
             suggestions = rule(df, column_types, statistics).evaluate()
@@ -41,7 +44,25 @@ class BasePipeline():
         if len(self.actions) == 0:
             print('Pipeline is empty.')
             return df
-        df_transformed = df
+        if self.actions[0]['action_payload']['action_type'] == ActionType.CLEAN_COLUMN_NAME:
+            action = self.actions[0]
+            df_transformed = BaseAction(action['action_payload']).execute(df)
+            action['status'] = STATUS_COMPLETED
+            new_column_types = {}
+            new_statistics = {}
+            new_statistics['timeseries_index'] = []
+            for old_key, new_key in zip(df.columns, df_transformed.columns):
+                new_column_types[new_key] = self.column_types[old_key]
+                search_term = old_key + '/'
+                for key in filter(lambda x: x.startswith(search_term), self.statistics):
+                    new_statistics[key.replace(old_key, new_key)] = self.statistics[key]
+                if old_key in self.statistics['timeseries_index']:
+                    new_statistics['timeseries_index'].append(new_key)
+            new_statistics['is_timeseries'] = self.statistics['is_timeseries']
+            new_statistics['count'] = self.statistics['count']
+            self.actions = self.create_actions(df_transformed, new_column_types, new_statistics)
+            self.statistics = new_statistics
+            self.column_types = new_column_types
         for action in self.actions:
             df_transformed = BaseAction(action['action_payload']).execute(df_transformed)
             action['status'] = STATUS_COMPLETED
