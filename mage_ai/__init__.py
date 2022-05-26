29a0ea480b72c08425d56fba49dd2218a789fe43
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-05-26 16:35:37 -0700

    [xy] Add max number of rows for cleaning.

--- mage_ai/__init__.py
+++ mage_ai/__init__.py
@@ -1,14 +1,23 @@
 from server.app import clean_df, connect_df, launch as launch_flask
 
+MAX_NUM_OF_ROWS = 100_000
+
+
 def launch():
     launch_flask()
 
+
 def connect_data(df, name):
-    connect_df(df, name)
+    if df.shape[0] > MAX_NUM_OF_ROWS:
+        connect_df(df.sample(MAX_NUM_OF_ROWS), name)
+    else:
+        connect_df(df, name)
+
 
 def clean(df, pipeline_uuid=None):
     _, df_clean = clean_df(df, pipeline_uuid)
     return df_clean
+
 
 def init(api_key):
     # verify api_key with Mage backend
