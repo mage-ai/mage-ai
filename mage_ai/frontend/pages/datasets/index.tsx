151c4c3949170b6c4347e6cf47c38e2846b16856
shrey-mage <shrey@mage.ai>
2022-05-26 20:16:43 -0700

    [sp] add API endpoint for Dashboard view

--- mage_ai/frontend/pages/datasets/index.tsx
+++ mage_ai/frontend/pages/datasets/index.tsx
@@ -1,5 +1,6 @@
 import Router from 'next/router'
 import type { NextPage } from 'next'
+import { useEffect, useMemo, useState } from 'react'
 
 import FlexContainer from '@oracle/components/FlexContainer'
 import Layout from '@oracle/components/Layout'
@@ -10,60 +11,40 @@ import Spacing from '@oracle/elements/Spacing'
 import Tab from '@oracle/components/Tabs/Tab'
 import Tabs from '@oracle/components/Tabs'
 import Text from '@oracle/elements/Text'
+import api from '@api'
 import { File } from '@oracle/icons'
-import { isBadQuality, DATASETS_PAYLOAD } from '@components/utils'
+import { UNIT } from '@oracle/styles/units/spacing'
+import { isBadQuality } from '@components/utils'
 import { pluralize } from '@utils/string'
 
-// TODO replace with API call to backend
-const data = [
-  {
-     "id":"1",
-     "metadata":{
-        "name":"test_data_2",
-        "pipeline_id": 6,
-        "statistics": {
-           "count": 100,
-           "quality": "Good",
-        },
-        "column_types":{
-           "order_id":"category",
-           "price_unit":"category",
-           "price_subtotal_incl":"category",
-           "price_subtotal":"category",
-           "product_id":"category",
-           "attribute_ids":"category",
-           "display_name":"category",
-           "qty":"category",
-           "default_code":"category",
-           "location_id":"category",
-           "create_date":"datetime",
-           "write_date":"datetime",
-           "xStudioFieldK5gaz":"number_with_decimals",
-           "x_studio_UPC_Individual":"number_with_decimals",
-           "lines_id":"text"
-        }
-     }
-  }
-];
-import { UNIT } from '@oracle/styles/units/spacing'
 
-const Dashboard: NextPage = () => (
-  <Layout
-    centerAlign
-    header={<Spacing mt={UNIT} />}
-  >
-    <Tabs defaultKey="datasets" bold large>
-      <Tab key="datasets" label="Datasets">
-        <Spacing pb={3} pt={3}>
-          <RowDataTable
-            headerTitle="datasets"
-            headerDetails={pluralize("dataset", DATASETS_PAYLOAD.length)}
-          >
-            {
-              DATASETS_PAYLOAD.length > 0
+const Dashboard: NextPage = () => {
+  const { data: featureSetsData } = api.feature_sets.list();
+  const featureSetsMemo = useMemo(() => featureSetsData, [
+    featureSetsData,
+  ]);
+
+  const [featureSets, setFeatureSets] = useState([]);
+  useEffect(() => setFeatureSets(featureSetsMemo), [
+    featureSetsMemo,
+  ]);
+
+  return (
+    <Layout
+      centerAlign
+      header={<Spacing mt={UNIT} />}
+    >
+      <Tabs defaultKey="datasets" bold large>
+        <Tab key="datasets" label="Datasets">
+          <Spacing pb={3} pt={3}>
+            <RowDataTable
+              headerTitle="datasets"
+              headerDetails={pluralize("dataset", featureSets?.length)}
+            >
+              {
+              featureSets?.length > 0
                 ?
-                DATASETS_PAYLOAD.map(dataset => {
-
+                featureSets?.map(dataset => {
                   const {
                     id,
                     metadata: {
@@ -71,9 +52,9 @@ const Dashboard: NextPage = () => (
                       name,
                       statistics: {
                         count,
-                        quality
-                      }
-                    }
+                        quality,
+                      },
+                    },
                   } = dataset;
 
                   const num_features = Object.keys(column_types).length;
@@ -112,12 +93,13 @@ const Dashboard: NextPage = () => (
                     No datasets available. Add one to get started.
                   </Text>
                 </Spacing>
-            }
-          </RowDataTable>
-        </Spacing>
-      </Tab>
-    </Tabs>
-  </Layout>
-)
+              }
+            </RowDataTable>
+          </Spacing>
+        </Tab>
+      </Tabs>
+    </Layout>
+  )
+};
 
 export default Dashboard
