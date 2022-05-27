e048aaea1f691f788d2db6b1b4c4c4a464a593b4
Johnson Kwok <johnson@mage.ai>
2022-05-26 18:01:16 -0700

    [jk] Add Input oracle elements

---
+++ mage_ai/frontend/oracle/elements/Inputs/TextInput.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import InputWrapper, { InputWrapperProps, SHARED_INPUT_STYLES } from './InputWrapper';
+
+export type TextInputProps = {} & InputWrapperProps;
+
+const TextInputStyle = styled.input<TextInputProps>`
+  ${SHARED_INPUT_STYLES}
+`;
+
+const TextInput = ({ ...props }: TextInputProps, ref) => {
+  return (
+    <InputWrapper
+      {...props}
+      input={<TextInputStyle {...props} />}
+      ref={ref}
+    />
+  );
+};
+
+export default React.forwardRef(TextInput);
