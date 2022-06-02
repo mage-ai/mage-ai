5dc2b6c56b6fcdc2f52784e7d55677a41d927269
Nathaniel Tjandra <nathaniel@mage.ai>
2022-06-02 15:44:39 -0700

    [nt] Move constants outside of page folder and update import paths

--- mage_ai/frontend/pages/datasets/[slug]/index.tsx
+++ mage_ai/frontend/pages/datasets/[slug]/index.tsx
@@ -28,7 +28,7 @@ import {
   NUMBER_TYPES,
   PERCENTAGE_KEYS,
   STAT_KEYS,
-} from './constants';
+} from '@components/datasets/constants';
 import { UNIT } from '@oracle/styles/units/spacing';
 
 function Data() {
