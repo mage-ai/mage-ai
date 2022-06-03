b08ba814654b19ebde03b16cecbec91881de4b17
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-06-03 11:54:12 -0700

    [xy] Return sample outliers in response.

--- mage_ai/data_cleaner/statistics/calculator.py
+++ mage_ai/data_cleaner/statistics/calculator.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import traceback
 
+OUTLIER_SAMPLE_COUNT = 100
 OUTLIER_ZSCORE_THRESHOLD = 3
 VALUE_COUNT_LIMIT = 20
 
@@ -196,9 +197,9 @@ class StatisticsCalculator:
                     series_z_score = (
                         (series_non_null - data[f'{col}/average']) / data[f'{col}/std']
                     ).abs()
-                    data[f'{col}/outlier_count'] = (
-                        series_z_score >= OUTLIER_ZSCORE_THRESHOLD
-                    ).sum()
+                    series_outliers = series_z_score[series_z_score >= OUTLIER_ZSCORE_THRESHOLD]
+                    data[f'{col}/outlier_count'] = series_outliers.count()
+                    data[f'{col}/outliers'] = series_outliers.iloc[:OUTLIER_SAMPLE_COUNT].tolist()
             elif column_type == DATETIME:
                 dates = pd.to_datetime(series_non_null, utc=True, errors='coerce').dropna()
                 data[f'{col}/max'] = dates.max().isoformat()
