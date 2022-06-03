0c0e5b2bbf2c22065f36cdd0ef4683da89f54d6f
Johnson Kwok <johnson@mage.ai>
2022-06-03 10:46:34 -0700

    [jk] Add initial props to slug page

--- mage_ai/frontend/pages/datasets/[...slug].tsx
+++ mage_ai/frontend/pages/datasets/[...slug].tsx
@@ -1,27 +1,45 @@
-import { useRouter } from 'next/router';
-
 import ApiReloader from '@components/ApiReloader';
 import ColumnDetail from '@components/datasets/columns/ColumnDetail';
 import ColumnList from '@components/datasets/columns/ColumnList';
 import DatasetOverview from '@components/datasets/overview';
+import FeatureSetType from '@interfaces/FeatureSetType';
+import api from '@api';
 
-function DatasetDetail() {
-  const router = useRouter();
-  const { slug = [] } = router.query;
+type DatasetDetailProps = {
+  featureSet: FeatureSetType;
+  slug: string[];
+};
 
+function DatasetDetail({
+  featureSet,
+  slug = [],
+}: DatasetDetailProps) {
   // @ts-ignore
   const [featureSetId, _, featureId] = slug;
 
+  const sharedProps = {
+    featureSet,
+    featureSetId,
+  };
+
   let el;
   if (slug.length === 1) {
-    el = <DatasetOverview slug={featureSetId} />;
+    el = (
+      <DatasetOverview
+        featureSet={featureSet}
+      />
+    );
   } else if (slug.length === 2) {
-    el = <ColumnList featureSetId={featureSetId} />;
+    el = (
+      <ColumnList
+        {...sharedProps}
+      />
+    );
   } else if (slug.length === 3) {
     el = (
       <ColumnDetail
         featureId={featureId}
-        featureSetId={featureSetId}
+        {...sharedProps}
       />
     );
   } else {
@@ -34,5 +52,18 @@ function DatasetDetail() {
     </ApiReloader>
   );
 }
+
+DatasetDetail.getInitialProps = async (ctx: any) => {
+  const { slug: slugArr }: { slug: string[] } = ctx.query;
+  const featureSetId = (slugArr || [])[0];
+  const response = await api.feature_sets.detailAsync(ctx, featureSetId);
+
+  return {
+    featureSet: {
+      ...response,
+    },
+    slug: slugArr,
+  };
+};
 
 export default DatasetDetail;
