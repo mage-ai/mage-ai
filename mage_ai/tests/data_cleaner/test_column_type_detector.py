24c6da80e5f6ada8a270323f4e8e4903f2af1855
skunichetty <sachchit@mage.ai>
2022-06-06 14:03:26 -0700

    [sk] Updated phone number detection

--- mage_ai/tests/data_cleaner/test_column_type_detector.py
+++ mage_ai/tests/data_cleaner/test_column_type_detector.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from mage_ai.data_cleaner import column_type_detector
 from mage_ai.data_cleaner.column_type_detector import (
     CATEGORY,
     CATEGORY_HIGH_CARDINALITY,
@@ -13,24 +14,31 @@ from mage_ai.data_cleaner.column_type_detector import (
     ZIP_CODE,
     get_mismatched_row_count,
     infer_column_types,
-    REGEX_NUMBER
+    REGEX_NUMBER,
 )
 from mage_ai.tests.base_test import TestCase
 import pandas as pd
-
-fake = Faker()
+import numpy as np
 
 
 class ColumnTypeDetectorTests(TestCase):
+    def setUp(self):
+        Faker.seed(42)
+        self.fake = Faker()
+        return super().setUp()
+
     def test_get_mismatched_row_count(self):
-        df = pd.DataFrame([
-            [1, 'abc@xyz.com', '32132'],
-            [2, 'abc2@xyz.com', '12345'],
-            [3, 'test', '1234'],
-            [4, 'abc@test.net', 'abcde'],
-            [5, 'abc12345@', '54321'],
-            [6, 'abcdef@123.com', '56789'],
-        ], columns=['id', 'email', 'zip_code'])
+        df = pd.DataFrame(
+            [
+                [1, 'abc@xyz.com', '32132'],
+                [2, 'abc2@xyz.com', '12345'],
+                [3, 'test', '1234'],
+                [4, 'abc@test.net', 'abcde'],
+                [5, 'abc12345@', '54321'],
+                [6, 'abcdef@123.com', '56789'],
+            ],
+            columns=['id', 'email', 'zip_code'],
+        )
         count1 = get_mismatched_row_count(df['id'], 'number')
         count2 = get_mismatched_row_count(df['email'], 'email')
         count3 = get_mismatched_row_count(df['zip_code'], 'zip_code')
@@ -54,7 +62,7 @@ class ColumnTypeDetectorTests(TestCase):
             '-8907987 CAD',
             '3239458734 元',
             '234,2342,2343.823034234%',
-            '234.3433   %'
+            '234.3433   %',
         ]
         bad_number_patterns = [
             'this is not a number',
@@ -62,14 +70,14 @@ class ColumnTypeDetectorTests(TestCase):
             '0.000.00.000.000',
             '1-111-111-111',
             ',2343234203',
-            'ksjh&A(*2jnkj&#$#$'
+            'ksjh&A(*2jnkj&#$#$',
         ]
 
         for good in good_number_patterns:
             print(good)
             match = REGEX_NUMBER.match(good)
             self.assertIsNotNone(match)
-        
+
         for bad in bad_number_patterns:
             match = REGEX_NUMBER.match(bad)
             self.assertIsNone(match)
@@ -108,7 +116,7 @@ class ColumnTypeDetectorTests(TestCase):
                 'fire@mage.ai',
                 '123-456-7890',
                 'May 4, 2021, 6:35 PM',
-                fake.name(),
+                self.fake.name(),
             ],
             [
                 '1',
@@ -125,7 +133,7 @@ class ColumnTypeDetectorTests(TestCase):
                 'mage@fire.com',
                 '(123) 456-7890',
                 'Feb 17, 2021, 2:57 PM',
-                fake.name(),
+                self.fake.name(),
             ],
             [
                 '1',
@@ -142,7 +150,7 @@ class ColumnTypeDetectorTests(TestCase):
                 'fire_mage@mage.com',
                 '1234567890',
                 'Feb 18, 2021, 2:57 PM',
-                fake.name(),
+                self.fake.name(),
             ],
             [
                 0,
@@ -159,7 +167,7 @@ class ColumnTypeDetectorTests(TestCase):
                 'mage-fire@mage.ai',
                 '1234567',
                 'Feb 19, 2021, 2:57 PM',
-                fake.name(),
+                self.fake.name(),
             ],
             [
                 0,
@@ -176,7 +184,7 @@ class ColumnTypeDetectorTests(TestCase):
                 'fire+1@mage.ai',
                 '(123)456-7890',
                 'Feb 20, 2021, 2:57 PM',
-                fake.name(),
+                self.fake.name(),
             ],
         ]
         date_formats = [
@@ -191,23 +199,25 @@ class ColumnTypeDetectorTests(TestCase):
         ]
 
         for date_format in date_formats:
-            table.append([
-                0,
-                '40.7',
-                'mutant',
-                date_format,
-                fake.text(),
-                4,
-                '$5,000.01',
-                '15.32%',
-                '01234',
-                12345,
-                12,
-                'fire+1@mage.ai',
-                '(123)456-7890',
-                'Feb 18, 2021, 2:57 PM',
-                fake.name(),
-            ])
+            table.append(
+                [
+                    0,
+                    '40.7',
+                    'mutant',
+                    date_format,
+                    self.fake.text(),
+                    4,
+                    '$5,000.01',
+                    '15.32%',
+                    '01234',
+                    12345,
+                    12,
+                    'fire+1@mage.ai',
+                    '(123)456-7890',
+                    'Feb 18, 2021, 2:57 PM',
+                    self.fake.name(),
+                ]
+            )
 
         df = pd.DataFrame(table, columns=columns)
         column_types = infer_column_types(df)
@@ -232,3 +242,60 @@ class ColumnTypeDetectorTests(TestCase):
                 'name': TEXT,
             },
         )
+
+    def test_phone_number_recognition(self):
+        df = pd.DataFrame(
+            dict(
+                phone_num=[self.fake.phone_number() for _ in range(6)],
+                str_int_phone_nums=[
+                    '9999999999',
+                    '12345678909',
+                    '918328328322',
+                    '33459830234',
+                    '8234592345',
+                    '23439433343',
+                ],
+                not_phone_nums=[
+                    '99999',
+                    '12349',
+                    '918328328322',
+                    '33430234',
+                    '8234592345',
+                    '23439433343',
+                ],
+                int_phone_nums=[
+                    9999999999,
+                    12345678909,
+                    918328328322,
+                    33459830234,
+                    8234592345,
+                    23439433343,
+                ],
+                float_phone_nums=[
+                    9999999999,
+                    np.nan,
+                    918328328322.0,
+                    33459830234,
+                    8234592345,
+                    np.nan,
+                ],
+                not_float_phone_nums=[
+                    9999999999.00234233,
+                    np.nan,
+                    918328328322.0,
+                    33459830234.234,
+                    8234592345,
+                    np.nan,
+                ],
+            )
+        )
+        ctypes = infer_column_types(df)
+        expected_ctypes = dict(
+            phone_num='phone_number',
+            str_int_phone_nums='phone_number',
+            not_phone_nums='number',
+            int_phone_nums='phone_number',
+            float_phone_nums='phone_number',
+            not_float_phone_nums='number_with_decimals',
+        )
+        self.assertEquals(ctypes, expected_ctypes)
