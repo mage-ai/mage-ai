3352337a1276ce52e5244ad730caf0c9dac3746d
Shrey Pandya <105667442+shrey-mage@users.noreply.github.com>
2022-06-07 00:44:31 -0700

    [sp] improve feature profiles layout (#215)
    * [sp] change layout of feature profiles
    
    * [sp] fix column header text overflow
    
    * [sp] truncate long column names
    
    * [sp] add percentages to unique, null, invalid
    
    * [sp] add outlier count, skewness, stddev
    
    * [sp] add warnings for stats
    
    * [sp] clean up flexbox usage
    
    * [sp] fix nit
    
    * [sp] add tooltip title to feature links
    
    * [sp] use theme variables & make warnings bold
    
    * [sp] fix styling to use Text props

--- mage_ai/frontend/components/datasets/FeatureProfiles.tsx
+++ mage_ai/frontend/components/datasets/FeatureProfiles.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import NextLink from 'next/link';
 import styled from 'styled-components';
 
-import FeatureType, {
-  ColumnTypeEnum,
-  COLUMN_TYPE_HUMAN_READABLE_MAPPING,
-  COLUMN_TYPE_NUMBERICAL_WITH_DATETIME_LIKE,
-} from '@interfaces/FeatureType';
+import FeatureType from '@interfaces/FeatureType';
 import FeatureSetType from '@interfaces/FeatureSetType';
 import Flex from '@oracle/components/Flex';
-import FlexContainer from '@oracle/components/FlexContainer';
 import Link from '@oracle/elements/Link';
 import Text from '@oracle/elements/Text';
 import { BORDER_RADIUS_LARGE } from '@oracle/styles/units/borders';
@@ -23,11 +18,20 @@ import {
 } from '@oracle/styles/colors/main';
 import { PADDING, UNIT } from '@oracle/styles/units/spacing';
 import { getFeatureIdMapping } from '@utils/models/featureSet';
-import { roundNumber } from '@utils/string';
+import Spacing from '@oracle/elements/Spacing';
+import { formatPercent, roundNumber } from '@utils/string';
+import FlexContainer from '@oracle/components/FlexContainer';
+import light from '@oracle/styles/themes/light';
 
 export const ContainerStyle = styled.div`
   border: 1px solid ${GRAY_LINES};
   border-radius: ${BORDER_RADIUS_LARGE}px;
+`;
+
+export const ColumnProfileStyle = styled.div`
+  background: ${SILVER};
+  border-bottom: 1px solid ${GRAY_LINES};
+  border-right: 1px solid ${GRAY_LINES};
 `;
 
 export const HeaderStyle = styled.div`
@@ -43,7 +47,6 @@ export const FeatureProfileStyle = styled.div`
 `;
 
 export const BodyStyle = styled.div`
-  height: 500px;
   border-bottom-left-radius: ${BORDER_RADIUS_LARGE}px;
   border-bottom-right-radius: ${BORDER_RADIUS_LARGE}px;
   overflow-y: scroll;
@@ -68,6 +71,31 @@ type FeatureProfilesProps = {
   statistics: any,
 };
 
+const entryTypes = [
+  'Type',
+  'Unique',
+  'Mean',
+  'Median',
+  'Mode',
+  'Min',
+  'Max',
+  'Missing',
+  'Invalid',
+  'Outliers',
+  'Skewness',
+  'Std dev',
+];
+
+const percentages = ['Missing', 'Invalid', 'Unique'];
+
+// % thresholds, above which we warn the user
+const warnings = {
+  'Missing': 0,
+  'Invalid': 0,
+  'Outliers': 0,
+  'Unique': 0.9,
+};
+
 function FeatureProfile({
   feature,
   featureSet,
@@ -88,69 +116,33 @@ function FeatureProfile({
   const medianValue = statistics?.[`${uuid}/median`];
   const modeValue = statistics?.[`${uuid}/mode`];
   const numberOfInvalidValues = statistics?.[`${uuid}/invalid_value_count`];
+  const numberOfOutliers = statistics?.[`${uuid}/outlier_count`];
+  const skewness = statistics?.[`${uuid}/skew`];
+  const stddev = statistics?.[`${uuid}/std`];
 
-  let entries = [];
-  
-  if (COLUMN_TYPE_NUMBERICAL_WITH_DATETIME_LIKE.includes(columnType)) {
-    entries = [
-      [
-        'Type',
-        'Count',
-        'Unique',
-        'Missing',
-      ],
-      [
-        COLUMN_TYPE_HUMAN_READABLE_MAPPING[columnType] || columnType,
-        numberOfValues,
-        numberOfUniqueValues,
-        numberOfNullValues,
-      ],
-      [
-        columnType === ColumnTypeEnum.DATETIME ? 'Median' : 'Mean',
-        'Minimum',
-        'Maximum',
-        'Invalid',
-      ],
-      [
-        columnType === ColumnTypeEnum.DATETIME ? medianValue : roundNumber(meanValue, 3),
-        typeof minValue === 'string' ? minValue: roundNumber(minValue, 3),
-        typeof maxValue === 'string' ? maxValue: roundNumber(maxValue, 3),
-        numberOfInvalidValues,
-      ],
-    ];
-  } else {
-    entries = [
-      [
-        'Type',
-        'Count',
-        'Unique',
-      ],
-      [
-        COLUMN_TYPE_HUMAN_READABLE_MAPPING[columnType] || columnType,
-        numberOfValues,
-        numberOfUniqueValues,
-      ],
-      [
-        'Missing',
-        'Mode',
-        'Invalid',
-      ],
-      [
-        numberOfNullValues,
-        modeValue,
-        numberOfInvalidValues,
-      ],
-    ];
-  }
+  const entries = [
+    columnType,
+    numberOfUniqueValues,
+    meanValue,
+    medianValue,
+    modeValue,
+    minValue,
+    maxValue,
+    numberOfNullValues,
+    numberOfInvalidValues,
+    numberOfOutliers,
+    skewness,
+    stddev,
+  ];
 
   const featureSetId = featureSet.id;
   const featureUuid = feature?.uuid;
-  const featureId = getFeatureIdMapping(featureSet)[featureUuid]
+  const featureId = getFeatureIdMapping(featureSet)[featureUuid];
 
   return (
-    <FlexContainer>
-      <Flex flex={1}>
-        <CellStyle>
+    <Flex flexDirection="column">
+      <FeatureProfileStyle>
+        <Spacing p={2}>
           <NextLink
             as={`/datasets/${featureSetId}/features/${featureId}`}
             href="/datasets/[...slug]"
@@ -158,25 +150,40 @@ function FeatureProfile({
             passHref
           >
             <Link inline>
-              <Text backgroundColor={PURPLE_HIGHLIGHT} bold color={PURPLE} monospace>
+              <Text
+                backgroundColor={light.feature.active}
+                bold
+                maxWidth={25 * UNIT}
+                monospace
+                secondary
+                textOverflow
+                title={uuid}
+              >
                 {uuid}
               </Text>
             </Link>
           </NextLink>
-        </CellStyle>
-      </Flex>
-      {entries.map((values, idx) => (
-        <Flex flex={1} flexDirection="column" key={`column-${idx}`}>
-          {values.map((label, idx) => (
-            <CellStyle backgroundColor={idx % 2 === 0 ? WHITE : LIGHT} key={idx}>
-              <Text>
-                {label}
-              </Text>
-            </CellStyle>
-          ))}
-        </Flex>
-      ))}
-    </FlexContainer>
+        </Spacing>
+      </FeatureProfileStyle>
+      {entries.map((label = '-', idx) => {
+        const entry = entryTypes[idx];
+        const val = !isNaN(label) ? roundNumber(label) : label;
+        const shouldWarn = entry in warnings && (val/numberOfValues) > warnings[entry];
+
+        return (
+          <CellStyle backgroundColor={idx % 2 === 0 ? WHITE : LIGHT} key={idx}>
+            <Text
+              bold={shouldWarn}
+              danger={shouldWarn}
+              textOverflow
+            >
+              {val}
+              {percentages.includes(entry) && ` (${formatPercent(label/numberOfValues)})`}
+            </Text>
+          </CellStyle>
+        );
+      })}
+    </Flex>
   );
 }
 
@@ -193,15 +200,27 @@ function FeatureProfiles({
         </Text>
       </HeaderStyle>
       <BodyStyle>
-        {features.map((feature, idx) => (
-          <FeatureProfileStyle key={idx}>
-            <FeatureProfile
-              feature={feature}
-              featureSet={featureSet}
-              statistics={statistics}
-            />
-          </FeatureProfileStyle>
-        ))}
+        <FlexContainer>
+          <ColumnProfileStyle>
+            <Flex flex={1} flexDirection="column" style={{ background: '#F9FAFC' }} >
+              <Spacing mr={1.25 * UNIT} mt={'52px'} />
+              {entryTypes.map((entry, idx) => (
+                <CellStyle key={`${entry}-${idx}`}>
+                  <Text secondary>{entry}</Text>
+                </CellStyle>
+              ))}
+            </Flex>
+          </ColumnProfileStyle>
+          {features.map((feature, idx) => (
+            <FeatureProfileStyle key={`${feature}-${idx}`}>
+              <FeatureProfile
+                feature={feature}
+                featureSet={featureSet}
+                statistics={statistics}
+              />
+            </FeatureProfileStyle>
+          ))}
+        </FlexContainer>
       </BodyStyle>
     </ContainerStyle>
   );
