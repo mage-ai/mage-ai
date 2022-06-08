8a4aab814c7fd54afa19b670325916a7dacc73b5
Nathaniel Tjandra <nathaniel@mage.ai>
2022-06-08 09:18:47 -0700

    [nt] Revert border changes
    this reverts it to what's on master, so it will still have the missing border corner.

--- mage_ai/frontend/oracle/components/Table/index.style.tsx
+++ mage_ai/frontend/oracle/components/Table/index.style.tsx
@@ -11,35 +11,37 @@ export const TableStyle = styled.div<any>`
   overflow-y: auto;
   position: relative;
   width: 100%;
+  z-index: 3;
+  border: collapse;
 
   ${props => `
     background-color: ${(props.theme.background || light.background).page};
-    border: 1px solid ${(props.theme.interative || light.interactive).defaultBorder};
-    border-radius: ${BORDER_RADIUS}px ${BORDER_RADIUS}px ${BORDER_RADIUS}px ${BORDER_RADIUS}px;
   `}
-
   ${props => props.height && `
     height: ${props.height}px;
   `}
+
   ${props => props.flex && `
     flex: 1;
   `}
 `;
 
 export const ColumnHeaderRowStyle = styled.div<any>`
+  border-top-left-radius: ${BORDER_RADIUS}px;
+  border-top-right-radius: ${BORDER_RADIUS}px;
+  position: sticky;
+  top: 0;
   width: 100%;
+  z-index: 2;
 
   ${props => `
     background-color: ${(props.theme.interactive || light.interactive).hoverBackground};
   `}
-
   ${props => !props.noBorder && `
-    border-bottom: 1px solid ${(props.theme.interative || light.interactive).defaultBorder};
+    border: 1px solid ${(props.theme.interative || light.interactive).defaultBorder};
   `}
-
   ${props => props.noBorder && `
-    border-radius: 0px 0px 0px 0px;
-    border: none;
+    border-radius: 0;
   `}
 `;
 
@@ -47,7 +49,6 @@ export const ColumnHeaderCellStyle = styled.div<any>`
   ${props => !props.small && `
     padding: ${UNIT * PADDING_SIZE_UNITS}px;
   `}
-
   ${props => props.small && `
     padding: ${UNIT * 1.5}px;
   `}
@@ -58,11 +59,9 @@ export const RowTitleStyle = styled.div<any>`
     background-color: ${(props.theme.background || light.background).header};
     border: 1px solid ${(props.theme.interative || light.interactive).defaultBorder};
   `}
-
   ${props => !props.small && `
     padding: ${UNIT * PADDING_SIZE_UNITS}px;
   `}
-
   ${props => props.small && `
     padding: ${UNIT * 1.5}px;
   `}
@@ -70,19 +69,22 @@ export const RowTitleStyle = styled.div<any>`
 
 export const RowStyle = styled.div<any>`
   ${transition()}
-
   ${props => `
     background-color: ${(props.theme.background || light.background).page};
     border-top: none;
     border-bottom: none;
   `}
 
-  ${props => !props.noBorder && !props.last && `
-    // border-bottom: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
-    // border-left: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
-    // border-right: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
+  ${props => !props.noBorder && `
+    border-bottom: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
+    border-left: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
+    border-right: 1px solid ${(props.theme.interactive || light.interactive).defaultBorder};
   `}
 
+  ${props => props.finalRow && `
+    border-bottom-left-radius: ${BORDER_RADIUS}px;
+    border-bottom-right-radius: ${BORDER_RADIUS}px;
+  `}
 `;
 
 export const TextStyle = styled.div`
@@ -93,19 +95,16 @@ export const TextStyle = styled.div`
 
 export const RowCellStyle = styled.div<any>`
   width: 100%;
-
+  z-index: 0;
   ${props => !props.first && `
     border-left: 1px solid ${(props.theme.background || light.background).page};
   `}
-
   ${props => !props.small && `
     padding: ${UNIT * PADDING_SIZE_UNITS}px;
   `}
-
   ${props => props.small && `
     padding: ${UNIT * 1.5}px;
   `}
-
   ${props => props.showBackground && `
     background-color: ${(props.theme.background || light.background).row};
   `}
