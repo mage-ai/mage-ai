98f4ecfb3ef6ef29ac571ce9989258adbac0065d
shrey-mage <shrey@mage.ai>
2022-06-09 14:31:40 -0700

    [sp] update colors

--- mage_ai/frontend/oracle/styles/colors/main.ts
+++ mage_ai/frontend/oracle/styles/colors/main.ts
@@ -1,24 +1,24 @@
 export const BLACK = '#060606';
-export const BLUE = '#195AFF';
+export const BLUE = '#2A60FE';
 export const BLUE_HIGHLIGHT = '#86E2FF';
 export const DARK_GRAY_LINES = '#B9BFCA';
-export const GRAY = '#B9BFCA';
+export const GRAY = '#B1B8C3';
 export const GRAY_LINES = '#D8DCE3';
 export const GREEN = '#2FCB52';
 export const LIGHT = '#FBFCFD';
 export const LIME = '#2FCB52';
-export const NAVY = '#0045F3';
+export const NAVY = '#0F4CFF';
 export const PINK = '#FF4FF8';
-export const PURPLE = '#7F4FEB';
-export const PURPLE_ALT = '#F7F4FE';
+export const PURPLE = '#6B50D7';
+export const PURPLE_ALT = 'rgba(247, 244, 254, 0.06)';
 export const PURPLE_DISABLED = 'rgba(201, 206, 218, 0.12)';
-export const PURPLE_HIGHLIGHT = '#FAF8FE';
-export const RED = '#FF0505';
-export const RED_HIGHLIGHT = '#FFF0F0';
+export const PURPLE_HIGHLIGHT = 'rgba(250, 248, 254, 0.14)';
+export const RED = '#FF144D';
+export const RED_HIGHLIGHT = 'rgba(255, 241, 244, 0.06)';
 export const SILVER = '#F9FAFC';
 export const WHITE = '#FFFFFF';
 export const YELLOW = '#FFCC19';
 
 // CSS background
-export const PURPLE_GRADIENT = 'linear-gradient(180deg, rgba(127, 79, 235, 0.3) 0%, rgba(127, 79, 235, 0) 100%);';
-export const RED_GRADIENT = 'linear-gradient(180deg, rgba(255, 5, 5, 0.3) 0%, rgba(255, 5, 5, 0) 100%);';
+export const PURPLE_GRADIENT = 'linear-gradient(180deg, rgba(107, 80, 215, 0.3) 0%, rgba(107, 80, 215, 0.04) 100%)';
+export const RED_GRADIENT = 'linear-gradient(180deg, rgba(255, 20, 77, 0.3) 0%, rgba(255, 20, 77, 0.04) 100%)';
