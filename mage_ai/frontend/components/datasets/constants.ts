40e908087364b67fb0e8f4313eaa5be93869d40d
shrey-mage <shrey@mage.ai>
2022-06-14 13:51:03 -0700

    Revert "[sp] remove old warnings"
    This reverts commit 64a8591eb4829b6761d33e6ebbe77499e620c8be.

--- mage_ai/frontend/components/datasets/constants.ts
+++ mage_ai/frontend/components/datasets/constants.ts
@@ -1,4 +1,5 @@
 export const LARGE_WINDOW_WIDTH = 992;
+import { greaterThan, lessThan } from '@utils/array';
 
 // TODO: Update to use total instead of average
 export const METRICS_KEYS = [
@@ -48,6 +49,33 @@ export const METRICS_SORTED_MAPPING = {
   'total_invalid_value_count': 3,
   'total_null_value_count': 2,
   'validity': 0,
+};
+
+export const WARNINGS = {
+  qualityMetrics: [
+    {
+      compare: lessThan,
+      name: 'Validity',
+      val: 80,
+    },
+    {
+      compare: lessThan,
+      name: 'Completeness',
+      val: 80,
+    },
+    {
+      compare: greaterThan,
+      name: 'Duplicate rows',
+      val: 0,
+    },
+  ],
+  statistics: [
+    {
+      compare: greaterThan,
+      name: 'Empty columns',
+      val: 0,
+    },
+  ],
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
