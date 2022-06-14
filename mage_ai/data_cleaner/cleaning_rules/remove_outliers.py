f4eff1c05badb8ad873b976d51b9d4a76b65367c
skunichetty <sachchit@mage.ai>
2022-06-14 14:10:29 -0700

    [sk] Removed cleaning suggestion

--- mage_ai/data_cleaner/cleaning_rules/remove_outliers.py
+++ mage_ai/data_cleaner/cleaning_rules/remove_outliers.py
@@ -1,13 +1,9 @@
-from pandas import DataFrame
 from mage_ai.data_cleaner.cleaning_rules.base import BaseRule
 from mage_ai.data_cleaner.shared.utils import wrap_column_name
 from mage_ai.data_cleaner.transformer_actions.constants import (
     ActionType,
     Axis,
 )
-from sklearn.decomposition import PCA
-from sklearn.ensemble import IsolationForest
-from sklearn.neighbors import LocalOutlierFactor
 from typing import Union, Dict
 import numpy as np
 
@@ -26,7 +22,6 @@ class RemoveOutliers(BaseRule):
         super().__init__(df, column_types, statistics)
         self.numeric_df, self.numeric_columns = self._filter_numeric_types()
         self.numeric_indices = np.arange(len(self.numeric_df))
-        self.pca_transformer = PCA(n_components=20)
 
     def one_dim_outlier_check(self, column: str) -> Union[Dict, None]:
         """
@@ -56,64 +51,11 @@ class RemoveOutliers(BaseRule):
             )
         return None
 
-    def multi_dim_outlier_check(self, df: DataFrame, ndim: int) -> Union[Dict, None]:
-        """
-        Checks multivariate data for multidimensional outliers. The algorithm followed is as such:
-        1. If the data is more than 20 dimensional, reduce to 20 dimensions using principal component analysis
-        2. If there are 2 or more dimensions but less than or equal to 5 dimensions, use Local Outlier Factor method (LOF) to calculate
-           outliers. This is faster for smaller data size - theoretically, runtime of LOF grows quadratically with input
-        3. Otherwise, use Isolation Forests
-
-        Args:
-            df (DataFrame): Dataframe consisting of the numerical columns to perform outlier checks on
-            ndim (int): Number of dimensions for each example in dataframe
-
-        Returns:
-            Union[Dict, None]: If a suggestion is made, returns the suggestion dictionary. Else returns `None`
-        """
-        data = df.to_numpy()
-        count = data.shape[0]
-        if count <= 1:
-            return None
-        if ndim > 20:
-            data = self.pca_transformer.fit_transform(data)
-        if ndim <= 5:
-            if count < 10:
-                n_neighbors = 2
-            elif count < 500:
-                n_neighbors = count // 10 + 1
-            else:
-                n_neighbors = 20
-            outlier_algorithm = LocalOutlierFactor(n_neighbors=n_neighbors, n_jobs=-1)
-            outlier_algorithm.fit(data)
-            outlier_scores = outlier_algorithm.negative_outlier_factor_
-            outlier_mask = outlier_scores <= LOF_ANOMALY_SCORE_THRESHOLD
-        else:
-            outlier_algorithm = IsolationForest(n_estimators=75, n_jobs=-1, random_state=42)
-            outlier_algorithm.fit(data)
-            outlier_scores = outlier_algorithm.decision_function(data)
-            outlier_mask = outlier_scores <= ITREE_ANOMALY_SCORE_THRESHOLD
-        outlier_count = outlier_mask.sum()
-        if outlier_count > 0:
-            return self._build_transformer_action_suggestion(
-                REMOVE_OUTLIERS_TITLE,
-                f'Remove {outlier_count} multidimensional outlier(s) to reduce the amount of noise in the data.',
-                ActionType.REMOVE,
-                action_options=dict(rows=df.index[outlier_mask].tolist()),
-                axis=Axis.ROW,
-            )
-        return None
-
     def evaluate(self):
         suggestions = []
-        ndim = len(self.numeric_columns)
         if len(self.numeric_df) > 0:
             for col in self.numeric_columns:
                 suggestion = self.one_dim_outlier_check(col)
-                if suggestion:
-                    suggestions.append(suggestion)
-            if ndim >= 2:
-                suggestion = self.multi_dim_outlier_check(self.numeric_df, ndim)
                 if suggestion:
                     suggestions.append(suggestion)
         return suggestions
