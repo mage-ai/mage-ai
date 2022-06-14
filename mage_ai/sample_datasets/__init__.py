2baba582ee3aab8bdc240f80191e6c7d3c3151a5
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-06-14 00:23:01 -0700

    [xy] Move sample datasets out of server folder. (#356)

--- mage_ai/sample_datasets/__init__.py
+++ mage_ai/sample_datasets/__init__.py
@@ -18,3 +18,19 @@ def load_dataset(name):
     if not os.path.isfile(fpath):
         raise Exception(f'Dataset {name} is not available')
     return pd.read_csv(fpath)
+
+
+def load_titanic():
+    return load_dataset('titanic_survival.csv')
+
+
+def load_product_purchases():
+    return load_dataset('product_purchases.csv')
+
+
+def load_salary_survey():
+    return load_dataset('salary_survey.csv')
+
+
+def load_user_emails():
+    return load_dataset('user_emails.csv')
