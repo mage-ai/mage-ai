99222a9c6e239d1815c683c28cc27a7140d38ae3
skunichetty <sachchit@mage.ai>
2022-06-15 16:59:06 -0700

    [sk] Refactored download endpoint

--- mage_ai/server/app.py
+++ mage_ai/server/app.py
@@ -1,9 +1,7 @@
-from tokenize import String
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 from flask_cors import CORS
-from io import BytesIO, StringIO
 from mage_ai.data_cleaner.data_cleaner import analyze, clean as clean_data
-from mage_ai.data_cleaner.pipelines.base import DEFAULT_RULES, BasePipeline
+from mage_ai.data_cleaner.pipelines.base import BasePipeline
 from mage_ai.data_cleaner.transformer_actions.utils import generate_action_titles
 from mage_ai.server.client.mage import Mage
 from mage_ai.server.constants import SERVER_PORT
@@ -11,7 +9,6 @@ from mage_ai.server.data.models import FeatureSet, Pipeline
 from numpyencoder import NumpyEncoder
 import json
 import logging
-import pandas as pd
 import os
 import simplejson
 import sys
@@ -30,6 +27,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 thread = None
 api_key = None
+
 
 def rescue_errors(endpoint, error_code=500):
     def handler(*args, **kwargs):
@@ -69,26 +67,6 @@ def index():
 @rescue_errors
 def dataset(id):
     return render_template('index.html')
-
-
-@app.route('/download/feature_sets/<id>', endpoint='download_feature_set')
-@rescue_errors
-def download_feature_set(id):
-    if not id or not FeatureSet.is_valid_id(id):
-        raise RuntimeError(f'Unknown feature set id: {id}')
-    feature_set = FeatureSet(id=id)
-    try:
-        name = feature_set.metadata['name']
-    except KeyError:
-        name = str(id)
-    return app.response_class(
-        response=feature_set.data.to_csv(index=False, mode='w'),
-        status=200,
-        mimetype='text/csv',
-        headers={
-            'Content-Disposition': f'attachment; filename="{name}.csv"',
-        },
-    )
 
 
 @app.route('/process', methods=['POST'], endpoint='process')
@@ -186,6 +164,23 @@ def feature_set(id):
         mimetype='application/json',
     )
     return response
+
+
+@app.route('/feature_sets/<id>/download/', endpoint='feature_set_download')
+@rescue_errors
+def feature_set_download(id):
+    if not FeatureSet.is_valid_id(id):
+        raise RuntimeError(f'Unknown feature set id: {id}')
+    feature_set = FeatureSet(id=id)
+    name = feature_set.metadata['name']
+    return app.response_class(
+        response=feature_set.data.to_csv(index=False, mode='w'),
+        status=200,
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition': f'attachment; filename="{name}.csv"',
+        },
+    )
 
 
 @app.route('/feature_sets/<id>/versions/<version>', endpoint='feature_set_versions_get')
@@ -416,12 +411,14 @@ def launch(mage_api_key=None) -> None:
     print(f'Mage running on host and port {host}:{port}')
     return thread
 
+
 def sync_pipelines():
     print('Syncing pipelines with cloud database.', end='')
     local_pipelines = Pipeline.objects()
     for pipeline in local_pipelines:
         print('.', end='')
         pipeline.sync_pipeline(api_key)
+
 
 def kill():
     if thread is not None:
