12bc2300c0c81386905d5db858b3642aa40fc482
shrey-mage <shrey@mage.ai>
2022-06-22 11:47:11 -0700

    [sp] fix React warning about false prop

--- mage_ai/frontend/components/DataTable/index.tsx
+++ mage_ai/frontend/components/DataTable/index.tsx
@@ -354,7 +354,7 @@ function Table({
                     className="th"
                     key={column.id}
                     style={columnStyle}
-                    title={firstColumn && 'Row number'}
+                    title={firstColumn ? 'Row number' : undefined}
                   >
                     {el}
                   </div>
