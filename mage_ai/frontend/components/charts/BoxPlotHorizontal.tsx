92f5a9ac9298b30dca17a5729b6fba2d0c06165a
shrey-mage <shrey@mage.ai>
2022-06-27 16:12:53 -0700

    [sp] add color & outlier props to box plot

--- mage_ai/frontend/components/charts/BoxPlotHorizontal.tsx
+++ mage_ai/frontend/components/charts/BoxPlotHorizontal.tsx
@@ -1,41 +1,70 @@
 import { BoxPlot } from '@visx/stats';
+import { ThemeContext } from 'styled-components';
 import { scaleLinear } from '@visx/scale';
+import { useContext } from 'react';
+
+import { ThemeType } from '@oracle/styles/themes/constants';
 
 export type BoxPlotHorizontalProps = {
-  min: number;
-  firstQuartile: number;
-  median: number;
-  thirdQuartile: number;
-  max: number;
+  data: {
+    min: number;
+    firstQuartile: number;
+    median: number;
+    thirdQuartile: number;
+    max: number;
+    outliers: number[];
+  },
+  primary?: boolean;
+  secondary?: boolean;
+  danger?: boolean;
+  width: number;
 };
 
 function BoxPlotHorizontal({
-  min,
-  firstQuartile,
-  median,
-  thirdQuartile,
-  max,
+  data,
+  width,
+  primary,
+  secondary,
+  danger,
 }: BoxPlotHorizontalProps) {
+  const themeContext: ThemeType = useContext(ThemeContext);
+
+  const colorIdx = [primary, secondary, danger].findIndex(el => el);
+  const colorMap = [
+    {
+      fill: themeContext.chart.backgroundPrimary,
+      stroke: themeContext.chart.primary,
+    },
+    {
+      fill: themeContext.chart.backgroundSecondary,
+      stroke: themeContext.chart.secondary,
+    },
+    {
+      fill: themeContext.chart.backgroundDanger,
+      stroke: themeContext.chart.danger,
+    },
+  ];
+
+  const color = colorMap[Math.max(colorIdx, 0)];
+  
+  const { min, max, outliers } = data;
+  const yMin = Math.min(...outliers) || min; 
+  const yMax = Math.max(...outliers) || max;
 
   const xScale = scaleLinear<number>({
-    domain: [0, max],
-    range: [min, max],
+    domain: [0, yMax],
+    range: [yMin, yMax],
   });
 
   return (
     <svg>
       <BoxPlot
-        min={min}
-        firstQuartile={firstQuartile}
-        median={median}
-        thirdQuartile={thirdQuartile}
-        max={max}
-        valueScale={xScale}
-        fill="#000"
-        fillOpacity={0.3}
-        stroke="#000"
-        strokeWidth={1}
+        {...data}
+        {...color}
+        boxWidth={width}
         horizontal
+        strokeWidth={1}
+        valueScale={xScale}
       />
     </svg>
   );
