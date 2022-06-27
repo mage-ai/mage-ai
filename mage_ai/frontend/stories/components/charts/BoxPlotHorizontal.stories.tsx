7eab60d264b868cf6f36adce0f0ad552a09da3be
shrey-mage <shrey@mage.ai>
2022-06-27 16:12:59 -0700

    [sp] update storybook

--- mage_ai/frontend/stories/components/charts/BoxPlotHorizontal.stories.tsx
+++ mage_ai/frontend/stories/components/charts/BoxPlotHorizontal.stories.tsx
@@ -3,6 +3,7 @@ import { Meta, Story } from '@storybook/react';
 
 import BoxPlotHorizontal, { BoxPlotHorizontalProps } from '@components/charts/BoxPlotHorizontal';
 import ThemeBlock from '../../ThemeBlock';
+import { UNIT } from '@oracle/styles/units/spacing';
 
 export default {
   component: BoxPlotHorizontal,
@@ -19,12 +20,46 @@ const TemplateWithTheme = ({ ...props }) => (
 const Template: Story<BoxPlotHorizontalProps> =
   (args) => <TemplateWithTheme {...args} />;
 
+const data = {
+  min: 5,
+  firstQuartile: 25,
+  median: 70,
+  thirdQuartile: 85,
+  max: 100,
+  outliers: [
+    105,
+    2,
+    125,
+  ],
+};
+
 export const Regular = Template.bind({});
 
 Regular.args = {
-  min: 24,
-  firstQuartile: 25,
-  median: 90,
-  thirdQuartile: 95,
-  max: 100,
+  data,
+  width: UNIT*4,
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  data,
+  primary: true,
+  width: UNIT*4,
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  data,
+  secondary: true,
+  width: UNIT*4,
+};
+
+export const Danger = Template.bind({});
+
+Danger.args = {
+  data,
+  danger: true,
+  width: UNIT*4,
 };
