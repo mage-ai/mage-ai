f4766ff7804efa874f70db9f4b883b2c4b64c9b1
skunichetty <sachchit@mage.ai>
2022-06-29 11:42:29 -0700

    [sk] Fixed name errors

--- mage_ai/data_preparation/templates/data_loaders/postgres.py
+++ mage_ai/data_preparation/templates/data_loaders/postgres.py
@@ -1,14 +1,14 @@
 from mage_ai.data_loader.postgres import Postgres
 
 
-def load_data_from_databricks():
+def load_data_from_postgres():
     """
     Template code for loading data from PostgreSQL database
     """
     query = 'your PostgreSQL query'  # Specify your SQL query here
 
     config = {
-        # Specify all Databricks connection configuration settings here
+        # Specify all database connection configuration settings here
         'dbname': 'name of your database',
         'user': 'login username',
         'password': 'login password',
