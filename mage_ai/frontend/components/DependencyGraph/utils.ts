ff6bfc6cf70ed65f400f7fe52b4beb74ea413066
shrey-mage <shrey@mage.ai>
2022-07-01 10:29:54 -0700

    [sp] default dep graph colors to dark

--- mage_ai/frontend/components/DependencyGraph/utils.ts
+++ mage_ai/frontend/components/DependencyGraph/utils.ts
@@ -1,5 +1,5 @@
 import BlockType, { BlockTypeEnum } from '@interfaces/BlockType';
-import light from '@oracle/styles/themes/light';
+import dark from '@oracle/styles/themes/dark';
 import { ThemeType } from '@oracle/styles/themes/constants';
 
 export const getFinalLevelIndex = (
@@ -28,13 +28,13 @@ export const getNodeColor = (
   blockType: BlockTypeEnum,
   themeType: ThemeType,
 ) => {
-  let color = (themeType?.chart || light.chart).button1;
+  let color = (themeType?.chart || dark.chart).button1;
   if (blockType === BlockTypeEnum.TRANSFORMER) {
-    color = (themeType?.chart || light.chart).primary;
+    color = (themeType?.chart || dark.chart).primary;
   } else if (blockType === BlockTypeEnum.DATA_EXPORTER) {
-    color = (themeType?.chart || light.chart).button2;
+    color = (themeType?.chart || dark.chart).button2;
   } else if (blockType === BlockTypeEnum.SCRATCHPAD) {
-    color = (themeType?.chart || light.chart).button3;
+    color = (themeType?.chart || dark.chart).button3;
   }
 
   return color;
