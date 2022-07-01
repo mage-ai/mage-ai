1cc0839c53368f7d49fad9989c1d318674a213f7
skunichetty <sachchit@mage.ai>
2022-06-30 18:24:00 -0700

    [sk] Fixed typo

--- mage_ai/data_preparation/templates/data_loaders/snowflake.py
+++ mage_ai/data_preparation/templates/data_loaders/snowflake.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 @data_loader
 def load_data_from_snowflake() -> DataFrame:
     config = {
-        'username': 'your_snowflake_username',
+        'user': 'your_snowflake_username',
         'password': 'your_snowflake_password',
         'account': 'your_snowflake_account_identifier',
     }
