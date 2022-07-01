28a0b88ac21aaf66c3d4efae61af208761f8a5b3
skunichetty <sachchit@mage.ai>
2022-06-30 23:31:12 -0700

    [sk] Fixed some requirements + refactored docs

--- mage_ai/data_preparation/templates/data_loaders/snowflake.py
+++ mage_ai/data_preparation/templates/data_loaders/snowflake.py
@@ -4,6 +4,9 @@ from pandas import DataFrame
 
 @data_loader
 def load_data_from_snowflake() -> DataFrame:
+    """
+    Template code for loading data from a Snowflake warehouse
+    """
     config = {
         'user': 'your_snowflake_username',
         'password': 'your_snowflake_password',
