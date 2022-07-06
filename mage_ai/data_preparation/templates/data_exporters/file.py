e6ce5750d0bb8dfcd2686e84d9b7da8b2f47e3bf
skunichetty <sachchit@mage.ai>
2022-07-05 21:58:03 -0700

    [sk] Fixed small typo in docs

--- mage_ai/data_preparation/templates/data_exporters/file.py
+++ mage_ai/data_preparation/templates/data_exporters/file.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 @data_exporter
 def export_data_to_file(df: DataFrame) -> None:
     """
-    Template code for loading data from local filesytem
+    Template code for exporting data to local filesytem
     """
     filepath = 'path/to/your/file.ext'  # Specify the path to your file.
     return FileIO(filepath).export(df)
