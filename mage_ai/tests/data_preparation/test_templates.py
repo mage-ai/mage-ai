d3fbb9d81ed4413cd67cb3abb3321ce337cea797
skunichetty <sachchit@mage.ai>
2022-07-06 16:55:40 -0700

    [sk] Updated tests

--- mage_ai/tests/data_preparation/test_templates.py
+++ mage_ai/tests/data_preparation/test_templates.py
@@ -185,7 +185,7 @@ def transform_df(df: DataFrame) -> DataFrame:
     \"\"\"
     Template code for a transformer block.
 
-    Add more parameters to this function if this block has multiple parents.
+    Add more parameters to this function if this block has multiple parent blocks.
     There should be one parameter for each output variable from each parent block.
 
     Args:
