369d15bef0e5b2bb3e8b2fec09fd1a7d60f80120
Johnson Kwok <johnson@mage.ai>
2022-07-05 21:42:43 -0700

    [jk] Fix sizing

--- mage_ai/frontend/components/Sidekick/index.style.ts
+++ mage_ai/frontend/components/Sidekick/index.style.ts
@@ -4,10 +4,13 @@ import { COLUMN_HEADER_CHART_HEIGHT } from '@components/datasets/overview/utils'
 import { REGULAR_LINE_HEIGHT } from '@oracle/styles/fonts/sizes';
 import { UNIT } from '@oracle/styles/units/spacing';
 
-export const TOTAL_PADDING = 4 * UNIT;
+const ASIDE_HEADER_HEIGHT = UNIT * 6;
+const SCROLLBAR_HEIGHT = UNIT * 2;
+export const TOTAL_PADDING = UNIT * 4;
 export const TABLE_COLUMN_HEADER_HEIGHT = COLUMN_HEADER_CHART_HEIGHT + (UNIT * 3) + REGULAR_LINE_HEIGHT
 
 export const ContainerStyle = styled.div`
   padding: ${UNIT * 2}px;
-  height: calc(100vh - 48px);
+  height: calc(100vh - ${ASIDE_HEADER_HEIGHT}px - ${SCROLLBAR_HEIGHT}px);
+  width: fit-content;
 `;
