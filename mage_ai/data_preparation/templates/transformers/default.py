5e8bba27f06407f2aba8f5645b3c81893ff535bd
skunichetty <71106771+skunichetty@users.noreply.github.com>
2022-07-06 17:12:47 -0700

    [sk] Updated transformer template to mention adding parameters (#553)

--- mage_ai/data_preparation/templates/transformers/default.py
+++ mage_ai/data_preparation/templates/transformers/default.py
@@ -6,8 +6,11 @@ def transform_df(df: DataFrame) -> DataFrame:
     """
     Template code for a transformer block.
 
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
     Args:
-        df (DataFrame): Data frame from previously executed block.
+        df (DataFrame): Data frame from parent block.
 
     Returns:
         DataFrame: Transformed data frame
