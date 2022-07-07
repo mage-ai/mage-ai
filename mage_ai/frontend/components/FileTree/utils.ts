580067cc7cd761aae9f00094ffb85a8f569a2400
shrey-mage <shrey@mage.ai>
2022-07-07 16:33:44 -0700

    [sp] clean up file util functions & enums

--- mage_ai/frontend/components/FileTree/utils.ts
+++ mage_ai/frontend/components/FileTree/utils.ts
@@ -1,17 +1,20 @@
 import BlockType from '@interfaces/BlockType';
+import { FileExtensionEnum } from '@interfaces/FileType';
+
+export const getBlockFilename = (path: string[]) => path.at(-1);
 
 export const getBlockType = (path: string[]) => (
-    path.at(-2).slice(0, -1)
+  path.at(-2).slice(0, -1)
 );
 
 export const getBlockUUID = (path: string[]) => (
-    path.at(-1).slice(0, -3)
+  getBlockFilename(path).slice(0, -3)
 );
 
 export const findBlockByPath = (blocks: BlockType[], path: string[]) => (
-    blocks.find(({ uuid }) => getBlockUUID(path) === uuid)
+  blocks.find(({ uuid }) => getBlockUUID(path) === uuid)
 );
 
 export const isBlockType = (path: string[]) => (
-    path.at(-1).slice(-2) === 'py'
+  getBlockFilename(path).slice(-2) === FileExtensionEnum.PY
 );
