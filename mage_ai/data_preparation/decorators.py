2a1b4c1eb45a1c4d9dfbde0d8e6ede0f9c82b166
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-07-07 17:43:12 -0700

    [xy] Add comments for decorators file.

--- mage_ai/data_preparation/decorators.py
+++ mage_ai/data_preparation/decorators.py
@@ -1,3 +1,10 @@
+"""Static definition of block decorators.
+These decorators definitions are added to avoid python compilation errors.
+The actual definitions of the decorators are generated at runtime dynamically in
+execute_block method of mage_ai/data_preparation/models/block.py file.
+"""
+
+
 def data_exporter(function):
     return function
 
