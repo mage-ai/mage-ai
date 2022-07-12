4fb85c145eed3f60f9431577464ed342b37485ea
Tommy Dang <quantumventuress@gmail.com>
2022-07-12 14:46:10 -0700

    [td] Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 def readme():
-    with open("README.md", encoding="utf8") as f:
+    with open('README.md', encoding='utf8') as f:
         README = f.read()
     return README
 
@@ -14,13 +14,12 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='mage-ai',
-    version='0.1.0',
+    version='0.1.1',
     author='Mage',
-    author_email="sales@mage.ai",
-    description='Mage - An open-source data management platform that helps you '
-    'clean data and prepare it for training AI/ML models',
+    author_email='eng@mage.ai',
+    description='Mage is an open-source code editor for transforming data and building ML pipelines.',
     long_description=readme(),
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/mage-ai/mage-ai',
     packages=setuptools.find_packages('./'),
     include_package_data=True,
