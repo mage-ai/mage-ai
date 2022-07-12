9b621cb4899dc02fd80c55c6d05b03c865820cf2
Tommy Dang <quantumventuress@gmail.com>
2022-07-12 11:16:29 -0700

    [td] Use positional arguments in scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,22 +1,5 @@
 # !/bin/bash
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
-case $key in
-    --project)
-    PROJECT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
+PROJECT="$1"
 
 : "${PROJECT:="default_repo"}"
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,27 +1,6 @@
 # !/bin/bash
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
-case $key in
-    --project)
-    PROJECT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    --pipeline)
-    PIPELINE="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
+PROJECT="$1"
+PIPELINE="$2"
 
 : "${PROJECT:=""}"
 : "${PIPELINE:=""}"
