0635aeb99d0fa13955770bf2f43e883f0dcbe2cf
skunichetty <71106771+skunichetty@users.noreply.github.com>
2022-07-12 14:24:11 -0700

    [sk] Hide pycache from file tree (#638)

--- mage_ai/data_preparation/models/file.py
+++ mage_ai/data_preparation/models/file.py
@@ -2,7 +2,7 @@ from mage_ai.data_preparation.repo_manager import get_repo_path
 from typing import Dict
 import os
 
-BLACKLISTED_DIRS = frozenset(['venv', 'env', '.git', '.DS_Store'])
+BLACKLISTED_DIRS = frozenset(['venv', 'env', '.git', '.DS_Store', '__pycache__'])
 INACCESSIBLE_DIRS = frozenset(['__pycache__'])
 MAX_DEPTH = 30
 
