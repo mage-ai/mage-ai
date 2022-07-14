8b5d221e2b24e2c5f9cc4e954131e3f5d08137cf
skunichetty <sachchit@mage.ai>
2022-07-13 22:40:37 -0700

    [sk] Refactored PostgreSQL data exporter

--- mage_ai/io/postgres.py
+++ mage_ai/io/postgres.py
@@ -1,8 +1,10 @@
-from mage_ai.io.base import BaseSQL, QUERY_ROW_LIMIT
+from io import StringIO
+from mage_ai.io.base import BaseSQL, QUERY_ROW_LIMIT, TableExistsError, TableExportPolicy
 from mage_ai.io.io_config import IOConfigKeys
+from mage_ai.io.type_handler import gen_table_creation_query, map_to_postgres
 from pandas import DataFrame, read_sql
-from sqlalchemy import create_engine
-from typing import Any, Mapping
+from psycopg2 import connect
+from typing import Any, Mapping, Union
 
 
 class Postgres(BaseSQL):
@@ -31,17 +33,22 @@ class Postgres(BaseSQL):
             port (str): Port on which the database is running.
             **kwargs: Additional settings for creating SQLAlchemy engine and connection
         """
-        self.dburl = (
-            f'postgresql+psycopg2://{user}:{password}@{host}{":"+port if port else ""}/{dbname}'
+        super().__init__(
+            verbose=verbose,
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+            **kwargs,
         )
-        super().__init__(verbose=verbose, **kwargs)
 
     def open(self) -> None:
         """
         Opens a connection to the PostgreSQL database specified by the parameters.
         """
         with self.printer.print_msg('Opening connection to PostgreSQL database'):
-            self._ctx = create_engine(self.dburl, **self.settings).connect(**self.settings)
+            self._ctx = connect(**self.settings)
 
     def query(self, query_string: str, **query_vars) -> None:
         """
@@ -52,7 +59,8 @@ class Postgres(BaseSQL):
             query_vars: Variable values to fill in when using format strings in query.
         """
         with self.printer.print_msg(f'Executing query \'{query_string}\''):
-            self.conn.execute(query_string, **query_vars)
+            with self.conn.cursor() as cur:
+                cur.execute(query_string, **query_vars)
 
     def load(self, query_string: str, limit: int = QUERY_ROW_LIMIT, **kwargs) -> DataFrame:
         """
@@ -73,14 +81,19 @@ class Postgres(BaseSQL):
             return read_sql(self._enforce_limit(query_string, limit), self.conn, **kwargs)
 
     def export(
-        self, df: DataFrame, name: str, index: bool = False, if_exists: str = 'replace', **kwargs
+        self,
+        df: DataFrame,
+        table_name: str,
+        index: bool = False,
+        if_exists: Union[TableExportPolicy, str] = 'replace',
+        **kwargs,
     ) -> None:
         """
         Exports dataframe to the connected database from a Pandas data frame.  If table doesn't
         exist, the table is automatically created.
 
         Args:
-            name (str): Name of the table to insert rows from this data frame into.
+            table_name (str): Name of the table to insert rows from this data frame into.
             index (bool): If true, the data frame index is also exported alongside the table. Defaults to False.
             if_exists (str): Specifies export policy if table exists. Either
                 - `'fail'`: throw an error.
@@ -89,9 +102,27 @@ class Postgres(BaseSQL):
             Defaults to `'replace'`.
             **kwargs: Additional query parameters.
         """
-        with self.printer.print_msg(f'Exporting data frame to table \'{name}\''):
-            df.to_sql(name, self.conn, index=index, if_exists=if_exists, **kwargs)
-        df.to_sql(name, self.conn, index=index, if_exists=if_exists, **kwargs)
+        with self.printer.print_msg(f'Exporting data frame to table \'{table_name}\''):
+            buffer = StringIO()
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    f'SELECT * FROM information_schema.tables WHERE table_name = \'{table_name}\''
+                )
+                exists = cur.rowcount
+                if exists:
+                    if if_exists == TableExportPolicy.FAIL:
+                        raise TableExistsError(f'Table \'{table_name}\' already exists in database')
+                    elif if_exists == TableExportPolicy.REPLACE:
+                        cur.execute(f'DELETE FROM {table_name}')
+                else:
+                    # TODO find a way to integrate column modifiers
+                    query = gen_table_creation_query(df, map_to_postgres, table_name)
+                    cur.execute(query)
+
+                df.to_csv(buffer, index=index, header=False)
+                buffer.seek(0)
+                cur.copy_from(buffer, table=table_name, sep=',', null='')
+            self.conn.commit()
 
     @classmethod
     def with_config(cls, config: Mapping[str, Any]) -> 'Postgres':
