f5d139ce43c69c3d80fa68381e41d58a8db0b496
skunichetty <sachchit@mage.ai>
2022-07-14 23:06:08 -0700

    [sk] Better file templates

--- mage_ai/data_preparation/templates/data_exporters/file.py
+++ mage_ai/data_preparation/templates/data_exporters/file.py
@@ -1,8 +1,5 @@
-from mage_ai.data_preparation.repo_manager import get_repo_path
 from mage_ai.io.file import FileIO
-from mage_ai.io.io_config import IOConfig
 from pandas import DataFrame
-from os import path
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
@@ -12,9 +9,6 @@ if 'data_exporter' not in globals():
 def export_data_to_file(df: DataFrame) -> None:
     """
     Template for exporting data to filesystem.
-    Specify your configuration settings in 'io_config.yaml'.
     """
-    config_path = path.join(get_repo_path(), 'io_config.yaml')
-    config_profile = 'default'
-
-    FileIO.with_config(IOConfig(config_path).use(config_profile)).export(df)
+    filepath = 'path/to/write/dataframe/to.csv'
+    FileIO(filepath).export(df)
--- mage_ai/data_preparation/templates/data_loaders/file.py
+++ mage_ai/data_preparation/templates/data_loaders/file.py
@@ -1,8 +1,5 @@
-from mage_ai.data_preparation.repo_manager import get_repo_path
 from mage_ai.io.file import FileIO
-from mage_ai.io.io_config import IOConfig
 from pandas import DataFrame
-from os import path
 
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
@@ -12,9 +9,6 @@ if 'data_loader' not in globals():
 def load_data_from_file() -> DataFrame:
     """
     Template for loading data from filesystem.
-    Specify your configuration settings in 'io_config.yaml'.
     """
-    config_path = path.join(get_repo_path(), 'io_config.yaml')
-    config_profile = 'default'
-
-    return FileIO.with_config(IOConfig(config_path).use(config_profile)).load()
+    filepath = 'path/to/your/file.csv'
+    return FileIO(filepath).load()
