cee480aed2464a2e35cc61b92a1d4b2443ee6335
skunichetty <sachchit@mage.ai>
2022-07-15 15:52:35 -0700

    [sk] Updated templates to use new configuration method

--- mage_ai/data_preparation/templates/data_loaders/s3.py
+++ mage_ai/data_preparation/templates/data_loaders/s3.py
@@ -1,5 +1,5 @@
 from mage_ai.data_preparation.repo_manager import get_repo_path
-from mage_ai.io.io_config import IOConfig
+from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.s3 import S3
 from pandas import DataFrame
 from os import path
@@ -17,4 +17,4 @@ def load_from_s3_bucket() -> DataFrame:
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
-    return S3.with_config(IOConfig(config_path).use(config_profile)).load()
+    return S3.with_config(ConfigFileLoader(config_path, config_profile)).load()
