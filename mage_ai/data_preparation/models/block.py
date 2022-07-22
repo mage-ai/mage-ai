ff4c78d2650dd13e912308ea2d2e5c5341bbd181
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-07-22 14:07:40 -0700

    [xy] Disable outputs verification.

--- mage_ai/data_preparation/models/block.py
+++ mage_ai/data_preparation/models/block.py
@@ -285,6 +285,7 @@ class Block:
         should_save_outputs=True,
         update_status=True,
     ):
+<<<<<<< HEAD
         try:
             if not run_all_blocks:
                 not_executed_upstream_blocks = list(
@@ -311,7 +312,7 @@ class Block:
                 ))
             else:
                 if should_save_outputs:
-                    self.__verify_outputs(block_output)
+                    # self.__verify_outputs(block_output)
                     variable_mapping = dict(zip(self.output_variables.keys(), block_output))
 
             if should_save_outputs:
