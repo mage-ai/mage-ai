33aeb591d979ae5ba02a37e1f53139631ea812e8
Tommy Dang <quantumventuress@gmail.com>
2022-07-22 10:29:03 -0700

    [td] Fix double table index output

--- mage_ai/frontend/components/CodeBlock/CodeOutput/index.tsx
+++ mage_ai/frontend/components/CodeBlock/CodeOutput/index.tsx
@@ -85,6 +85,7 @@ function CodeOutput({
         // Remove border 2px and padding from each side
         width={mainContainerWidth - (2 + (PADDING_UNITS * UNIT * 2) + 2 + SCROLLBAR_WIDTH)}
       />
+      {console.log(index)}
       {shape && (
         <Spacing ml={1} my={1}>
           <Text>
