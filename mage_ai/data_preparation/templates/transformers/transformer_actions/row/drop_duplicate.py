4c8603877d90e033adcb00ea9dbf206b1a449779
skunichetty <sachchit@mage.ai>
2022-07-27 03:45:51 -0400

    [sk] Addressed review comments #2

--- mage_ai/data_preparation/templates/transformers/transformer_actions/row/drop_duplicate.py
+++ mage_ai/data_preparation/templates/transformers/transformer_actions/row/drop_duplicate.py
@@ -1,7 +1,7 @@
 from mage_ai.data_cleaner.transformer_actions.base import BaseAction
 from mage_ai.data_cleaner.transformer_actions.constants import ActionType, Axis
 from mage_ai.data_cleaner.transformer_actions.utils import build_transformer_action
-from pandas import DataFrame, concat
+from pandas import DataFrame
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
