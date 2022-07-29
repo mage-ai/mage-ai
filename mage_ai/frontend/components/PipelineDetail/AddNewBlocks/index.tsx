344d1d809dcf34ce3baeaae5b0ca068ccb805cde
Shrey Pandya <105667442+shrey-mage@users.noreply.github.com>
2022-07-29 11:48:54 -0400

    [sp] update block templates on frontend (#758)
    * [sp] add data exporter templates
    
    * [sp] remove custom code from transformer actions
    
    * [sp] add API template to data loader only

--- mage_ai/frontend/components/PipelineDetail/AddNewBlocks/index.tsx
+++ mage_ai/frontend/components/PipelineDetail/AddNewBlocks/index.tsx
@@ -30,6 +30,7 @@ type AddNewBlocksProps = {
 
 const DATA_LOADER_BUTTON_INDEX = 0;
 const TRANSFORMER_BUTTON_INDEX = 1;
+const DATA_EXPORTER_BUTTON_INDEX = 2;
 
 function AddNewBlocks({
   addNewBlock,
@@ -38,23 +39,41 @@ function AddNewBlocks({
   const [buttonMenuOpenIndex, setButtonMenuOpenIndex] = useState(null);
   const dataLoaderButtonRef = useRef(null);
   const transformerButtonRef = useRef(null);
+  const dataExporterButtonRef = useRef(null);
   const sharedProps = {
     compact,
     inline: true,
   };
 
-  const dataLoaderMenuItems = DATA_SOURCE_TYPES.map((sourceType: DataSourceTypeEnum) => ({
-    label: () => DATA_SOURCE_TYPE_HUMAN_READABLE_NAME_MAPPING[sourceType],
-    onClick: () => {
-      addNewBlock({
-        config: {
-          data_source: sourceType === DataSourceTypeEnum.GENERIC ? null : sourceType,
-        },
-        type: BlockTypeEnum.DATA_LOADER,
-      });
-    },
-    uuid: sourceType,
-  }));
+  const dataLoaderMenuItems = (
+    DATA_SOURCE_TYPES[BlockTypeEnum.DATA_LOADER].map((sourceType: DataSourceTypeEnum) => ({
+      label: () => DATA_SOURCE_TYPE_HUMAN_READABLE_NAME_MAPPING[sourceType],
+      onClick: () => {
+        addNewBlock({
+          config: {
+            data_source: sourceType === DataSourceTypeEnum.GENERIC ? null : sourceType,
+          },
+          type: BlockTypeEnum.DATA_LOADER,
+        });
+      },
+      uuid: `data_loader/${sourceType}`,
+    }))
+  );
+
+  const dataExporterMenuItems = (
+    DATA_SOURCE_TYPES[BlockTypeEnum.DATA_EXPORTER].map((sourceType: DataSourceTypeEnum) => ({
+      label: () => DATA_SOURCE_TYPE_HUMAN_READABLE_NAME_MAPPING[sourceType],
+      onClick: () => {
+        addNewBlock({
+          config: {
+            data_source: sourceType === DataSourceTypeEnum.GENERIC ? null : sourceType,
+          },
+          type: BlockTypeEnum.DATA_EXPORTER,
+        });
+      },
+      uuid: `data_exporter/${sourceType}`,
+    }))
+  );
 
   const columnActionMenuItems = createActionMenuGroupings(
     COLUMN_ACTION_GROUPINGS,
@@ -155,31 +174,44 @@ function AddNewBlocks({
               Transformer
             </KeyboardShortcutButton>
           </FlyoutMenuWrapper>
+
+          <Spacing ml={1} />
+
+          <FlyoutMenuWrapper
+            items={dataExporterMenuItems}
+            onClickCallback={closeButtonMenu}
+            open={buttonMenuOpenIndex === DATA_EXPORTER_BUTTON_INDEX}
+            parentRef={dataExporterButtonRef}
+            uuid="data_exporter_button"
+          >
+            <KeyboardShortcutButton
+              {...sharedProps}
+              beforeElement={
+                <IconContainerStyle compact={compact} yellow>
+                  <Add
+                    inverted
+                    size={compact ? ICON_SIZE / 2 : ICON_SIZE}
+                  />
+                </IconContainerStyle>
+              }
+              onClick={(e) => {
+                e.preventDefault();
+                setButtonMenuOpenIndex(val =>
+                  val === DATA_EXPORTER_BUTTON_INDEX
+                    ? null
+                    : DATA_EXPORTER_BUTTON_INDEX,
+                );
+              }}
+              uuid="AddNewBlocks/Data_exporter"
+            >
+              Data exporter
+            </KeyboardShortcutButton>
+          </FlyoutMenuWrapper>
         </FlexContainer>
       </ClickOutside>
 
-      <Spacing ml={1} />
 
-      <KeyboardShortcutButton
-        {...sharedProps}
-        beforeElement={
-          <IconContainerStyle compact={compact} yellow>
-            <Add
-              inverted
-              size={compact ? ICON_SIZE / 2 : ICON_SIZE}
-            />
-          </IconContainerStyle>
-        }
-        onClick={(e) => {
-          e.preventDefault();
-          addNewBlock({
-            type: BlockTypeEnum.DATA_EXPORTER,
-          });
-        }}
-        uuid="AddNewBlocks/Data_exporter"
-      >
-        Data exporter
-      </KeyboardShortcutButton>
+
 
       <Spacing ml={1} />
 
--- mage_ai/frontend/interfaces/DataSourceType.ts
+++ mage_ai/frontend/interfaces/DataSourceType.ts
@@ -1,4 +1,7 @@
+import { BlockTypeEnum } from './BlockType';
+
 export enum DataSourceTypeEnum {
+  API = 'api',
   BIGQUERY = 'bigquery',
   FILE = 'file',
   GENERIC = 'generic',
@@ -9,6 +12,7 @@ export enum DataSourceTypeEnum {
 }
 
 export const DATA_SOURCE_TYPE_HUMAN_READABLE_NAME_MAPPING = {
+  [DataSourceTypeEnum.API]: 'API',
   [DataSourceTypeEnum.BIGQUERY]: 'Google BigQuery',
   [DataSourceTypeEnum.FILE]: 'Local file',
   [DataSourceTypeEnum.GENERIC]: 'Generic (no template)',
@@ -18,14 +22,26 @@ export const DATA_SOURCE_TYPE_HUMAN_READABLE_NAME_MAPPING = {
   [DataSourceTypeEnum.SNOWFLAKE]: 'Snowflake',
 };
 
-export const DATA_SOURCE_TYPES: DataSourceTypeEnum[] = [
-  DataSourceTypeEnum.GENERIC,
-  DataSourceTypeEnum.FILE,
-  DataSourceTypeEnum.BIGQUERY,
-  DataSourceTypeEnum.POSTGRES,
-  DataSourceTypeEnum.REDSHIFT,
-  DataSourceTypeEnum.S3,
-  DataSourceTypeEnum.SNOWFLAKE,
-];
+export const DATA_SOURCE_TYPES: { [blockType in BlockTypeEnum]?: DataSourceTypeEnum[] } = {
+  [BlockTypeEnum.DATA_LOADER]: [
+    DataSourceTypeEnum.GENERIC,
+    DataSourceTypeEnum.FILE,
+    DataSourceTypeEnum.API,
+    DataSourceTypeEnum.BIGQUERY,
+    DataSourceTypeEnum.POSTGRES,
+    DataSourceTypeEnum.REDSHIFT,
+    DataSourceTypeEnum.S3,
+    DataSourceTypeEnum.SNOWFLAKE,
+  ],
+  [BlockTypeEnum.DATA_EXPORTER]: [
+    DataSourceTypeEnum.GENERIC,
+    DataSourceTypeEnum.FILE,
+    DataSourceTypeEnum.BIGQUERY,
+    DataSourceTypeEnum.POSTGRES,
+    DataSourceTypeEnum.REDSHIFT,
+    DataSourceTypeEnum.S3,
+    DataSourceTypeEnum.SNOWFLAKE,
+  ],
+};
 
 export default DataSourceTypeEnum;
