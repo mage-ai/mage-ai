ac63c9889fe0dc3b77eb01e4499b71bde08f9c33
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-08-04 15:26:04 -0700

    [xy] Add cloudformation template for deploying to ecs cluster. (#816)
    * [xy] Add cloudformation template for deploying to ecs cluster.
    
    * [xy] Add bash script to deploy to ECS.

---
+++ scripts/deploy/ecs.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+SUBNET="$1"
+PARAMETERS=ParameterKey=SubnetID,ParameterValue=${SUBNET}
+DIR=$(cd "$(dirname "$0")"; pwd)
+TEMPLATE_PATH=file://$DIR/ecs.yaml
+
+aws cloudformation create-stack --stack-name mage-data-preparation \
+--template-body $TEMPLATE_PATH \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters $PARAMETERS
