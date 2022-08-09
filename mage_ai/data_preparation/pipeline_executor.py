e0d05a8ce316122fdad45b015b6bbebcb54e0b46
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-08-09 01:39:38 -0700

    [xy] Upload bootstrap script before launching cluster. (#841)

--- mage_ai/data_preparation/pipeline_executor.py
+++ mage_ai/data_preparation/pipeline_executor.py
@@ -2,6 +2,7 @@ from mage_ai.data_preparation.models.constants import PipelineType
 from mage_ai.data_preparation.models.pipeline import Pipeline
 from mage_ai.data_preparation.templates.utils import template_env
 from mage_ai.services.emr import emr
+from mage_ai.services.emr.resource_manager import EmrResourceManager
 from mage_ai.services.s3 import s3
 from typing import Dict
 import asyncio
@@ -35,9 +36,12 @@ class PipelineExecutor:
 class PySparkPipelineExecutor(PipelineExecutor):
     def __init__(self, pipeline: Pipeline):
         super().__init__(pipeline)
-        path_parts = self.pipeline.remote_variables_dir.replace('s3://', '').split('/')
-        self.s3_bucket = path_parts.pop(0)
-        self.s3_path_prefix = '/'.join(path_parts)
+        self.resource_manager = EmrResourceManager(
+            pipeline.repo_config.s3_bucket,
+            pipeline.repo_config.s3_path_prefix,
+        )
+        self.s3_bucket = pipeline.repo_config.s3_bucket
+        self.s3_path_prefix = pipeline.repo_config.s3_path_prefix
 
     def execute(
         self,
@@ -52,19 +56,8 @@ class PySparkPipelineExecutor(PipelineExecutor):
         3. Submit a spark job
         """
         self.upload_pipeline_execution_script(global_vars=global_vars)
+        self.resource_manager.upload_bootstrap_script()
         self.submit_spark_job()
-
-    @property
-    def bootstrap_script_path(self) -> str:
-        return os.path.join('s3://', self.s3_bucket, self.bootstrap_script_path_key)
-
-    @property
-    def bootstrap_script_path_key(self) -> str:
-        return os.path.join(self.s3_path_prefix, 'scripts/emr_bootstrap.sh')
-
-    @property
-    def log_uri(self) -> str:
-        return os.path.join('s3://', self.s3_bucket, self.s3_path_prefix, 'logs')
 
     @property
     def pipeline_script_path(self) -> str:
@@ -82,13 +75,10 @@ class PySparkPipelineExecutor(PipelineExecutor):
             pipeline_config=self.pipeline.to_dict(include_content=True),
             pipeline_uuid=self.pipeline.uuid,
             repo_config=self.pipeline.repo_config.to_dict(remote=True),
-            spark_log_path=self.log_uri,
+            spark_log_path=self.resource_manager.log_uri,
         )
-        bootstrap_script_code = template_env.get_template(
-            'pipeline_execution/emr_bootstrap.sh',
-        ).render()
+
         s3.Client(self.s3_bucket).upload(self.pipeline_script_path_key, execution_script_code)
-        s3.Client(self.s3_bucket).upload(self.bootstrap_script_path_key, bootstrap_script_code)
 
     def submit_spark_job(self):
         step = {
@@ -99,7 +89,7 @@ class PySparkPipelineExecutor(PipelineExecutor):
         return emr.submit_spark_job(
             cluster_name=step['name'],
             steps=[step],
-            bootstrap_script_path=self.bootstrap_script_path,
+            bootstrap_script_path=self.resource_manager.bootstrap_script_path,
             emr_config=self.pipeline.repo_config.emr_config,
-            log_uri=self.log_uri,
+            log_uri=self.resource_manager.log_uri,
         )
