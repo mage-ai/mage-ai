edb7fa1fef6a143ebd7d7430317caa579c60ccec
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-08-11 10:10:11 -0700

    [xy] Fix code template for load_data_from_api.

--- mage_ai/tests/data_preparation/test_templates.py
+++ mage_ai/tests/data_preparation/test_templates.py
@@ -217,7 +217,7 @@ if 'test' not in globals():
 
 
 @data_loader
-def load_data_from_api() -> DataFrame:
+def load_data_from_api(**kwargs) -> DataFrame:
     \"\"\"
     Template for loading data from API
     \"\"\"
