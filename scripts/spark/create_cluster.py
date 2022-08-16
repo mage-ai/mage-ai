8246bb5753a1d0264417b668447848eba2356f99
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-08-15 23:41:13 -0700

    [xy] Add command to create spark cluster.

--- scripts/spark/create_cluster.py
+++ scripts/spark/create_cluster.py
@@ -3,9 +3,7 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 
-from mage_ai.data_preparation.repo_manager import RepoConfig
-from mage_ai.services.emr.emr import create_a_new_cluster
-from mage_ai.services.emr.resource_manager import EmrResourceManager
+from mage_ai.services.emr.launcher import create_cluster
 
 
 if __name__ == '__main__':
@@ -16,22 +14,4 @@ if __name__ == '__main__':
         )
 
     project_path = os.path.abspath(sys.argv[1])
-    repo_config = RepoConfig(project_path)
-    # Upload bootstrap script
-    resource_manager = EmrResourceManager(
-        repo_config.s3_bucket,
-        repo_config.s3_path_prefix,
-    )
-    resource_manager.upload_bootstrap_script()
-
-    # Create EMR cluster
-    cluster_id = create_a_new_cluster(
-        'mage-data-prepartion',
-        [],
-        repo_config.emr_config,
-        bootstrap_script_path=resource_manager.bootstrap_script_path,
-        done_status='WAITING',
-        keep_alive=True,
-        log_uri=resource_manager.log_uri,
-    )
-    print(f'Cluster {cluster_id} is created')
+    create_cluster(project_path)
