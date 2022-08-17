efe451d12739248fd6e4895e1ee9afd56337d907
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-08-17 16:03:16 -0700

    [xy] Make sqlalchemy required. (#882)

--- setup.py
+++ setup.py
@@ -40,10 +40,6 @@ setuptools.setup(
     extras_require={
         'bigquery': ['google-cloud-bigquery==3.2.0', 'db-dtypes==1.0.2'],
         'hdf5': ['tables==3.7.0'],
-        'orchestration': [
-            'alembic>=1.7.5',
-            'sqlalchemy>=1.4.20'
-        ],
         'postgres': ['psycopg2-binary==2.9.3'],
         'redshift': ['boto3==1.24.19', 'redshift-connector==2.0.907'],
         's3': ['botocore==1.27.19', 'boto3==1.24.19'],
@@ -57,7 +53,6 @@ setuptools.setup(
             'psycopg2-binary==2.9.3',
             'redshift-connector==2.0.907',
             'snowflake-connector-python==2.7.9',
-            'sqlalchemy>=1.4.20',
         ],
     },
 )
