d79b177f33ea61d7afa319213a6b3c23cedbe505
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-08-23 23:15:10 -0700

    [xy] Bump version to 0.3.2. (#918)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='mage-ai',
-    version='0.3.1',
+    version='0.3.2',
     author='Mage',
     author_email='eng@mage.ai',
     description='Mage is a notebook for building and deploying data pipelines.',
