688478a3abf590cce07da91462a1995a4cb2e2f2
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-08-30 16:50:06 -0700

    [xy] Add terraform script to deploy to ECS. (#948)

---
+++ scripts/deploy/terraform/iam.tf
@@ -0,0 +1,26 @@
+# iam.tf | IAM Role Policies
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "${var.app_name}-execution-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name        = "${var.app_name}-iam-role"
+    Environment = var.app_environment
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
---
+++ scripts/deploy/terraform/main.tf
@@ -0,0 +1,131 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_ecs_cluster" "aws-ecs-cluster" {
+  name = "${var.app_name}-${var.app_environment}-cluster"
+  tags = {
+    Name        = "${var.app_name}-ecs"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.app_environment}-logs"
+
+  tags = {
+    Application = var.app_name
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_ecs_task_definition" "aws-ecs-task" {
+  family = "${var.app_name}-task"
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "${var.app_name}-${var.app_environment}-container",
+      "image": "${var.docker_image}",
+      "essential": true,
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${aws_cloudwatch_log_group.log-group.id}",
+          "awslogs-region": "${var.aws_region}",
+          "awslogs-stream-prefix": "${var.app_name}-${var.app_environment}"
+        }
+      },
+      "portMappings": [
+        {
+          "containerPort": 6789,
+          "hostPort": 6789
+        }
+      ],
+      "cpu": 256,
+      "memory": 512,
+      "networkMode": "awsvpc"
+    }
+  ]
+  DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "512"
+  cpu                      = "256"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+
+  tags = {
+    Name        = "${var.app_name}-ecs-td"
+    Environment = var.app_environment
+  }
+}
+
+data "aws_ecs_task_definition" "main" {
+  task_definition = aws_ecs_task_definition.aws-ecs-task.family
+}
+
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "${var.app_name}-${var.app_environment}-ecs-service"
+  cluster              = aws_ecs_cluster.aws-ecs-cluster.id
+  task_definition      = "${aws_ecs_task_definition.aws-ecs-task.family}:${max(aws_ecs_task_definition.aws-ecs-task.revision, data.aws_ecs_task_definition.main.revision)}"
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
+
+  network_configuration {
+    subnets          = aws_subnet.public.*.id
+    assign_public_ip = true
+    security_groups = [
+      aws_security_group.service_security_group.id,
+      # aws_security_group.load_balancer_security_group.id
+    ]
+  }
+
+  # load_balancer {
+  #  target_group_arn = aws_lb_target_group.target_group.arn
+  #  container_name   = "${var.app_name}-${var.app_environment}-container"
+  #  container_port   = 8080
+  #}
+
+  #depends_on = [aws_lb_listener.listener]
+}
+
+resource "aws_security_group" "service_security_group" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  ingress {
+    from_port       = 6789
+    to_port         = 6789
+    protocol        = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "${var.app_name}-service-sg"
+    Environment = var.app_environment
+  }
+}
+
