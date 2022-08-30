90a29edb2fd68d6a3b3d54c9b977b87abe447dcf
Tommy Dang <quantumventuress@gmail.com>
2022-08-30 13:54:26 -0700

    [td] Display table for pipelines

--- mage_ai/frontend/pages/pipelines/index.tsx
+++ mage_ai/frontend/pages/pipelines/index.tsx
@@ -1,10 +1,15 @@
+import { useMemo } from 'react';
 import { useMutation } from 'react-query';
 import { useRouter } from 'next/router';
 
+import BlockType, { BlockTypeEnum } from '@interfaces/BlockType'
 import Dashboard from '@components/Dashboard';
+import Flex from '@oracle/components/Flex';
+import FlexTable from '@oracle/components/FlexTable';
 import KeyboardShortcutButton from '@oracle/elements/Button/KeyboardShortcutButton';
+import Text from '@oracle/elements/Text';
 import api from '@api';
-import { Add } from '@oracle/icons';
+import { Add, ChevronRight } from '@oracle/icons';
 import { BUTTON_GRADIENT } from '@oracle/styles/colors/gradients';
 import { UNIT } from '@oracle/styles/units/spacing';
 import { onSuccess } from '@api/utils/response';
@@ -27,7 +32,7 @@ function PipelineListPage() {
               uuid,
             },
           }) => {
-            router.push('/pipelines/[...slug]', `/pipelines/${uuid}`);
+            router.push('/pipelines/[pipeline]', `/pipelines/${uuid}`);
           },
           onErrorCallback: ({
             error: {
@@ -42,7 +47,7 @@ function PipelineListPage() {
     },
   );
 
-  const pipelines = data?.pipelines;
+  const pipelines = useMemo(() => data?.pipelines || [], [data]);
   const projects = dataProjects?.projects;
 
   return (
@@ -53,6 +58,7 @@ function PipelineListPage() {
           background={BUTTON_GRADIENT}
           bold
           beforeElement={<Add size={2.5 * UNIT} />}
+          inline
           loading={isLoading}
           // @ts-ignore
           onClick={() => createPipeline({
@@ -66,7 +72,38 @@ function PipelineListPage() {
         </KeyboardShortcutButton>
       }
       title="Pipelines"
-    />
+    >
+      <FlexTable
+        buildLinkProps={(rowIndex: number) => ({
+          as: `/pipelines/${pipelines[rowIndex].uuid}`,
+          href: '/pipelines/[pipeline]',
+        })}
+        columnHeaders={[
+          <Text bold monospace>
+            Name
+          </Text>,
+          <Text bold monospace>
+            Blocks
+          </Text>,
+          null,
+        ]}
+        columnFlex={[8, 1, 1]}
+        rows={pipelines.map(({
+          blocks,
+          name,
+        }) => [
+          <Text>
+            {name}
+          </Text>,
+          <Text>
+            {blocks.filter(({ type }) => BlockTypeEnum.SCRATCHPAD !== type).length}
+          </Text>,
+          <Flex flex={1} justifyContent="flex-end">
+            <ChevronRight muted size={2 * UNIT} />
+          </Flex>
+        ])}
+      />
+    </Dashboard>
   );
 }
 
