05a66872c44aa72adb7b34e4b60efec7ef1e2222
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-09-08 13:10:11 -0700

    [xy] Integrate with slack for notification.

--- mage_ai/data_preparation/repo_manager.py
+++ mage_ai/data_preparation/repo_manager.py
@@ -1,8 +1,10 @@
 from mage_ai.data_preparation.shared.constants import REPO_PATH_ENV_VAR
 from mage_ai.data_preparation.templates.utils import copy_template_directory
+from jinja2 import Template
 from typing import Dict
 import os
 import sys
+import traceback
 import yaml
 
 
@@ -13,7 +15,8 @@ class RepoConfig:
         self.variables_dir = self.repo_path
         try:
             with open(os.path.join(self.repo_path, 'metadata.yaml')) as f:
-                repo_config = yaml.full_load(f) or {}
+                config_file = Template(f.read()).render(env_var=os.getenv)
+                repo_config = yaml.full_load(config_file) or {}
             self.variables_dir = repo_config.get('variables_dir', self.repo_path)
             if self.variables_dir is not None and not self.variables_dir.startswith('s3'):
                 self.variables_dir = os.path.abspath(
@@ -22,15 +25,17 @@ class RepoConfig:
             self.remote_variables_dir = repo_config.get('remote_variables_dir')
             self.ecs_config = repo_config.get('ecs_config')
             self.emr_config = repo_config.get('emr_config')
+            self.notification_config = repo_config.get('notification_config')
+
             self.s3_bucket = None
             self.s3_path_prefix = None
-
             if self.remote_variables_dir is not None and \
                     self.remote_variables_dir.startswith('s3://'):
                 path_parts = self.remote_variables_dir.replace('s3://', '').split('/')
                 self.s3_bucket = path_parts.pop(0)
                 self.s3_path_prefix = '/'.join(path_parts)
         except Exception:
+            traceback.print_exc()
             pass
 
     @classmethod
@@ -38,6 +43,7 @@ class RepoConfig:
         repo_config = RepoConfig()
         repo_config.ecs_config = config_dict.get('ecs_config')
         repo_config.emr_config = config_dict.get('emr_config')
+        repo_config.notification_config = config_dict.get('notification_config')
         repo_config.repo_path = config_dict.get('repo_path')
         repo_config.repo_name = os.path.basename(repo_config.repo_path)
         repo_config.variables_dir = config_dict.get('variables_dir')
@@ -48,6 +54,7 @@ class RepoConfig:
         return dict(
             ecs_config=self.ecs_config,
             emr_config=self.emr_config,
+            notification_config=self.notification_config,
             repo_path=self.repo_path,
             variables_dir=self.remote_variables_dir if remote else self.variables_dir,
             remote_variables_dir=self.remote_variables_dir,
