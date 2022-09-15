e6a736265b757a2319e695709049dae36c4e44b9
David Yang <davidyang1996@gmail.com>
2022-09-15 14:54:54 -0700

    [dy] Update run_status_checker

--- mage_ai/orchestration/run_status_checker.py
+++ mage_ai/orchestration/run_status_checker.py
@@ -1,12 +1,26 @@
 from datetime import datetime
+from mage_ai.data_preparation.models.pipeline import Pipeline
+from mage_ai.data_preparation.repo_manager import get_repo_path
 from mage_ai.orchestration.db.models import BlockRun, PipelineRun, PipelineSchedule
+from mage_ai.shared.array import find
 
+PIPELINE_FAILURE_STATUSES = [
+    PipelineRun.PipelineRunStatus.CANCELLED,
+    PipelineRun.PipelineRunStatus.FAILED,
+]
+
+BLOCK_FAILURE_STATUSES = [
+    BlockRun.BlockRunStatus.CANCELLED,
+    BlockRun.BlockRunStatus.FAILED,
+]
 
 def check_status(
     pipeline_uuid: str,
     execution_date: datetime,
     block_uuid: str = None,
 ) -> bool:
+    __validate_pipeline_and_block(pipeline_uuid, block_uuid)
+
     pipeline_run = (
         PipelineRun
         .query
@@ -21,9 +35,28 @@ def check_status(
     pipeline_run.refresh()
 
     if block_uuid is not None:
-        block_run = next(filter(lambda run: run.block_uuid == block_uuid, pipeline_run.block_runs))
+        block_run = find(lambda run: run.block_uuid == block_uuid, pipeline_run.block_runs)
         if block_run is None:
             return False
-        return block_run.status == BlockRun.BlockRunStatus.COMPLETED
+
+        if block_run.status in BLOCK_FAILURE_STATUSES:
+            raise Exception('Upstream block run failed, stopping sensor...')
+        else:
+            return block_run.status == BlockRun.BlockRunStatus.COMPLETED
     else:
-        return pipeline_run.status == PipelineRun.PipelineRunStatus.COMPLETED
+        if pipeline_run.status in PIPELINE_FAILURE_STATUSES:
+            raise Exception('Upstream pipeline run failed, stopping sensor...')
+        else:
+            return pipeline_run.status == PipelineRun.PipelineRunStatus.COMPLETED
+
+
+def __validate_pipeline_and_block(pipeline_uuid, block_uuid):
+    if pipeline_uuid not in Pipeline.get_all_pipelines(get_repo_path()):
+        raise Exception('Pipeline not found, stopping sensor...')
+
+    pipeline = Pipeline(pipeline_uuid, get_repo_path())
+
+    if block_uuid is not None:
+        block = pipeline.get_block(block_uuid)
+        if block is None:
+            raise Exception('Block not found in pipeline, stopping sensor...')
