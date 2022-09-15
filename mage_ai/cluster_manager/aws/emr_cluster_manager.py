834402989559ab4ff74bc9b96a14c24ab216bbe7
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-09-15 12:19:02 -0700

    [xy] Filter valid clusters by cluster name.

--- mage_ai/cluster_manager/aws/emr_cluster_manager.py
+++ mage_ai/cluster_manager/aws/emr_cluster_manager.py
@@ -2,9 +2,12 @@ from mage_ai.cluster_manager.cluster_manager import ClusterManager
 from mage_ai.data_preparation.repo_manager import get_repo_path
 from mage_ai.services.aws.emr.emr import describe_cluster, list_clusters
 from mage_ai.services.aws.emr.launcher import create_cluster
+from mage_ai.shared.array import find
 from pathlib import Path
 import json
 import os
+
+CLUSTER_NAME = 'mage-data-prep'
 
 
 class EmrClusterManager(ClusterManager):
@@ -13,18 +16,27 @@ class EmrClusterManager(ClusterManager):
 
     def list_clusters(self):
         clusters = list_clusters()['Clusters']
+        valid_clusters = []
+        for c in clusters:
+            cluster_info = describe_cluster(c['Id'])
+            cluster_tags = cluster_info['Tags']
+            if find(
+                lambda t: t['Key'] == 'name' and t['Value'] == CLUSTER_NAME,
+                cluster_tags,
+            ):
+                valid_clusters.append(c)
         return [dict(
             id=c['Id'],
             name=c['Name'],
             status=c['Status']['State'],
             is_active=c['Id'] == self.active_cluster_id,
-        ) for c in clusters]
+        ) for c in valid_clusters]
 
     def create_cluster(self):
         create_cluster(
             get_repo_path(),
             done_status=None,
-            tags=dict(name='mage-data-prep'),
+            tags=dict(name=CLUSTER_NAME),
         )
 
     def set_active_cluster(self, auto_selection=False, cluster_id=None):
