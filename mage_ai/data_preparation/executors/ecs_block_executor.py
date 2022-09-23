04748c9b43fd981b8add8e3348b6970eebe05328
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-09-23 12:39:03 -0700

    [xy] Allow customizing ECS resource at block level.

--- mage_ai/data_preparation/executors/ecs_block_executor.py
+++ mage_ai/data_preparation/executors/ecs_block_executor.py
@@ -1,10 +1,17 @@
 from mage_ai.data_preparation.executors.block_executor import BlockExecutor
 from mage_ai.services.aws.ecs import ecs
+from mage_ai.shared.hash import merge_dict
 from requests import get
 from typing import Dict
 
 
 class EcsBlockExecutor(BlockExecutor):
+    def __init__(self, pipeline, block_uuid: str, execution_partition: str = None):
+        super().__init__(pipeline, block_uuid, execution_partition=execution_partition)
+        self.executor_config = self.pipeline.repo_config.ecs_config
+        if self.block.executor_config is not None:
+            self.executor_config = merge_dict(self.executor_config, self.block.executor_config)
+
     def execute(
         self,
         block_run_id: int = None,
