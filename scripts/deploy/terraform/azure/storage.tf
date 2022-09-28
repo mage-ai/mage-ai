a2f80b84a535927a84d88caf69b37c0c1709a356
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-09-28 10:45:05 -0700

    [xy] Make storage account name a variable.

--- scripts/deploy/terraform/azure/storage.tf
+++ scripts/deploy/terraform/azure/storage.tf
@@ -1,7 +1,7 @@
 # storage.tf | Shared Storage Configuration
 
 resource "azurerm_storage_account" "aci_storage" {
-  name                     = "magedataprepproduction"
+  name                     = "${var.storage_account_name}"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
