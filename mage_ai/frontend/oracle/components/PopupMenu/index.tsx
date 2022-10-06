5e719b8c134bcb39269207c74c5a838f4a5efdef
Johnson Kwok <johnson@mage.ai>
2022-10-06 10:50:48 -0700

    [jk] Left-align confirm question

--- mage_ai/frontend/oracle/components/PopupMenu/index.tsx
+++ mage_ai/frontend/oracle/components/PopupMenu/index.tsx
@@ -44,7 +44,7 @@ function PopupMenu({
     >
       <FlexContainer alignItems="center" flexDirection="column">
         <Spacing pb={1}>
-          <Text bold center large warning>
+          <Text bold large warning>
             {title}
           </Text>
         </Spacing>
