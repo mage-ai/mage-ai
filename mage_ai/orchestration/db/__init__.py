803800ea19734a555090dfe71b5d3811f6aeef82
David Yang <davidyang1996@gmail.com>
2022-10-13 15:49:24 -0700

    [dy] Initial commit

--- mage_ai/orchestration/db/__init__.py
+++ mage_ai/orchestration/db/__init__.py
@@ -2,13 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
-# TODO: Support Postgres
-if os.path.exists('mage_ai/orchestration/db/'):
-    DB_CONNECTION_URL = 'sqlite:///mage_ai/orchestration/db/mage-ai.db'
-else:
-    DB_CONNECTION_URL = 'sqlite:///mage-ai.db'
+from mage_ai.orchestration.constants import DATABASE_CONNECTION_URL_ENV_VAR
+ 
 
-engine = create_engine(DB_CONNECTION_URL)
+db_connection_url = os.getenv(DATABASE_CONNECTION_URL_ENV_VAR)
+
+if db_connection_url is None:
+    if os.path.exists('mage_ai/orchestration/db/'):
+        db_connection_url = 'sqlite:///mage_ai/orchestration/db/mage-ai.db'
+    else:
+        db_connection_url = 'sqlite:///mage-ai.db'
+
+engine = create_engine(db_connection_url)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
 session = Session()
