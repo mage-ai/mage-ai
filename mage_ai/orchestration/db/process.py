4fa2a63f28df9d7880c314a3249567f16e3d9901
David Yang <davidyang1996@gmail.com>
2022-10-19 10:20:19 -0700

    [dy] Update start_session_and_run (#1214)

--- mage_ai/orchestration/db/process.py
+++ mage_ai/orchestration/db/process.py
@@ -1,13 +1,20 @@
 import multiprocessing
 
+def start_session_and_run(*target_args):
+    from mage_ai.orchestration.db import db_connection
+
+    if len(target_args) == 0:
+        return None
+    target = target_args[0]
+    args = target_args[1:]
+
+    db_connection.start_session()
+
+    results = target(*args)
+
+    db_connection.close_session()
+    return results
+
 def create_process(target, args=()):
-    def start_session_and_run(*target_args):
-        from mage_ai.orchestration.db import db_connection
-        db_connection.start_session()
-
-        results = target(*target_args)
-
-        db_connection.close_session()
-        return results
-    
-    return multiprocessing.Process(target=start_session_and_run, args=args)
+    new_args = [target, *args]
+    return multiprocessing.Process(target=start_session_and_run, args=new_args)
