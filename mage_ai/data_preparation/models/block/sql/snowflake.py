7e3709b6ca28573cb9ce7b734da1dbad2a614f9c
DANGerous <quantumventuress@gmail.com>
2022-10-28 09:51:18 -0700

    [td] Cache SQL results if upstream differs in profile (#1272)
    * [td] Cache SQL results if upstream differs in profile
    
    * compare profile keys

--- mage_ai/data_preparation/models/block/sql/snowflake.py
+++ mage_ai/data_preparation/models/block/sql/snowflake.py
@@ -1,6 +1,10 @@
 from mage_ai.data_preparation.models.constants import BlockLanguage
 from mage_ai.data_preparation.variable_manager import get_variable
-from mage_ai.data_preparation.models.block.sql.utils.shared import interpolate_input
+from mage_ai.data_preparation.models.block.sql.utils.shared import (
+    interpolate_input,
+    should_cache_data_from_upstream,
+)
+from mage_ai.io.config import ConfigKey
 
 
 def create_upstream_block_tables(
@@ -13,8 +17,13 @@ def create_upstream_block_tables(
     schema = block.configuration.get('data_provider_schema')
 
     for idx, upstream_block in enumerate(block.upstream_blocks):
-        if BlockLanguage.SQL != upstream_block.language or \
-           data_provider != upstream_block.configuration.get('data_provider'):
+        if should_cache_data_from_upstream(block, upstream_block, [
+            'data_provider',
+        ], [
+            ConfigKey.SNOWFLAKE_ACCOUNT,
+            ConfigKey.SNOWFLAKE_DEFAULT_WH,
+            ConfigKey.SNOWFLAKE_DEFAULT_DB,
+        ]):
 
             df = get_variable(
                 upstream_block.pipeline.uuid,
--- mage_ai/data_preparation/models/block/sql/utils/shared.py
+++ mage_ai/data_preparation/models/block/sql/utils/shared.py
@@ -1,6 +1,32 @@
 from jinja2 import Template
 from mage_ai.data_preparation.models.constants import BlockLanguage
+from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from os import path
+from typing import List
 import re
+
+
+def should_cache_data_from_upstream(
+    block,
+    upstream_block,
+    config_keys: List[str],
+    config_profile_keys: List[str],
+) -> bool:
+    if BlockLanguage.SQL == block.language and BlockLanguage.SQL != upstream_block.language:
+        return True
+
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+
+    config1 = block.configuration or {}
+    config2 = upstream_block.configuration or {}
+
+    loader1 = ConfigFileLoader(config_path, config1.get('data_provider_profile'))
+    loader2 = ConfigFileLoader(config_path, config2.get('data_provider_profile'))
+
+    return not all([config1.get(k) == config2.get(k) for k in config_keys]) \
+        or not all([loader1.config.get(k) == loader2.config.get(k) for k in config_profile_keys])
+
 
 
 def interpolate_input(block, query, replace_func=None):
