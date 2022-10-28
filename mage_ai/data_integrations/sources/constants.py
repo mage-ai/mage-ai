d7e9186ad5fe97fd87f10292c6dd372e85f49c88
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-10-28 15:26:10 -0700

    [xy] Add postgres to UI.

--- mage_ai/data_integrations/sources/constants.py
+++ mage_ai/data_integrations/sources/constants.py
@@ -2,6 +2,7 @@ SOURCES = [
     dict(name='Amplitude'),
     dict(name='Google Search Console'),
     dict(name='MySQL'),
+    dict(name='PostgreSQL'),
     dict(name='Redshift'),
     dict(name='Salesforce'),
     dict(name='Stripe'),
