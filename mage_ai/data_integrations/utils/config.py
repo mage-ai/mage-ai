97818b1a0022d259e325935efac2634d3112f401
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2022-11-09 14:55:26 -0800

    [xy] Support variable n_days_ago. (#1340)

--- mage_ai/data_integrations/utils/config.py
+++ mage_ai/data_integrations/utils/config.py
@@ -1,4 +1,5 @@
 from jinja2 import Template
+from mage_ai.shared.dates import n_days_ago
 from mage_ai.shared.parsers import encode_complex
 from typing import Dict
 import os
@@ -35,7 +36,7 @@ def interpolate_variables(
     settings_string = Template(text).render(
         env_var=os.getenv,
         variables=lambda x: variables.get(x),
+        n_days_ago=n_days_ago,
     )
 
     return yaml.full_load(settings_string)
-
