ecf71f102336f94cc201c43e1744c67c689b02c7
David Yang <davidyang1996@gmail.com>
2022-11-21 18:00:15 -0800

    [dy] Support dbt docs server

---
+++ mage_ai/server/docs_manager.py
@@ -0,0 +1,41 @@
+from http.server import SimpleHTTPRequestHandler
+import multiprocessing
+import os
+import socketserver
+
+from mage_ai.orchestration.db.process import create_process
+from mage_ai.data_preparation.repo_manager import get_repo_path
+
+def get_dbt_target_path():
+    return os.path.join(get_repo_path(), 'dbt/target')
+
+def run_docs_server():
+    os.chdir(get_dbt_target_path())
+
+    httpd = socketserver.TCPServer(('', 7789), SimpleHTTPRequestHandler)
+
+    try:
+        print('Start docs server.')
+        httpd.serve_forever()
+    finally:
+        httpd.shutdown()
+        httpd.server_close()
+
+
+class DocsManager:
+    def __init__(self):
+        self.docs_process: multiprocessing.Process = None
+
+    @property
+    def is_alive(self):
+        return self.docs_process is not None and self.docs_process.is_alive()
+
+    def start_docs_server(self):
+        if self.is_alive:
+            return
+
+        proc = create_process(target=run_docs_server)
+        proc.start()
+        self.docs_process = proc
+
+docs_manager = DocsManager()
