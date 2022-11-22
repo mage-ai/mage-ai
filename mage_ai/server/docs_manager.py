66dda66971c18ea7bae9a65d495e4efda2df5a86
David Yang <davidyang1996@gmail.com>
2022-11-22 10:13:20 -0500

    [dy] Update

--- mage_ai/server/docs_manager.py
+++ mage_ai/server/docs_manager.py
@@ -2,6 +2,7 @@ from http.server import SimpleHTTPRequestHandler
 import multiprocessing
 import os
 import socketserver
+import time
 
 from mage_ai.orchestration.db.process import create_process
 from mage_ai.data_preparation.repo_manager import get_repo_path
@@ -10,16 +11,21 @@ def get_dbt_target_path():
     return os.path.join(get_repo_path(), 'dbt/target')
 
 def run_docs_server():
-    os.chdir(get_dbt_target_path())
+    while True:
+        print('Checking if DBT docs have been generated...')
+        if (os.path.exists(get_dbt_target_path())):
+            os.chdir(get_dbt_target_path())
 
-    httpd = socketserver.TCPServer(('', 7789), SimpleHTTPRequestHandler)
+            httpd = socketserver.TCPServer(('', 7789), SimpleHTTPRequestHandler)
 
-    try:
-        print('Start docs server.')
-        httpd.serve_forever()
-    finally:
-        httpd.shutdown()
-        httpd.server_close()
+            try:
+                print('Start DBT docs server.')
+                httpd.serve_forever()
+            finally:
+                httpd.shutdown()
+                httpd.server_close()
+        else:
+            time.sleep(10)
 
 
 class DocsManager:
