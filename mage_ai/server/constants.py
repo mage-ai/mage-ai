0604ac5a75f3d42f6eaedd0417983320e9f4983a
Tommy Dang <quantumventuress@gmail.com>
2022-12-15 21:14:49 -0800

    [td] Explicit pip install of mage-ai version

--- mage_ai/server/constants.py
+++ mage_ai/server/constants.py
@@ -7,4 +7,8 @@ DATA_PREP_SERVER_PORT = os.getenv('DATA_PREP_SERVER_PORT', 6789)
 
 DATAFRAME_OUTPUT_SAMPLE_COUNT = 10
 
-VERSION = '0.7.39'
+# The last line of this file must be the version number.
+# Dockerfile depends on it because it runs ./scripts/install_mage.sh and uses
+# the last line to determine the version to install.
+VERSION = \
+'0.7.39'
