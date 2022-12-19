e1684284dde5df7329e8080fc4ef99e9adf09c4e
Prashanth Reddy Munagala <prashanth.munagala.bolt.earth>
2022-12-19 21:34:14 +0530

    added teams webhook

---
+++ mage_ai/services/teams/teams.py
@@ -0,0 +1,17 @@
+from mage_ai.services.teams.config import TeamsConfig
+import json
+import requests
+
+
+def send_teams_message(config: TeamsConfig, message: str ,title = "Mage Ai Pipeline Run Status Logs",) -> None:
+    requests.post(
+        url = config.webhook_url,
+        json = {
+            "summary": title,
+            "sections": [{
+                "activityTitle": title,
+                "activitySubtitle": message
+            }],
+            
+        }
+    )
---
+++ tesr/data_loaders/load_titanic.py
@@ -0,0 +1,28 @@
+import io
+import pandas as pd
+import requests
+from pandas import DataFrame
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(**kwargs) -> DataFrame:
+    """
+    Template for loading data from API
+    """
+    url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
+
+    response = requests.get(url)
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(df) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert df is not None, 'The output is undefined'
