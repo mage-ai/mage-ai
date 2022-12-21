9f989083095c5d6293d19b43823674c2c2d87115
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-12-21 15:14:44 -0800

    [xy] Get gcp config from env vars.

--- mage_ai/data_preparation/executors/gcp_cloud_run_block_executor.py
+++ mage_ai/data_preparation/executors/gcp_cloud_run_block_executor.py
@@ -3,12 +3,17 @@ from mage_ai.services.gcp.cloud_run import cloud_run
 from mage_ai.shared.hash import merge_dict
 from requests import get
 from typing import Dict
+import os
 
 
 class GcpCloudRunBlockExecutor(BlockExecutor):
     def __init__(self, pipeline, block_uuid: str, execution_partition: str = None):
         super().__init__(pipeline, block_uuid, execution_partition=execution_partition)
         self.executor_config = self.pipeline.repo_config.gcp_cloud_run_config or dict()
+        if os.getenv('GCP_REGION'):
+            self.executor_config['region'] = os.getenv('GCP_REGION')
+        if os.getenv('GCP_PROJECT_ID'):
+            self.executor_config['project_id'] = os.getenv('GCP_PROJECT_ID')
         if self.block.executor_config is not None:
             self.executor_config = merge_dict(self.executor_config, self.block.executor_config)
 
