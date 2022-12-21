a9c26211ead0d73a0112bde8530bf4d0a78ab6fb
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-12-20 22:41:20 -0800

    [xy] Fix gcp cloud run config.

--- mage_ai/services/gcp/cloud_run/config.py
+++ mage_ai/services/gcp/cloud_run/config.py
@@ -1,15 +1,9 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from mage_ai.shared.config import BaseConfig
-from typing import Dict, List
-import boto3
-import os
-import requests
-
-ECS_CONTAINER_METADATA_URI_VAR = 'ECS_CONTAINER_METADATA_URI_V4'
 
 
 @dataclass
 class CloudRunConfig(BaseConfig):
     project_id: str
     path_to_credentials_json_file: str
-    region: str = 'us-west2',
+    region: str = 'us-west2'
