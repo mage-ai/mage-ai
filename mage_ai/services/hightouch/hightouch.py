3ce655f69db4d920b790566710ee409488a2cc89
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-12-22 14:53:13 -0800

    [xy] Implement sync_and_poll.

--- mage_ai/services/hightouch/hightouch.py
+++ mage_ai/services/hightouch/hightouch.py
@@ -1,9 +1,17 @@
+from datetime import datetime, timedelta
 from mage_ai.services.hightouch.config import HightouchConfig
-from typing import Dict, Union
+from mage_ai.services.hightouch.constants import (
+    HIGHTOUCH_BASE_URL,
+    DEFAULT_POLL_INTERVAL,
+    PENDING_STATUSES,
+    SUCCESS,
+    TERMINAL_STATUSES,
+    WARNING,
+)
+from typing import Dict, Optional, Union
 import json
 import requests
-
-HIGHTOUCH_BASE_URL = 'https://api.hightouch.com/api/v1'
+import time
 
 
 class HightouchClient:
@@ -31,16 +39,66 @@ class HightouchClient:
     def get_sync(self, sync_id: int):
         return self.make_request(f'/syncs/{sync_id}')
 
-    def list_sync_runs(self, sync_id: int):
-        return self.make_request(f'/syncs/{sync_id}/runs')
+    def list_sync_runs(self, sync_id: int, params: Dict = dict()):
+        return self.make_request(f'/syncs/{sync_id}/runs', params=params)
 
     def trigger_sync(self, sync_id: int, payload: Dict = dict(fullResync=False)):
         return self.make_request(f'/syncs/{sync_id}/trigger', method='POST', payload=payload)
 
-    def sync_and_poll(self):
-        pass
+    def sync_and_poll(
+        self,
+        sync_id: int,
+        payload: Dict = dict(fullResync=False),
+        poll_interval: float = DEFAULT_POLL_INTERVAL,
+        poll_timeout: Optional[float] = None,
+    ):
+        trigger_response = self.trigger_sync(sync_id, payload=payload)
+        run_id = trigger_response.get('id')
+        if not run_id:
+            raise Exception(f'Failed to trigger Hightouch sync {sync_id}')
+        poll_start = datetime.now()
+        while True:
+            sync_run = self.list_sync_runs(sync_id, dict(runId=run_id))['data'][0]
+            print(
+                f"Polling Hightouch Sync {sync_id}. Current status: {sync_run['status']}. "
+                f"{100 * sync_run.get('completionRatio', 0)}% completed."
+            )
 
-    def make_request(self, url_path: str, method: str = 'GET', payload: Dict = dict()):
+            if sync_run['status'] in TERMINAL_STATUSES:
+                print(f"Sync request status: {sync_run['status']}. Polling complete")
+                if sync_run['error']:
+                    print(f"Sync Request Error: {sync_run['error']}")
+
+                if sync_run['status'] == SUCCESS:
+                    break
+                raise Exception(
+                    f"Sync {sync_id} for request: {run_id} failed with status: "
+                    f"{sync_run['error']} and error:  {sync_run['error']}"
+                )
+            if sync_run['status'] not in PENDING_STATUSES:
+                print(
+                    f"Unexpected status: {sync_run['status']} returned for sync {sync_id} and "
+                    f"request {run_id}. Will try again, but if you see this error, please let "
+                    "someone at Hightouch know.",
+                )
+            if (
+                poll_timeout
+                and datetime.now()
+                > poll_start + timedelta(seconds=poll_timeout)
+            ):
+                raise Exception(
+                    f"Sync {sync_id} for run: {run_id} time out after "
+                    f"{datetime.now() - poll_start}. Last status was {sync_run['status']}."
+                )
+            time.sleep(poll_interval)
+
+    def make_request(
+        self,
+        url_path: str,
+        method: str = 'GET',
+        params: Dict = dict(),
+        payload: Dict = dict()
+    ):
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.config.api_key}',
@@ -50,6 +108,8 @@ class HightouchClient:
         if method == 'GET':
             response = requests.get(
                 url,
+                headers=headers,
+                params=params,
             )
         elif method == 'POST':
             response = requests.post(
