7011221439649043ec0da1a5811994092c60c7a9
Tommy Dang <quantumventuress@gmail.com>
2022-12-29 12:16:05 -0800

    [td] Update docs URL in transformer template code

--- mage_ai/data_preparation/templates/transformers/transformer_actions/column/clean_column_name.py
+++ mage_ai/data_preparation/templates/transformers/transformer_actions/column/clean_column_name.py
@@ -3,7 +3,7 @@
     """
     Execute Transformer Action: ActionType.CLEAN_COLUMN_NAME
 
-    Docs: https://github.com/mage-ai/mage-ai/blob/master/docs/actions/transformer_actions/README.md#clean-column-names
+    Docs: https://docs.mage.ai/guides/transformer-blocks#clean-column-names
     """
     action = build_transformer_action(
         df,
