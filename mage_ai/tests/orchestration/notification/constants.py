ff2ed78df2327cd983b02d5b6f5eaf1f6989a712
Prashanth Reddy Munagala <prashanth.munagala.bolt.earth>
2022-12-29 10:52:27 +0530

    added team config

--- mage_ai/tests/orchestration/notification/constants.py
+++ mage_ai/tests/orchestration/notification/constants.py
@@ -17,6 +17,6 @@ SLACK_NOTIFICATION_CONFIG = dict(
 
 TEAMS_NOTIFICATION_CONFIG = dict(
     teams_config=dict(
-        webhook_url='https://revosauto.webhook.office.com/webhookb2/58ad38d7-78e0-4f71-b486-4b1bd9d6766a@38cb47ac-2bbf-436d-a973-3336374b629c/IncomingWebhook/44ef4a958c724032b253632d35dc6fa4/4f6cc58b-9ef4-462b-83a5-bef6b78cc1d5',
+        webhook_url='test_webhook_url'
     )
 )
