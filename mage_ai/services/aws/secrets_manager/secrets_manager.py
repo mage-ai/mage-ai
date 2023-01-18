cb92e7dff94a1191d8c54d9777d70be4885b8cce
David Yang <davidyang1996@gmail.com>
2023-01-17 17:44:42 -0800

    [dy] Update imports

--- mage_ai/services/aws/secrets_manager/secrets_manager.py
+++ mage_ai/services/aws/secrets_manager/secrets_manager.py
@@ -1,24 +1,32 @@
-from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
-from botocore.config import Config
-import boto3
 import os
 
 
-region_name = os.getenv('AWS_REGION_NAME', 'us-west-2')
-config = Config(region_name=region_name)
-client = boto3.client('secretsmanager', config=config)
-cache_config = SecretCacheConfig()
-cache = SecretCache(config=cache_config, client=client)
+class SecretsManager:
+    def __init__(self):
+        from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+        from botocore.config import Config
+        import boto3
+
+        region_name = os.getenv('AWS_REGION_NAME', 'us-west-2')
+        config = Config(region_name=region_name)
+        client = boto3.client('secretsmanager', config=config)
+        cache_config = SecretCacheConfig()
+        self.cache = SecretCache(config=cache_config, client=client)
+
+secrets_manager = SecretsManager()
 
 
 def get_secret(secret_id: str, cached=True) -> str:
     if cached:
-        return cache.get_secret_string(secret_id)
+        return secrets_manager.cache.get_secret_string(secret_id)
     else:
         return get_secret_force(secret_id)
 
 
 def get_secret_force(secret_id: str) -> str:
+    from botocore.config import Config
+    import boto3
+    
     region_name = os.getenv('AWS_REGION_NAME', 'us-west-2')
     config = Config(region_name=region_name)
     client = boto3.client('secretsmanager', config=config)
