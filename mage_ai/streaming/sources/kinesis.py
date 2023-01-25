adccd1d3cc16be06360e1d08aaf549befb970044
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-01-25 12:02:39 -0800

    [xy] Support reading from multiple shards and checkpoint for Kinesis (#1829)
    * [xy] Support multiple shards.
    
    * [xy] Support checkpoint in kinesis consumer.

--- mage_ai/streaming/sources/kinesis.py
+++ mage_ai/streaming/sources/kinesis.py
@@ -35,27 +35,60 @@ class KinesisSource(BaseSource):
             batch_size = self.config.batch_size
         else:
             batch_size = DEFAULT_BATCH_SIZE
-        try:
-            response = self.kinesis_client.get_shard_iterator(
-                StreamName=self.config.stream_name,
-                ShardId=self.details['Shards'][0]['ShardId'],
-                ShardIteratorType='TRIM_HORIZON',
 
-            )
-            shard_iter = response['ShardIterator']
-            while True:
-                response = self.kinesis_client.get_records(
-                    ShardIterator=shard_iter,
-                    Limit=batch_size,
+        try:
+            shard_iterators = dict()
+            for shard_id in self.details['Shards']:
+                shard_id = shard_id['ShardId']
+                iterator_kwargs = dict(
+                    ShardIteratorType='LATEST',
                 )
-                shard_iter = response['NextShardIterator']
-                records = response['Records']
-                if records:
-                    self._print(f'Got {len(records)} records. Sample: {records[0]}')
-                    handler([json.loads(r['Data'].decode('utf-8')) for r in records])
+                if self.checkpoint is not None and self.checkpoint.get(shard_id):
+                    iterator_kwargs = dict(
+                        ShardIteratorType='AFTER_SEQUENCE_NUMBER',
+                        StartingSequenceNumber=self.checkpoint.get(shard_id),
+                    )
+                self._print(f'Shard {shard_id} args: {iterator_kwargs}')
+                shard_iterator = self.kinesis_client.get_shard_iterator(
+                    StreamName=self.config.stream_name,
+                    ShardId=shard_id,
+                    **iterator_kwargs,
+                )
+                shard_iterators[shard_id] = shard_iterator['ShardIterator']
+            self._print(f'Consuming messages from shards: {list(shard_iterators.keys())}')
+            closed_streams = set()
+            while True:
+                for shard_id, shard_iter in shard_iterators.items():
+                    if shard_id in closed_streams:
+                        continue
+                    response = self.kinesis_client.get_records(
+                        ShardIterator=shard_iter,
+                        Limit=batch_size,
+                    )
+                    if 'NextShardIterator' not in response:
+                        self._print(f'Shards {shard_id} is closed.')
+                        closed_streams.add(shard_id)
+                        continue
+                    shard_iterators[shard_id] = response['NextShardIterator']
+                    records = response['Records']
+                    if records:
+                        self._print(f'Got {len(records)} records from shard {shard_id}. '
+                                    f'Sample: {records[0]}')
+                        handler([json.loads(r['Data'].decode('utf-8')) for r in records])
+                        max_seq_number = max(r['SequenceNumber'] for r in records)
+                        self.update_checkpoint(shard_id, max_seq_number)
+                if len(closed_streams) == len(shard_iterators):
+                    self._print(f'All shards {list(shard_iterators.keys())} are closed.')
+                    break
         except Exception:
             self._print(f'Couldn\'t get records from stream {self.config.stream_name}.')
             raise
 
-    def test_connection(self):
-        return True
+    def update_checkpoint(self, shard_id, sequence_number):
+        if self.checkpoint is None:
+            self.checkpoint = {}
+        if not self.checkpoint.get(shard_id):
+            self.checkpoint[shard_id] = sequence_number
+        elif sequence_number > self.checkpoint.get(shard_id):
+            self.checkpoint[shard_id] = sequence_number
+        super().update_checkpoint()
