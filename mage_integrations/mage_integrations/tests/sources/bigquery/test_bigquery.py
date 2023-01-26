169a503a8d62a4e28e200543e543d419037dcf83
Johnson Kwok <johnson@mage.ai>
2023-01-26 00:37:04 -0800

    [jk] Add unit test for bigquery source

---
+++ mage_integrations/mage_integrations/tests/sources/bigquery/test_bigquery.py
@@ -0,0 +1,137 @@
+from google.cloud.bigquery import Row
+from mage_integrations.sources.bigquery import BigQuery
+from unittest.mock import MagicMock, patch
+import os
+import unittest
+
+
+def build_sample_bigquery_rows():
+    return [
+        Row(
+            ('demo_users', 'NULL', None, 'id', 'STRING(100)', 'NO'),
+            {
+                'table_name': 0,
+                'column_default': 1,
+                'column_key': 2,
+                'column_name': 3,
+                'data_type': 4,
+                'is_nullable': 5,
+            },
+        ),
+        Row(
+            ('demo_users', 'NULL', None, 'first_name', 'STRING(100)', 'YES'),
+            {
+                'table_name': 0,
+                'column_default': 1,
+                'column_key': 2,
+                'column_name': 3,
+                'data_type': 4,
+                'is_nullable': 5,
+            },
+        ),
+        Row(
+            ('demo_users', 'NULL', None, 'last_name', 'STRING(100)', 'YES'),
+            {
+                'table_name': 0,
+                'column_default': 1,
+                'column_key': 2,
+                'column_name': 3,
+                'data_type': 4,
+                'is_nullable': 5,
+            },
+        ),
+        Row(
+            ('demo_users', 'NULL', None, 'age', 'INT64', 'YES'),
+            {
+                'table_name': 0,
+                'column_default': 1,
+                'column_key': 2,
+                'column_name': 3,
+                'data_type': 4,
+                'is_nullable': 5,
+            },
+        ),
+        Row(
+            ('demo_users', 'NULL', None, 'color', 'STRING(30)', 'YES'),
+            {
+                'table_name': 0,
+                'column_default': 1,
+                'column_key': 2,
+                'column_name': 3,
+                'data_type': 4,
+                'is_nullable': 5,
+            },
+        ),
+    ]
+
+class BigQuerySourceTests(unittest.TestCase):
+    def test_discover(self):
+        source = BigQuery(config=dict(project_id="mage_test_project"))
+        bigquery_connection = MagicMock()
+        bigquery_connection.load.return_value = build_sample_bigquery_rows()
+        with patch.object(source, 'build_discover_query') as mock_build_query:
+            with patch.object(
+                source,
+                'build_connection',
+                return_value=bigquery_connection,
+            ) as mock_build_connection:
+                    catalog = source.discover()
+                    mock_build_query.assert_called_once()
+                    mock_build_connection.assert_called_once()
+                    self.assertEqual(
+                        catalog.to_dict(),
+                        {
+                            'streams': [
+                                {
+                                    'tap_stream_id': 'demo_users',
+                                    'replication_method': 'FULL_TABLE',
+                                    'key_properties': [],
+                                    'schema': {
+                                        'properties': {
+                                            'id': {'type': ['string']},
+                                            'first_name': {'type': ['null', 'string']},
+                                            'last_name': {'type': ['null', 'string']},
+                                            'age': {'type': ['null', 'integer']},
+                                            'color': {'type': ['null', 'string']},
+                                        },
+                                        'type': 'object',
+                                    },
+                                    'stream': 'demo_users',
+                                    'metadata': [
+                                        {
+                                            'breadcrumb': (),
+                                            'metadata': {
+                                                'table-key-properties': [],
+                                                'forced-replication-method': 'FULL_TABLE',
+                                                'valid-replication-keys': [],
+                                                'inclusion': 'available',
+                                                'schema-name': 'demo_users',
+                                            },
+                                        },
+                                        {
+                                            'breadcrumb': ('properties', 'id'),
+                                            'metadata': {'inclusion': 'available'},
+                                        },
+                                        {
+                                            'breadcrumb': ('properties', 'first_name'),
+                                            'metadata': {'inclusion': 'available'},
+                                        },
+                                        {
+                                            'breadcrumb': ('properties', 'last_name'),
+                                            'metadata': {'inclusion': 'available'},
+                                        },
+                                        {
+                                            'breadcrumb': ('properties', 'age'),
+                                            'metadata': {'inclusion': 'available'},
+                                        },
+                                        {
+                                            'breadcrumb': ('properties', 'color'),
+                                            'metadata': {'inclusion': 'available'},
+                                        },
+                                    ],
+                                    'auto_add_new_fields': False,
+                                    'unique_conflict_method': 'UPDATE',
+                                },
+                            ]
+                        },
+                    )
