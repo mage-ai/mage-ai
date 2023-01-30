b111e6c5810706d189e800bdf0cf118a2324c885
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-01-30 11:41:19 -0800

    [xy] Include UNIQUE column into unique constraints.

--- mage_integrations/mage_integrations/sources/sql/base.py
+++ mage_integrations/mage_integrations/sources/sql/base.py
@@ -66,7 +66,7 @@ class Source(BaseSource):
                 column_properties = None
                 column_types = []
 
-                if column_key is not None and 'PRI' in column_key:
+                if column_key is not None and ('PRI' in column_key or 'UNIQUE' == column_key):
                     unique_constraints.append(column_name)
 
                 if 'YES' == is_nullable:
