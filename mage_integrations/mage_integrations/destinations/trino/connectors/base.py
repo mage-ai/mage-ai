76f2615526e28c004bb91e04762bb7fdfe7e726d
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-01-30 18:25:07 -0800

    [xy] Enforce unique constraints for Trino when inserting rows. (#1886)
    * [xy] Enforce unique constraints for Trino when inserting rows.
    
    * [xy] Execute create schema command first.
    
    * [xy] Fix merge command.
    
    * [xy] Limit the usage of merging rows.

--- mage_integrations/mage_integrations/destinations/trino/connectors/base.py
+++ mage_integrations/mage_integrations/destinations/trino/connectors/base.py
@@ -1,4 +1,7 @@
 from mage_integrations.connections.trino import Trino as TrinoConnection
+from mage_integrations.destinations.constants import (
+    UNIQUE_CONFLICT_METHOD_UPDATE,
+)
 from mage_integrations.destinations.sql.base import Destination
 from mage_integrations.destinations.sql.utils import (
     build_alter_table_command,
@@ -10,6 +13,14 @@ from mage_integrations.destinations.sql.utils import (
 from mage_integrations.destinations.trino.utils import convert_column_type, convert_json_or_string
 from typing import Dict, List, Tuple
 import json
+
+
+# https://trino.io/docs/current/develop/supporting-merge.html
+MERGEABLE_CONNECTORS = [
+    'blackhole',
+    'delta-lake',
+    'iceberg',
+]
 
 
 class TrinoConnector(Destination):
@@ -83,6 +94,9 @@ DESCRIBE {schema_name}.{table_name}
         unique_conflict_method: str = None,
         unique_constraints: List[str] = None,
     ) -> List[str]:
+        full_table_name = f'{schema_name}.{table_name}'
+        full_table_name_temp = f'{schema_name}.temp_{table_name}'
+
         columns = list(schema['properties'].keys())
         insert_columns, insert_values = build_insert_command(
             column_type_mapping=self.column_type_mapping(schema),
@@ -93,6 +107,48 @@ DESCRIBE {schema_name}.{table_name}
         )
         insert_columns = ', '.join(insert_columns)
         insert_values = ', '.join(insert_values)
+
+        if self._support_merge_rows() and unique_constraints and unique_conflict_method:
+            drop_temp_table_command = f'DROP TABLE IF EXISTS {full_table_name_temp}'
+            commands = [
+                drop_temp_table_command,
+            ] + self.build_create_table_commands(
+                schema=schema,
+                schema_name=schema_name,
+                stream=None,
+                table_name=f'temp_{table_name}',
+                database_name=database_name,
+                unique_constraints=unique_constraints,
+            ) + self.wrap_insert_commands([
+                f'INSERT INTO {full_table_name_temp} ({insert_columns})',
+                f'VALUES {insert_values}',
+            ])
+
+            unique_constraints_clean = [clean_column_name(col) for col in unique_constraints]
+            columns_cleaned = [clean_column_name(col) for col in columns]
+
+            merge_commands = [
+                f'MERGE INTO {full_table_name} AS a',
+                f'USING (SELECT * FROM {full_table_name_temp}) AS b',
+                f"ON {', '.join([f'a.{col} = b.{col}' for col in unique_constraints_clean])}",
+            ]
+
+            if UNIQUE_CONFLICT_METHOD_UPDATE == unique_conflict_method:
+                set_command = ', '.join(
+                    [f'{col} = b.{col}' for col in columns_cleaned],
+                )
+                merge_commands.append(f'WHEN MATCHED THEN UPDATE SET {set_command}')
+
+            merge_values = f"({', '.join([f'b.{col}' for col in columns_cleaned])})"
+            merge_commands.append(
+                f"WHEN NOT MATCHED THEN INSERT ({insert_columns}) VALUES {merge_values}",
+            )
+            merge_command = '\n'.join(merge_commands)
+
+            return commands + [
+                merge_command,
+                drop_temp_table_command,
+            ]
 
         commands = [
             f'INSERT INTO {schema_name}.{table_name} ({insert_columns})',
@@ -151,3 +207,6 @@ DESCRIBE {schema_name}.{table_name}
         return [
             commands_string,
         ]
+
+    def _support_merge_rows(self):
+        return self.config['connector'] in MERGEABLE_CONNECTORS
