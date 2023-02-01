4ccb25a8bf84eae8fc30ef9e4e7e43412dc3cf96
Tommy Dang <quantumventuress@gmail.com>
2023-01-31 21:28:22 -0800

    [td] Create users and oauth2 tables

--- mage_ai/orchestration/db/models.py
+++ mage_ai/orchestration/db/models.py
@@ -119,6 +119,53 @@ pipeline_schedule_event_matcher_association_table = Table(
 )
 
 
+class User(BaseModel):
+    avatar = Column(String(255), default=None)
+    email = Column(String(255), default=None)
+    first_name = Column(String(255), default=None)
+    last_name = Column(String(255), default=None)
+    owner = Column(Boolean, default=False)
+    password_hash = Column(String(255), default=None)
+    password_salt = Column(String(255), default=None)
+    roles = Column(Integer, default=None)
+    username = Column(String(255), default=None)
+
+    oauth2_applications = relationship('Oauth2Application', back_populates='user')
+    oauth2_access_tokens = relationship('Oauth2AccessToken', back_populates='user')
+
+
+class Oauth2Application(BaseModel):
+    class AuthorizationGrantType(str, enum.Enum):
+        AUTHORIZATION_CODE = 'authorization-code'
+        CLIENT_CREDENTIALS = 'client-credentials'
+
+    class ClientType(str, enum.Enum):
+        PRIVATE = 'private'
+        PUBLIC = 'public'
+
+    authorization_grant_type = Column(
+        Enum(AuthorizationGrantType),
+        default=AuthorizationGrantType.AUTHORIZATION_CODE,
+    )
+    client_id = Column(String(255))
+    client_type = Column(Enum(ClientType), default=ClientType.PRIVATE)
+    name = Column(String(255))
+    redirect_uris = Column(String(255), default=None)
+    user = relationship(User, back_populates='oauth2_applications')
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    oauth2_access_tokens = relationship('Oauth2AccessToken', back_populates='oauth2_application')
+
+
+class Oauth2AccessToken(BaseModel):
+    expires = Column(DateTime(timezone=True))
+    oauth2_application = relationship(Oauth2Application, back_populates='oauth2_access_tokens')
+    oauth2_application_id = Column(Integer, ForeignKey('oauth2_application.id'))
+    token = Column(String(255))
+    user = relationship(User, back_populates='oauth2_access_tokens')
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
 class PipelineSchedule(BaseModel):
     class ScheduleStatus(str, enum.Enum):
         ACTIVE = 'active'
