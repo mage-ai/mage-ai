7b3fcee37dc5ce146c66fcccb4155648e4ea734b
jlondonob <j.londonobo@gmail.com>
2023-02-01 21:18:11 -0500

    Update tests

--- mage_ai/tests/cli/test_start.py
+++ mage_ai/tests/cli/test_start.py
@@ -12,23 +12,23 @@ class StartTests(TestCase):
     def tests_start_with_no_arguments(self, mock_start_sever):
         result = runner.invoke(app, ['start'])
         mock_start_sever.assert_called_once_with(
-            'localhost',
-            '6789',
-            os.getcwd(),
-            False,
-            False
+            host='localhost',
+            port='6789',
+            project=os.getcwd(),
+            manage=False,
+            dbt_docs=False
         )
         assert result.exit_code == 0
         assert 'Mage is running at http://' in result.output
 
-    def test_with_host(self, mock_start_sever):
+    def test_start_with_host(self, mock_start_sever):
         result = runner.invoke(app, ['start', '--host', '127.0.0.1'])
         mock_start_sever.assert_called_once_with(
-            '127.0.0.1',
-            '6789',
-            os.getcwd(),
-            False,
-            False
+            host='127.0.0.1',
+            port='6789',
+            project=os.getcwd(),
+            manage=False,
+            dbt_docs=False
         )
         assert result.exit_code == 0
         assert 'Mage is running at http://127.0.0.1:6789' in result.output
@@ -36,11 +36,11 @@ class StartTests(TestCase):
     def test_start_with_port(self, mock_start_sever):
         result = runner.invoke(app, ['start', '--port', '8000'])
         mock_start_sever.assert_called_once_with(
-            'localhost',
-            '8000',
-            os.getcwd(),
-            False,
-            False
+            host='localhost',
+            port='8000',
+            project=os.getcwd(),
+            manage=False,
+            dbt_docs=False
         )
         assert result.exit_code == 0
         assert 'Mage is running at http://localhost:8000' in result.output
@@ -50,11 +50,11 @@ class StartTests(TestCase):
         mock_abspath.side_effect = lambda x: x
         result = runner.invoke(app, ['start', 'my_mage_project'])
         mock_start_sever.assert_called_once_with(
-            'localhost',
-            '6789',
-            'my_mage_project',
-            False,
-            False
+            host='localhost',
+            port='6789',
+            project='my_mage_project',
+            manage=False,
+            dbt_docs=False
         )
         assert result.exit_code == 0
         assert ('Mage is running at http://localhost:6789'
