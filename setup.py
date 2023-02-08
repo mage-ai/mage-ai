fa7697f6596f9e748e7bfbb8d25938f531516e1b
Tommy Dang <quantumventuress@gmail.com>
2023-02-07 19:17:52 -0800

    fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     name='mage-ai',
     # NOTE: when you change this, change the value of VERSION in the following file:
     # mage_ai/server/constants.py
-    version='0.7.90',
+    version='0.7.91',
     author='Mage',
     author_email='eng@mage.ai',
     description='Mage is a tool for building and deploying data pipelines.',
