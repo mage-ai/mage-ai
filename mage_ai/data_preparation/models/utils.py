ec4ddd41c5d51e84b61a56dad5566eb5527c68f6
Tommy Dang <quantumventuress@gmail.com>
2023-02-08 16:58:50 -0800

    fix lint

--- mage_ai/data_preparation/models/utils.py
+++ mage_ai/data_preparation/models/utils.py
@@ -1,6 +1,7 @@
 from mage_integrations.utils.parsers import encode_complex
 from typing import Dict
 import dask.dataframe as dd
+import pandas as pd
 import simplejson
 
 MAX_PARTITION_BYTE_SIZE = 100 * 1024 * 1024
@@ -11,7 +12,7 @@ JSON_SERIALIZABLE_COLUMN_TYPES = [
 ]
 
 
-def serialize_columns(row: 'pd.Series', column_types: Dict) -> 'pd.Series':
+def serialize_columns(row: pd.Series, column_types: Dict) -> pd.Series:
     for column, column_type in column_types.items():
         if column_type not in JSON_SERIALIZABLE_COLUMN_TYPES:
             continue
@@ -28,7 +29,7 @@ def serialize_columns(row: 'pd.Series', column_types: Dict) -> 'pd.Series':
     return row
 
 
-def deserialize_columns(row: 'pd.Series', column_types: Dict) -> 'pd.Series':
+def deserialize_columns(row: pd.Series, column_types: Dict) -> pd.Series:
     for column, column_type in column_types.items():
         if column_type not in JSON_SERIALIZABLE_COLUMN_TYPES:
             continue
@@ -40,7 +41,7 @@ def deserialize_columns(row: 'pd.Series', column_types: Dict) -> 'pd.Series':
     return row
 
 
-def dask_from_pandas(df: 'pd.DataFrame') -> 'dask.dataframe':
+def dask_from_pandas(df: pd.DataFrame) -> dd:
     ddf = dd.from_pandas(df, npartitions=1)
     npartitions = 1 + ddf.memory_usage(deep=True).sum().compute() // MAX_PARTITION_BYTE_SIZE
     ddf = ddf.repartition(npartitions=npartitions)
@@ -48,6 +49,6 @@ def dask_from_pandas(df: 'pd.DataFrame') -> 'dask.dataframe':
     return ddf
 
 
-def apply_transform(ddf: 'dask.dataframe', apply_function) -> 'dask.dataframe':
+def apply_transform(ddf: dd, apply_function) -> dd:
     res = ddf.apply(apply_function, axis=1, meta=ddf)
     return res.compute()
