a1c5a50bf138afdbd7ad1fcdb86f44695ea77ec1
Johnson Kwok <johnson@mage.ai>
2023-02-17 11:20:53 -0800

    [jk] Add Terminal icons

---
+++ mage_ai/frontend/oracle/icons/custom/TerminalGradient.tsx
@@ -0,0 +1,37 @@
+function TerminalGradient({ size }: { size: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_5247_222548)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4 0C1.79086 0 0 1.79086 0 4V12C0 14.2091 1.79086 16 4 16H12C14.2091 16 16 14.2091 16 12V4C16 1.79086 14.2091 0 12 0H4ZM3.18976 3.86413C3.43051 3.52707 3.89893 3.449 4.23599 3.68976L7.73599 6.18976C7.93308 6.33054 8.05006 6.55784 8.05006 6.80006C8.05006 7.04227 7.93308 7.26957 7.73599 7.41036L4.23599 9.91036C3.89893 10.1511 3.43051 10.073 3.18976 9.73599C2.949 9.39893 3.02707 8.93051 3.36413 8.68976L6.00971 6.80006L3.36413 4.91036C3.02707 4.6696 2.949 4.20119 3.18976 3.86413ZM8.05 9.3C8.05 8.88579 8.38579 8.55 8.8 8.55H12.3C12.7142 8.55 13.05 8.88579 13.05 9.3C13.05 9.71421 12.7142 10.05 12.3 10.05H8.8C8.38579 10.05 8.05 9.71421 8.05 9.3Z"
+          fill="url(#paint0_linear_1244_32583)"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12 1H4C2.34315 1 1 2.34315 1 4V12C1 13.6569 2.34315 15 4 15H12C13.6569 15 15 13.6569 15 12V4C15 2.34315 13.6569 1 12 1ZM4 0C1.79086 0 0 1.79086 0 4V12C0 14.2091 1.79086 16 4 16H12C14.2091 16 16 14.2091 16 12V4C16 1.79086 14.2091 0 12 0H4Z"
+          fill="url(#paint0_linear_1244_32583"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5247_222548">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+        <linearGradient id="paint0_linear_1244_32583" x1="1.07031" y1="11.8041" x2="21.9428" y2="11.8041" gradientUnits="userSpaceOnUse">
+          <stop offset="0.28125" stopColor="#7D55EC"/>
+          <stop offset="1" stopColor="#2AB2FE"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default TerminalGradient;
