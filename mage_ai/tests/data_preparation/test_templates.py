3cab652ece06a405a7714deed7cdc442713ba8b0
Johnson Kwok <johnson@mage.ai>
2023-02-22 00:47:07 -0800

    [jk] Update unit tests

--- mage_ai/tests/data_preparation/test_templates.py
+++ mage_ai/tests/data_preparation/test_templates.py
@@ -36,8 +36,6 @@ class TemplateTest(TestCase):
         expected_string = """from mage_ai.data_cleaner.transformer_actions.base import BaseAction
 from mage_ai.data_cleaner.transformer_actions.constants import ActionType, Axis
 from mage_ai.data_cleaner.transformer_actions.utils import build_transformer_action
-from pandas import DataFrame
-
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
@@ -220,8 +218,8 @@ def load_data_from_api(*args, **kwargs):
     Template for loading data from API
     \"\"\"
     url = ''
-
     response = requests.get(url)
+
     return pd.read_csv(io.StringIO(response.text), sep=',')
 
 
@@ -268,16 +266,14 @@ consumer_group: unique_consumer_group
         self.assertEqual(kafka_template, new_kafka_template)
 
     def test_template_generation_transformer_default(self):
-        expected_template = """from pandas import DataFrame
-
-if 'transformer' not in globals():
+        expected_template = """if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
 
 @transformer
-def transform_df(df: DataFrame, *args, **kwargs) -> DataFrame:
+def transform(*args, **kwargs):
     \"\"\"
     Template code for a transformer block.
 
@@ -285,60 +281,14 @@ def transform_df(df: DataFrame, *args, **kwargs) -> DataFrame:
     There should be one parameter for each output variable from each parent block.
 
     Args:
-        df (DataFrame): Data frame from parent block.
+        df (optional): Data frame from parent block.
 
     Returns:
-        DataFrame: Transformed data frame
+        Anything (e.g. df)
     \"\"\"
     # Specify your transformation logic here
 
-    return df
-
-
-@test
-def test_output(df, *args) -> None:
-    \"\"\"
-    Template code for testing the output of the block.
-    \"\"\"
-    assert df is not None, 'The output is undefined'
-"""
-
-        config1 = {'action_type': 'custom'}
-        config2 = {'axis': 'row'}
-        config3 = {}
-        new_template1 = fetch_template_source(BlockType.TRANSFORMER, config1)
-        new_template2 = fetch_template_source(BlockType.TRANSFORMER, config2)
-        new_template3 = fetch_template_source(BlockType.TRANSFORMER, config3)
-        self.assertEqual(expected_template, new_template1)
-        self.assertEqual(expected_template, new_template2)
-        self.assertEqual(expected_template, new_template3)
-
-    def test_template_generation_transformer_action_default(self):
-        expected_template = """from pandas import DataFrame
-
-if 'transformer' not in globals():
-    from mage_ai.data_preparation.decorators import transformer
-if 'test' not in globals():
-    from mage_ai.data_preparation.decorators import test
-
-
-@transformer
-def transform_df(df: DataFrame, *args, **kwargs) -> DataFrame:
-    \"\"\"
-    Template code for a transformer block.
-
-    Add more parameters to this function if this block has multiple parent blocks.
-    There should be one parameter for each output variable from each parent block.
-
-    Args:
-        df (DataFrame): Data frame from parent block.
-
-    Returns:
-        DataFrame: Transformed data frame
-    \"\"\"
-    # Specify your transformation logic here
-
-    return df
+    return {}
 
 
 @test
@@ -531,19 +481,17 @@ def test_output(df, *args) -> None:
         self.assertEqual(expected_template, new_template)
 
     def test_template_generation_data_exporter_default(self):
-        expected_template = """from pandas import DataFrame
-
-if 'data_exporter' not in globals():
+        expected_template = """if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
 @data_exporter
-def export_data(df: DataFrame, **kwargs):
+def export_data(*args, **kwargs):
     \"\"\"
     Exports data to some source
 
     Args:
-        df (DataFrame): Data frame to export to
+        df (optional): Data frame to export to
 
     Output (optional):
         Optionally return any object and it'll be logged and
