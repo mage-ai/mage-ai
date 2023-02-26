972fd4b33844fad2e6484836963b59d619b3de3c
Tommy Dang <quantumventuress@gmail.com>
2023-02-26 15:37:28 -0800

    add has_callback

--- mage_ai/api/presenters/BlockPresenter.py
+++ mage_ai/api/presenters/BlockPresenter.py
@@ -14,6 +14,7 @@ class BlockPresenter(BasePresenter):
         'downstream_blocks',
         'executor_config',
         'executor_type',
+        'has_callback',
         'language',
         'metadata',
         'name',
