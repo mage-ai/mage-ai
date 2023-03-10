1e93d4c6f0551a3c3ae2c33a1855858aa22c03bd
David Yang <david@mage.ai>
2023-03-10 11:43:18 -0800

    Revert "[dy] Overwrite origin url (#2174)"
    This reverts commit 159b459d2fc5a234f9bd75a9c10f2db1eaf6268b.

--- mage_ai/data_preparation/sync/git_sync.py
+++ mage_ai/data_preparation/sync/git_sync.py
@@ -2,7 +2,6 @@ from mage_ai.data_preparation.sync import SyncConfig
 from mage_ai.data_preparation.sync.base_sync import BaseSync
 from mage_ai.shared.logger import VerboseFunctionExec
 import git
-import subprocess
 
 
 class GitSync(BaseSync):
@@ -14,12 +13,7 @@ class GitSync(BaseSync):
             self.repo = git.Repo(self.repo_path)
         except git.exc.InvalidGitRepositoryError:
             self.repo = git.Repo.init(self.repo_path)
-
-        try:
             self.repo.create_remote('origin', self.remote_repo_link)
-        except git.exc.GitCommandError:
-            # if the remote already exists
-            self.repo.remotes.origin.set_url(self.remote_repo_link)
 
         self.origin = self.repo.remotes.origin
 
@@ -28,6 +22,5 @@ class GitSync(BaseSync):
             f'Syncing data with remote repo {self.remote_repo_link}',
             verbose=True,
         ):
-            # use subprocess because the gitpython command wasn't timing out correctly
-            subprocess.run(['git', 'fetch', self.origin.name], timeout=30)
-            self.repo.git.reset('--hard', f'{self.origin.name}/{self.branch}')
+            self.origin.fetch(kill_after_timeout=60)
+            self.repo.git.reset('--hard', f'origin/{self.branch}')
