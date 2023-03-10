ff0893c07cc1ed049317d3f644e859a298ee2425
Dhia Gharsallaoui <safedhia@gmail.com>
2023-03-10 14:03:47 +0100

    [dg] Support LDAP

---
+++ mage_ai/authentication/ldap.py
@@ -0,0 +1,106 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+from ldap3 import Server, Connection
+from ldap3.core.exceptions import LDAPException
+from mage_ai.settings import (
+    LDAP_SERVER,
+    LDAP_BIND_DN,
+    LDAP_BIND_PASSWORD,
+    LDAP_BASE_DN,
+    LDAP_AUTHENTICATATION_FILTER,
+    LDAP_AUTORIZATION_FILTER,
+)
+
+
+class LDAPAuthenticator(ABC):
+    @abstractmethod
+    def authenticate(self, username: str, password: str) -> Tuple[bool, str]:
+        pass
+
+    @abstractmethod
+    def authorize(self, username: str) -> bool:
+        pass
+
+    def verify(self, username: str, password: str) -> bool:
+        authenticated, user_dn = self.authenticate(username, password)
+        if authenticated:
+            return self.authorize(user_dn)
+        return False
+
+
+class LDAPConnection(LDAPAuthenticator):
+    def __init__(
+        self,
+        server_url,
+        bind_dn,
+        bind_password,
+        base_dn,
+        authentication_filter,
+        authorization_filter,
+    ):
+        self.server_url = server_url
+        self.bind_dn = bind_dn
+        self.bind_password = bind_password
+        self.base_dn = base_dn
+        self.authentication_filter = authentication_filter
+        self.authorization_filter = authorization_filter
+        self.conn = None
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.unbind()
+
+    def bind(self):
+        self.conn = Connection(
+            Server(self.server_url), self.bind_dn, self.bind_password, auto_bind=True
+        )
+
+    def authenticate(self, username: str, password: str) -> Tuple[bool, str]:
+        try:
+            if not self.conn:
+                self.bind()
+            self.conn.search(
+                self.base_dn, self.authentication_filter.format(username=username)
+            )
+            if not self.conn.entries:
+                return False, ""
+            user_dn = self.conn.entries[0].entry_dn
+            if self.conn.rebind(user=user_dn, password=password):
+                return True, user_dn
+            return False, user_dn
+        except LDAPException:
+            return False, ""
+
+    def authorize(self, user_dn: str) -> bool:
+        try:
+            if not self.conn:
+                self.bind()
+            self.conn.entries.clear()
+            self.conn.search(self.base_dn, self.authorization_filter.format(user_dn=user_dn))
+            if self.conn.entries:
+                if len(self.conn.entries) > 0:
+                    return True
+            return False
+        except LDAPException:
+            return False
+
+
+def new_ldap_connection() -> LDAPConnection:
+    return LDAPConnection(
+        LDAP_SERVER,
+        LDAP_BIND_DN,
+        LDAP_BIND_PASSWORD,
+        LDAP_BASE_DN,
+        LDAP_AUTHENTICATATION_FILTER,
+        LDAP_AUTORIZATION_FILTER,
+    )
+
+
+def verify(username, password: str) -> bool:
+    return LDAPConnection(
+        LDAP_SERVER,
+        LDAP_BIND_DN,
+        LDAP_BIND_PASSWORD,
+        LDAP_BASE_DN,
+        LDAP_AUTHENTICATATION_FILTER,
+        LDAP_AUTORIZATION_FILTER,
+    ).verify(username, password)
--- mage_ai/tests/api/operations/test_sessions.py
+++ mage_ai/tests/api/operations/test_sessions.py
@@ -2,9 +2,11 @@ from datetime import datetime
 from freezegun import freeze_time
 from mage_ai.api.operations import constants
 from mage_ai.authentication.oauth2 import encode_token
-from mage_ai.orchestration.db.models import Oauth2AccessToken
+from mage_ai.authentication.ldap import LDAPConnection
+from mage_ai.orchestration.db.models import User, Oauth2AccessToken
 from mage_ai.tests.api.operations.base import BaseApiTestCase
 from mage_ai.tests.factory import create_user
+from unittest.mock import patch
 
 
 class SessionOperationTests(BaseApiTestCase):
@@ -44,4 +46,72 @@ class SessionOperationTests(BaseApiTestCase):
         )
         response = await operation.execute()
 
+        self.assertIsNotNone(response['error'])
+
+    @patch('mage_ai.api.resources.SessionResource.AUTHENTIFICATION_MODE', 'ldap')
+    @patch.object(LDAPConnection, 'authorize')
+    @patch.object(LDAPConnection, 'authenticate')
+    async def test_ldap_login(self, mock_authenticate, mock_authorize):
+        mock_authenticate.return_value = (True, "Julius_Novachrono")
+        mock_authorize.return_value = True
+
+        username = "novachrono"
+        operation = self.build_operation(
+            action=constants.CREATE,
+            payload=dict(session=dict(
+                email=username,
+                password="Wizard King",
+            )),
+            resource='sessions',
+        )
+        await operation.execute()
+
+        mock_authenticate.assert_called_once_with("novachrono", "Wizard King")
+        mock_authorize.assert_called_once_with("Julius_Novachrono")
+        self.assertIsNotNone(User.query.filter(User.username == username).first())
+
+    @patch('mage_ai.api.resources.SessionResource.AUTHENTIFICATION_MODE', 'ldap')
+    @patch.object(LDAPConnection, 'authorize')
+    @patch.object(LDAPConnection, 'authenticate')
+    async def test_ldap_login_unauthenticated(self, mock_authenticate, mock_authorize):
+        mock_authenticate.return_value = (False, "")
+        mock_authorize.return_value = False
+
+        username = "licht"
+        operation = self.build_operation(
+            action=constants.CREATE,
+            payload=dict(session=dict(
+                email=username,
+                password="Golden Dawn",
+            )),
+            resource='sessions',
+        )
+        response = await operation.execute()
+
+        mock_authenticate.assert_called_once_with("licht", "Golden Dawn")
+        mock_authorize.assert_not_called()
+        self.assertIsNone(User.query.filter(User.username == username).first())
+        self.assertIsNotNone(response['error'])
+
+    @patch('mage_ai.api.resources.SessionResource.AUTHENTIFICATION_MODE', 'ldap')
+    @patch.object(LDAPConnection, 'authorize')
+    @patch.object(LDAPConnection, 'authenticate')
+    async def test_ldap_login_unauthorized(self, mock_authenticate, mock_authorize):
+        mock_authenticate.return_value = (True, "Yami_Sukehiro")
+        mock_authorize.return_value = False
+
+        username = "yami"
+        operation = self.build_operation(
+            action=constants.CREATE,
+            payload=dict(session=dict(
+                email=username,
+                password="black bull",
+            )),
+            resource='sessions',
+        )
+        response = await operation.execute()
+
+        mock_authenticate.assert_called_once_with("yami", "black bull")
+        mock_authorize.assert_called_once_with("Yami_Sukehiro")
+        self.assertIsNone(User.query.filter(User.username == username).first())
         self.assertIsNotNone(response['error'])
