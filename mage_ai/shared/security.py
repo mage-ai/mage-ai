84bf71d0279b493b0e32bec7e9a56174ca6e65a9
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-03-16 01:34:30 -0700

    [xy] Whitelist certain env vars in terminal output.

--- mage_ai/shared/security.py
+++ mage_ai/shared/security.py
@@ -1,11 +1,18 @@
 import os
 
-MIN_SECRET_ENV_VAR_LENGTH = 5
+MIN_SECRET_ENV_VAR_LENGTH = 8
+WHITELISTED_ENV_VARS = set([
+    'MAGE_REPO_PATH',
+    'HOME',
+    'PWD',
+])
 
 
 def filter_out_env_var_values(value: str):
     env_var_values = dict(os.environ).values()
-    env_var_values = [v for v in env_var_values if v and len(v) >= MIN_SECRET_ENV_VAR_LENGTH]
+    env_var_values = [v for v in env_var_values
+                      if v and len(v) >= MIN_SECRET_ENV_VAR_LENGTH
+                      and v not in WHITELISTED_ENV_VARS]
     env_var_values.sort(key=len, reverse=True)
     value_clean = value
     for env_var_value in env_var_values:
