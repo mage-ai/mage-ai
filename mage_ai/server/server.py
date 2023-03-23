a15417e4dffac464185382a5ab0e5ec5e04b2ad7
bruno-hl <bruno@homelight.com>
2023-03-22 23:31:18 -0300

    Automatically open app in a new tab using default browser

--- mage_ai/server/server.py
+++ mage_ai/server/server.py
@@ -61,6 +61,7 @@ import json
 import os
 import tornado.ioloop
 import tornado.web
+import webbrowser
 
 
 class MainPageHandler(tornado.web.RequestHandler):
@@ -268,7 +269,8 @@ async def main(
         address=host if host != 'localhost' else None,
     )
 
-    print(f'Mage is running at http://{host or "localhost"}:{port} and serving project {project}')
+    webbrowser.open_new_tab(url := f'http://{host or "localhost"}:{port}')
+    print(f'Mage is running at {url} and serving project {project}')
 
     db_connection.start_session(force=True)
 
