ed1407de21d633403412468cc926a98488da133e
David Yang <david@mage.ai>
2023-04-03 16:14:40 -0700

    [dy] Add tests

--- mage_ai/data_preparation/sync/git_sync.py
+++ mage_ai/data_preparation/sync/git_sync.py
@@ -2,7 +2,6 @@ from mage_ai.data_preparation.git import Git
 from mage_ai.data_preparation.sync import GitConfig
 from mage_ai.data_preparation.sync.base_sync import BaseSync
 from mage_ai.shared.logger import VerboseFunctionExec
-import asyncio
 
 
 class GitSync(BaseSync):
