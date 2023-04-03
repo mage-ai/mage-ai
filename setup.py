b7577f5712006d33ad694d51d22ae84d9479f640
Tommy Dang <quantumventuress@gmail.com>
2023-04-02 20:29:33 -0700

    [td] 0.8.35

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     name='mage-ai',
     # NOTE: when you change this, change the value of VERSION in the following file:
     # mage_ai/server/constants.py
-    version='0.8.34',
+    version='0.8.35',
     author='Mage',
     author_email='eng@mage.ai',
     description='Mage is a tool for building and deploying data pipelines.',
