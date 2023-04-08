f3202bbc2ccd7d1952ab7e972dc7dae5962a5df1
Johnson Kwok <johnson@mage.ai>
2023-04-07 17:33:26 -0700

    [jk] Remove unused import

--- mage_ai/frontend/components/IntegrationPipeline/SelectStreams/index.tsx
+++ mage_ai/frontend/components/IntegrationPipeline/SelectStreams/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 
 import Button from '@oracle/elements/Button';
 import Checkbox from '@oracle/elements/Checkbox';
