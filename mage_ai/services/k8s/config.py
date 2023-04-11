393227ccf10477186c0be0560127e21180bd674c
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-04-11 10:55:50 -0700

    [xy] Add k8s config file.

---
+++ mage_ai/services/k8s/config.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from mage_ai.shared.config import BaseConfig
+from typing import Dict
+import traceback
+
+ECS_CONTAINER_METADATA_URI_VAR = 'ECS_CONTAINER_METADATA_URI_V4'
+
+
+@dataclass
+class K8sResourceConfig(BaseConfig):
+    cpu: str
+    memory: str
+
+
+@dataclass
+class K8sExecutorConfig(BaseConfig):
+    resource_limits: K8sResourceConfig
+    resource_requests: K8sResourceConfig
+
+    @classmethod
+    def load(self, config_path: str = None, config: Dict = None):
+        executor_config = super().load(config_path=config_path, config=config)
+        if executor_config.resource_limits is not None and \
+                type(executor_config.resource_limits) is dict:
+            try:
+                executor_config.resource_limits = K8sResourceConfig.load(
+                    config=executor_config.resource_limits,
+                )
+            except Exception:
+                traceback.print_exc()
+                executor_config.resource_limits = None
+        if executor_config.resource_requests is not None and \
+                type(executor_config.resource_requests) is dict:
+            try:
+                executor_config.resource_requests = K8sResourceConfig.load(
+                    config=executor_config.resource_requests,
+                )
+            except Exception:
+                traceback.print_exc()
+                executor_config.resource_requests = None
+        return executor_config
