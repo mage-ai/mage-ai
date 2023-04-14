641e3b2a284de295e75d3d93152dc5616e5236fc
Johnson Kwok <johnson@mage.ai>
2023-04-14 00:54:01 -0700

    [jk] Add comment

--- mage_ai/frontend/components/Logs/Toolbar/constants.ts
+++ mage_ai/frontend/components/Logs/Toolbar/constants.ts
@@ -3,6 +3,18 @@ import { LogRangeEnum } from '@interfaces/LogType';
 export const LIMIT_PARAM = '_limit';
 export const OFFSET_PARAM = '_offset';
 export const LOG_ITEMS_PER_PAGE = 100;
+
+/*
+ * LOG_OFFSET_INTERVAL is the default number of hour log groupings fetched.
+ * (e.g. If user fetches a day's worth of logs, and there are logs written
+ * during each hour that day, there would be 2 batches of logs fetched, each
+ * batch containing 12 hour groupings of logs.)
+ *
+ * Since streaming pipelines would be expected to have continuous logs while
+ * they are running, the log grouping interval is lower than for other types
+ * of pipelines. Increase the numbers below to increase the amount of logs fetched
+ * at a time.
+*/
 export const LOG_OFFSET_INTERVAL = 12;
 export const LOG_STREAM_OFFSET_INTERVAL = 1;
 
