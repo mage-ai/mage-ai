bbe6ee57a4b3dd24f7cc7daf5c767fd744746c62
dy46 <david@mage.ai>
2023-04-19 16:56:12 -0700

    [dy] Add kernel stats

--- mage_ai/frontend/interfaces/KernelType.ts
+++ mage_ai/frontend/interfaces/KernelType.ts
@@ -4,6 +4,12 @@ export default interface KernelType {
   alive: boolean;
   id: string;
   name: string;
+  usage: {
+    host_virtual_memory: any;
+    kernel_cpu: number;
+    kernel_memory: number;
+    pid: number;
+  }
 }
 
 export type SetMessagesType = {
