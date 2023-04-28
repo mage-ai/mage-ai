568cc7a3aa9215aac8bf92a8b1ec3f474582c697
Henry Wu <csharplus@gmail.com>
2023-04-28 00:50:38 -0700

    [hw] Implement subscription creation related logics

--- mage_ai/streaming/sources/google_cloud_pubsub.py
+++ mage_ai/streaming/sources/google_cloud_pubsub.py
@@ -21,46 +21,76 @@ class GoogleCloudPubSubConfig(BaseConfig):
 class GoogleCloudPubSubSource(BaseSource):
     config_class = GoogleCloudPubSubConfig
 
+    def _exist_subscription(self, project_id: str) -> bool:
+        project_path = f"projects/{project_id}"
+        subscriptions = self.subscriber_client.list_subscriptions(
+            request={"project": project_path}
+        )
+        for subscription in subscriptions:
+            if subscription.name == self.subscription_path:
+                return True
+        return False
+
+    def _create_subscription(
+            self, project_id: str, topic_id: str, subscription_id: str) -> None:
+        """Create a new pull subscription on the given topic."""
+        # [START pubsub_create_pull_subscription]
+        if self._exist_subscription(project_id):
+            self._print(f"Subscription already exists: {self.subscription_path}")
+            return
+
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(project_id, topic_id)
+        subscription = self.subscriber_client.create_subscription(
+            request={"name": self.subscription_path, "topic": topic_path}
+        )
+        self._print(f"Subscription created: {subscription}")
+        # [END pubsub_create_pull_subscription]
+
     def init_client(self) -> None:
         if self.config.pubsub_emulator_host is not None:
             os.environ["PUBSUB_EMULATOR_HOST"] = self.config.pubsub_emulator_host
         self.subscriber_client = pubsub_v1.SubscriberClient()
+        self.subscription_path = self.subscriber_client.subscription_path(
+            self.config.project_id, self.config.subscription_id)
+        self._create_subscription(
+            self.config.project_id,
+            self.config.topic_id,
+            self.config.subscription_id)
 
     def read(self, handler: Callable) -> None:
-        self._print('Start consuming messages.')
+        self._print("Start consuming messages.")
 
-        def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-            self._print(f"Received Google cloud pubsub_v1 message: {message}.")
-            handler(dict(data=message.data.decode()))
-            message.ack()
+        def callback(
+                received_message: pubsub_v1.subscriber.message.Message) -> None:
+            self._print(f"Received: {received_message}.")
+            handler(dict(data=received_message.message.data.decode()))
+            self._print(f"Handled: {received_message.message.data}.")
+            received_message.ack()
 
         with self.subscriber_client:
-            subscription_path = self.subscriber_client.subscription_path(
-                self.config.project_id, self.config.subscription_id)
             streaming_pull_future = self.subscriber_client.subscribe(
-                subscription_path, callback=callback)
+                self.subscription_path, callback=callback)
 
             try:
                 # When `timeout` is not set, result() will block indefinitely,
                 # unless an exception is encountered first.
-                self._print('Start receiving message with timeout: {self.config.timeout}')
+                self._print("Start receiving message with timeout: {self.config.timeout}")
                 streaming_pull_future.result(timeout=self.config.timeout)
             except TimeoutError:
                 streaming_pull_future.cancel()  # Trigger the shutdown.
                 streaming_pull_future.result()  # Block until the shutdown is complete.
 
     def batch_read(self, handler: Callable) -> None:
-        self._print('Start consuming messages.')
+        self._print("Start consuming messages.")
         if self.config.batch_size > 0:
             batch_size = self.config.batch_size
         else:
             batch_size = DEFAULT_BATCH_SIZE
 
         with self.subscriber_client:
-            subscription_path = self.subscriber_client.subscription_path(
-                self.config.project_id, self.config.subscription_id)
             response = self.subscriber_client.pull(
-                request={"subscription": subscription_path, "max_messages": batch_size},
+                request={"subscription": self.subscription_path, "max_messages": batch_size},
                 retry=retry.Retry(deadline=300),
             )
 
@@ -68,7 +98,7 @@ class GoogleCloudPubSubSource(BaseSource):
                 return
 
             ack_ids = []
-            self._print(f"Total number of received messages: {len(response.received_messages)}")
+            self._print(f"Number of received messages: {len(response.received_messages)}")
             for received_message in response.received_messages:
                 self._print(f"Received: {received_message.message.data}.")
                 handler(dict(data=received_message.message.data.decode()))
@@ -77,10 +107,10 @@ class GoogleCloudPubSubSource(BaseSource):
 
             # Acknowledges the received messages so they will not be sent again.
             self.subscriber_client.acknowledge(
-                request={"subscription": subscription_path, "ack_ids": ack_ids}
+                request={"subscription": self.subscription_path, "ack_ids": ack_ids}
             )
 
             self._print(
                 f"Received and acknowledged {len(response.received_messages)}"
-                f" messages from {subscription_path}."
+                f" messages from {self.subscription_path}."
             )
