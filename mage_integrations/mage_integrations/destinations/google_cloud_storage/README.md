e9c75894a1c9775068ae835415a7fdbcadf4e692
André Ventura <62493271+andreventura02@users.noreply.github.com>
2023-04-30 03:40:53 -0300

    [av] Add GCS destination to data integration pipeline. (#2535)
    * Config template for gcs destination
    
    * Add core for gcs destination
    
    * Add README
    
    * Add a new requirement
    
    * Add destination to the UI
    
    * Add credential key in README
    
    * Alter google credentials key in  build_client function and add one empty line
    
    * Add an empty line at the end of json
    
    * Version change

---
+++ mage_integrations/mage_integrations/destinations/google_cloud_storage/README.md
@@ -0,0 +1,19 @@
+# Google Cloud Storage
+
+![]()
+
+<br />
+
+## Config
+
+You must enter the following credentials when configuring this source:
+
+| Key | Description | Sample value
+| --- | --- | --- |
+| `bucket` | Name of the Google Cloud Storage bucket to save data in. | `user_generated_content` |
+| `google_application_credentials` | Path of where is credential in json file | `/path/to/your/service/account/key.json`
+| `file_type` | The type of GCS files. Supported file type values: `parquet`, `csv`. | `parquet` or `csv` |
+| `object_key_path` | The path of the location where you have files. Don’t include the `gs`, the bucket name, or the table name in this path value.  | `users/ds/20221225` |
+| `date_partition_format` | (Optional) The datetime format of the partition. If it's null, files will not be saved into paratitions. | `null`, `%Y%m%d`, `%Y%m%dT%H` |
+
+<br />
---
+++ mage_integrations/mage_integrations/destinations/google_cloud_storage/__init__.py
@@ -0,0 +1,93 @@
+from google.cloud.storage import Client
+from mage_integrations.destinations.base import Destination
+from mage_integrations.destinations.utils import update_record_with_internal_columns
+from datetime import datetime, timezone
+from io import BytesIO
+from typing import Dict, List
+import argparse
+import os
+import pandas as pd
+import sys
+
+
+class GoogleCloudStorage(Destination):
+    @property
+    def bucket(self) -> str:
+        return self.config['bucket']
+    
+    @property
+    def file_type(self) -> str:
+        return self.config.get('file_type')
+    
+    @property
+    def object_key_path(self) -> str:
+        return self.config['object_key_path']
+    
+    def build_client(self) -> Client:
+        return Client.from_service_account_json(
+            json_credentials_path=self.config['google_application_credentials']
+        )
+        
+    def export_batch_data(self, record_data: List[Dict], stream: str) -> None:
+        client = self.build_client()
+
+        table_name = self.config.get('table')
+
+        tags = dict(
+            records=len(record_data),
+            stream=stream,
+            table_name=table_name,
+        )
+
+        self.logger.info('Export data started', tags=tags)
+
+        # Add _mage_created_at and _mage_updated_at columns
+        for r in record_data:
+            r['record'] = update_record_with_internal_columns(r['record'])
+
+        df = pd.DataFrame([d['record'] for d in record_data])
+
+        buffer = BytesIO()
+        if self.file_type == 'parquet':
+            df.to_parquet(buffer)
+        elif self.file_type == 'csv':
+            df.to_csv(buffer, index=False)
+        else:
+            raise Exception(f'File type {self.file_type} is not supported.')
+
+        buffer.seek(0)
+
+        curr_time = datetime.now(timezone.utc)
+
+        filename = curr_time.strftime('%Y%m%d-%H%M%S')
+        filename = f'{filename}.{self.file_type}'
+
+        object_key = os.path.join(self.object_key_path, table_name)
+
+        date_partition_format = self.config.get('date_partition_format')
+        if date_partition_format:
+            object_key = os.path.join(object_key, curr_time.strftime(date_partition_format))
+
+        object_key = os.path.join(object_key, filename)
+        
+        bucket = client.bucket(self.bucket)
+        blob = bucket.blob(object_key)
+        blob.upload_from_file(buffer)
+        
+        tags.update(
+            records_inserted=len(record_data),
+        )
+
+        self.logger.info('Export data completed.', tags=tags)
+
+    def test_connection(self) -> None:
+        client = self.build_client()
+        client.get_bucket(self.bucket)
+
+
+if __name__ == '__main__':
+    destination = GoogleCloudStorage(
+        argument_parser=argparse.ArgumentParser(),
+        batch_processing=True,
+    )
+    destination.process(sys.stdin.buffer)
