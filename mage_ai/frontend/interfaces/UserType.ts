e3015552b0069fbe07d9d3ca3578edf0d9e531d1
Johnson Kwok <johnson@mage.ai>
2023-05-01 11:40:00 -0700

    [jk] Set users with owner role

--- mage_ai/frontend/interfaces/UserType.ts
+++ mage_ai/frontend/interfaces/UserType.ts
@@ -1,12 +1,14 @@
 export enum RoleValueEnum {
   ADMIN = 1,
   EDITOR = 2,
+  OWNER = 'owner',
   VIEWER = 4,
 }
 
 export const ROLE_DISPLAY_MAPPING = {
   [RoleValueEnum.ADMIN]: 'Admin',
   [RoleValueEnum.EDITOR]: 'Editor',
+  [RoleValueEnum.OWNER]: 'Owner',
   [RoleValueEnum.VIEWER]: 'Viewer',
 };
 
