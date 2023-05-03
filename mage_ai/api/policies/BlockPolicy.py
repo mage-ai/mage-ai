31c252ba13813993200d2c04d0cc6483fb880ade
dy46 <david@mage.ai>
2023-05-03 13:45:42 -0700

    [dy] Add allow query

--- mage_ai/api/policies/BlockPolicy.py
+++ mage_ai/api/policies/BlockPolicy.py
@@ -114,3 +114,11 @@ BlockPolicy.allow_query([
     constants.DELETE,
     constants.UPDATE,
 ], condition=lambda policy: policy.has_at_least_viewer_role())
+
+BlockPolicy.allow_query([
+    'update_state',
+], scopes=[
+    OauthScope.CLIENT_PRIVATE,
+], on_action=[
+    constants.UPDATE,
+], condition=lambda policy: policy.has_at_least_editor_role_and_pipeline_edit_access())
