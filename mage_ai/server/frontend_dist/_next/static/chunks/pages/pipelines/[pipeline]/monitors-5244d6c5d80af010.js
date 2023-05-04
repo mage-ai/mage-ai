0de1c9d7b5e9df4f8477ed88eeeaedcd01c33262
Circle CI <ci@mage.ai>
2023-05-04 06:53:10 +0000

    [CI] Update production build

---
+++ mage_ai/server/frontend_dist/_next/static/chunks/pages/pipelines/[pipeline]/monitors-5244d6c5d80af010.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7713],{89677:function(n,_,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/pipelines/[pipeline]/monitors",function(){return i(67400)}])}},function(n){n.O(0,[844,7607,5896,2714,1424,1005,547,9129,9774,2888,179],(function(){return _=89677,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
