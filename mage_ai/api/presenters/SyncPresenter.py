de76c473fc1312806449757e2aea4de21f998f9d
dy46 <davidyang1996@gmail.com>
2023-05-05 10:36:23 -0700

    [dy] Save cwd as repo path if empty

--- mage_ai/api/presenters/SyncPresenter.py
+++ mage_ai/api/presenters/SyncPresenter.py
@@ -1,5 +1,4 @@
 from mage_ai.api.presenters.BasePresenter import BasePresenter
-import os
 
 
 class SyncPresenter(BasePresenter):
@@ -16,9 +15,3 @@ class SyncPresenter(BasePresenter):
         'access_token',
         'auth_type',
     ]
-
-    def present(self, **kwargs):
-        sync_config = self.model
-        if not sync_config.get('repo_path', None):
-            sync_config['repo_path'] = os.getcwd()
-        return sync_config
