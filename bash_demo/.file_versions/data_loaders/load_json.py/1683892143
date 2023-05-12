518d428003fcbea9a1b00ac754dce1d8528e7b99
israarhamid <israarhamid03@gmail.com>
2023-05-12 13:57:35 +0200

    Add test blocks

---
+++ bash_demo/.file_versions/data_loaders/load_json.py/1683892143
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from mage_ai.io.file import FileIO
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_file(*args, **kwargs):
+    """
+    Template for loading data from filesystem.
+    Load data from 1 file or multiple file directories.
+
+    For multiple directories, use the following:
+        FileIO().load(file_directories=['dir_1', 'dir_2'])
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    filepath = 'path/to/your/file.csv'
+
+    return FileIO().load(filepath)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
---
+++ bash_demo/transformers/json_to_df.py
@@ -0,0 +1,34 @@
+from mage_ai.data_cleaner.transformer_actions.base import BaseAction
+from mage_ai.data_cleaner.transformer_actions.constants import ActionType, Axis
+from mage_ai.data_cleaner.transformer_actions.utils import build_transformer_action
+from pandas import DataFrame
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def execute_transformer_action(df: DataFrame, *args, **kwargs) -> DataFrame:
+    """
+    Execute Transformer Action: ActionType.NORMALIZE
+
+    Docs: https://docs.mage.ai/guides/transformer-blocks#normalize-data
+    """
+    action = build_transformer_action(
+        df,
+        action_type=ActionType.NORMALIZE,
+        arguments=[],  # Specify columns to normalize
+        axis=Axis.COLUMN,
+    )
+
+    return BaseAction(action).execute(df)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
