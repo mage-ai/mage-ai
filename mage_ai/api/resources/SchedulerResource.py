e924af23607517d342ad7046b1efb45f31cd8d1a
dy46 <david@mage.ai>
2023-05-15 09:36:57 -0700

    clean

--- mage_ai/api/resources/SchedulerResource.py
+++ mage_ai/api/resources/SchedulerResource.py
@@ -1,25 +1,8 @@
 from mage_ai.api.resources.GenericResource import GenericResource
-from mage_ai.data_preparation.preferences import (
-    get_preferences,
-    Preferences,
-)
-from mage_ai.data_preparation.shared.secrets import create_secret
-from mage_ai.data_preparation.sync import (
-    GitConfig,
-    GIT_ACCESS_TOKEN_SECRET_NAME,
-    GIT_SSH_PRIVATE_KEY_SECRET_NAME,
-    GIT_SSH_PUBLIC_KEY_SECRET_NAME,
-)
-from mage_ai.data_preparation.sync.git_sync import GitSync
 from mage_ai.orchestration.db import safe_db_query
-from mage_ai.orchestration.db.models.oauth import User
-from mage_ai.orchestration.db.models.secrets import Secret
 from mage_ai.server.scheduler_manager import (
-    SCHEDULER_AUTO_RESTART_INTERVAL,
-    check_scheduler_status,
     scheduler_manager,
 )
-import os
 
 
 class SchedulerResource(GenericResource):
