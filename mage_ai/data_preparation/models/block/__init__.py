aceb3b23d7bc2c6535a847b3f8c71aefe39a3150
Henry Wu <csharplus@gmail.com>
2023-05-23 18:52:28 -0700

    [hw] Update the Spark user guide with a Custom Spark Session section

--- mage_ai/data_preparation/models/block/__init__.py
+++ mage_ai/data_preparation/models/block/__init__.py
@@ -21,6 +21,7 @@ from mage_ai.data_preparation.models.block.extension.utils import handle_run_tes
 from mage_ai.data_preparation.models.block.utils import (
     clean_name,
     fetch_input_variables,
+    get_spark_session,
     input_variables,
     is_dynamic_block,
     is_dynamic_block_child,
@@ -1786,35 +1787,7 @@ df = get_variable('{self.pipeline.uuid}', '{block_uuid}', 'df')
         if self.spark_init:
             return self.spark
         try:
-            from pyspark.conf import SparkConf
-            from pyspark.sql import SparkSession
-
-            repo_config = RepoConfig(repo_path=self.repo_path)
-            spark_config = repo_config.spark_config
-
-            if spark_config:
-                conf = SparkConf()
-                if spark_config.get('app_name'):
-                    conf.setAppName(spark_config.get('app_name'))
-                if spark_config.get('spark_master'):
-                    conf.setMaster(spark_config.get('spark_master'))
-                if spark_config.get('spark_home'):
-                    conf.setSparkHome(spark_config.get('spark_home'))
-                if spark_config.get('executor_env'):
-                    list_kv_pairs = []
-                    for key, value in spark_config.get('executor_env').items():
-                        list_kv_pairs.append((key, value))
-                    conf.setExecutorEnv(key=None, value=None, pairs=list_kv_pairs)
-                if spark_config.get('spark_jars'):
-                    conf.set('spark.jars', ','.join(spark_config.get('spark_jars')))
-                if spark_config.get('others'):
-                    conf.setAll(spark_config.get('others'))
-
-                self.spark = SparkSession.builder.config(
-                    conf=conf).getOrCreate()
-            else:
-                self.spark = SparkSession.builder.master(
-                    os.getenv('SPARK_MASTER_HOST', 'local')).getOrCreate()
+            self.spark = get_spark_session()
         except Exception:
             self.spark = None
 
