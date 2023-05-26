b25736e4ef13e7d8cf40cded8df3bc076a775950
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-05-25 17:59:42 -0700

    [xy] Fix config.

--- mage_ai/data_preparation/shared/retry.py
+++ mage_ai/data_preparation/shared/retry.py
@@ -2,7 +2,7 @@ from mage_ai.shared.config import BaseConfig
 
 
 class RetryConfig(BaseConfig):
-    retries: int = 0,
-    delay: int = 5,
-    max_delay: int = 60,
-    exponential_backoff: bool = True,
+    retries: int = 0
+    delay: int = 5
+    max_delay: int = 60
+    exponential_backoff: bool = True
