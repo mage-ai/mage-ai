caa27a27c6e2bed6cc1f1193f291fe0cebe0db17
dy46 <david@mage.ai>
2023-05-30 18:45:24 -1000

    [dy] Fix tests again

--- mage_ai/api/presenters/UserPresenter.py
+++ mage_ai/api/presenters/UserPresenter.py
@@ -1,5 +1,6 @@
 from mage_ai.api.operations import constants
 from mage_ai.api.presenters.BasePresenter import BasePresenter
+from mage_ai.shared.hash import extract
 
 
 class UserPresenter(BasePresenter):
@@ -19,6 +20,7 @@ class UserPresenter(BasePresenter):
 
     def present(self, **kwargs):
         data = self.model.to_dict(include_attributes=self.default_attributes)
+        data = extract(data, self.default_attributes, include_blank_values=True)
 
         roles_new = self.model.roles_new
         data['roles_new'] = [
