878c5ea3340113dfba08ee78e32ed68f904ed8cd
Henry Wu <csharplus@gmail.com>
2023-06-09 01:41:03 -0700

    Add more debugging information

--- mage_ai/services/spark/spark.py
+++ mage_ai/services/spark/spark.py
@@ -9,23 +9,29 @@ def has_same_spark_config(spark_session, spark_config: SparkConfig) -> bool:
     if spark_config:
         if (spark_config.app_name and spark_config.app_name
                 != spark_session.conf.get('spark.app.name')):
+            print('Different spark.app.name found!')
             return False
         if (spark_config.spark_master and spark_config.spark_master
                 != spark_session.conf.get('spark.master')):
+            print('Different spark.master found!')
             return False
         if (spark_config.spark_home and spark_config.spark_home
                 != spark_session.conf.get('spark.home')):
+            print('Different spark.home found!')
             return False
         if spark_config.executor_env:
             for key, value in spark_config.executor_env.items():
                 if spark_session.conf.get(f'spark.executorEnv.{key}') != value:
+                    print(f'Different spark.executorEnv.{key} found!')
                     return False
         if (spark_config.spark_jars and spark_config.spark_jars
                 != spark_session.conf.get('spark.jars')):
+            print('Different spark.jars found!')
             return False
         if spark_config.others:
             for key, value in spark_config.others.items():
                 if spark_session.conf.get(key) != value:
+                    print(f'Different {key} found!')
                     return False
     return True
 
@@ -36,12 +42,15 @@ def get_spark_session(spark_config: SparkConfig):
 
     if spark_config:
         active_session = SparkSession.getActiveSession()
+        print('Check the given spark_config against the active Spark session.')
         if has_same_spark_config(
             spark_session=active_session,
             spark_config=spark_config
         ):
+            print('Reuse the active Spark session.')
             return active_session
         else:
+            print('Create a new Spark session.')
             if active_session:
                 active_session.stop()
             conf = SparkConf()
