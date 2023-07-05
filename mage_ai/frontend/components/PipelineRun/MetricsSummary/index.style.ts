52970143107e0bfc0188fe71f35d779504008f1d
Johnson Kwok <johnson@mage.ai>
2023-07-04 22:11:21 -0700

    [jk] Add MetricsSummary component

---
+++ mage_ai/frontend/components/PipelineRun/MetricsSummary/index.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import dark from '@oracle/styles/themes/dark';
+import { BORDER_RADIUS, BORDER_WIDTH, BORDER_STYLE } from '@oracle/styles/units/borders';
+import { UNIT } from '@oracle/styles/units/spacing';
+
+export const MetricsSummaryContainerStyle = styled.div`
+  width: 100%;
+  padding: ${UNIT * 3}px;
+  border-radius: ${BORDER_RADIUS}px;
+
+  ${props => `
+    border: ${BORDER_WIDTH}px ${BORDER_STYLE} ${(props.theme || dark).interactive.defaultBorder};
+  `}
+
+`;
---
+++ mage_ai/frontend/components/PipelineRun/MetricsSummary/index.tsx
@@ -0,0 +1,94 @@
+import FlexContainer from '@oracle/components/FlexContainer';
+import Flex from '@oracle/components/Flex';
+import Spacing from '@oracle/elements/Spacing';
+import Text from '@oracle/elements/Text';
+import Tile from '@oracle/components/Tile';
+import dark from '@oracle/styles/themes/dark';
+
+import { GroupedPipelineRunCountType } from '@interfaces/MonitorStatsType';
+import { MetricsSummaryContainerStyle } from './index.style';
+import {
+  PIPELINE_TYPE_ICON_MAPPING,
+  PIPELINE_TYPE_LABEL_MAPPING,
+} from '@interfaces/PipelineType';
+import { RunStatus as RunStatusEnum } from '@interfaces/BlockRunType';
+import { TimePeriodEnum } from '@utils/date';
+import { TIME_PERIOD_DISPLAY_MAPPING } from '@components/Dashboard/constants';
+import { capitalize } from '@utils/string';
+import { formatNumber } from '@utils/number';
+import { queryFromUrl } from '@utils/url';
+import { sortTuplesArrayByFirstItem } from '@utils/array';
+
+type MetricsSummaryProps = {
+   pipelineRunCountByPipelineType: GroupedPipelineRunCountType;
+};
+
+function MetricsSummary({
+  pipelineRunCountByPipelineType,
+}: MetricsSummaryProps) {
+  const q = queryFromUrl();
+  const timePeriod: TimePeriodEnum = q?.tab || TimePeriodEnum.TODAY;
+  const pipelineRunCounts = sortTuplesArrayByFirstItem(
+    Object.entries(pipelineRunCountByPipelineType)
+      .filter(([pipelineType, countsObj]) => Object.keys(countsObj).length !== 0),
+  );
+
+  return (
+    <MetricsSummaryContainerStyle>
+      <Text bold>
+        Pipeline run metrics for {TIME_PERIOD_DISPLAY_MAPPING[timePeriod]}
+      </Text>
+
+      <Spacing mb={2} />
+
+      <FlexContainer alignItems="center" justifyContent="space-between">
+        {pipelineRunCounts.map((
+          [pipelineType, countsObj],
+          idx,
+        ) => (
+          <Flex
+            alignItems="center"
+            flex="1"
+            justifyContent="space-between"
+            key={`${pipelineType}_metric`}
+            style={idx !== 0
+              ? { borderLeft: `1px solid ${dark.interactive.defaultBorder}` }
+              : null
+            }
+          >
+            <Spacing pl={idx !== 0 ? 3 : 0}>
+              <Tile
+                Icon={PIPELINE_TYPE_ICON_MAPPING[pipelineType]}
+                label={PIPELINE_TYPE_LABEL_MAPPING[pipelineType]}
+              />
+            </Spacing>
+
+            {sortTuplesArrayByFirstItem(Object.entries(countsObj))
+              .map(([runStatus, count], idx) => (
+                <Flex
+                  flexDirection="column"
+                  key={`${runStatus}_${idx}`}
+                >
+                  <Text>
+                    {capitalize(runStatus)}
+                  </Text>
+                  <Text
+                    bold
+                    danger={runStatus === RunStatusEnum.FAILED && count > 0}
+                    xlarge
+                  >
+                    {formatNumber(count)}
+                  </Text>
+                </Flex>
+              )
+            )}
+
+            <Spacing pr={idx !== pipelineRunCounts.length - 1 ? 2 : 0} />
+          </Flex>
+        ))}
+      </FlexContainer>
+    </MetricsSummaryContainerStyle>
+  );
+}
+
+export default MetricsSummary;
