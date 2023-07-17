1c1b7e4114a784b7d0d120e3a0165f314cfdd8d8
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-07-17 15:05:13 -0700

    [xy] Fix API.

--- mage_ai/api/resources/LlmResource.py
+++ mage_ai/api/resources/LlmResource.py
@@ -2,11 +2,12 @@ from mage_ai.ai.generator import Generator
 from mage_ai.api.resources.GenericResource import GenericResource
 
 
-class LLMResource(GenericResource):
+class LlmResource(GenericResource):
     @classmethod
     def create(self, payload, user, **kwargs):
         response = Generator.generate(payload.get('use_case'), payload.get('payload'))
-        return dict(
+
+        return self(dict(
             use_case=payload.get('use_case'),
             response=response,
-        )
+        ), user, **kwargs)
