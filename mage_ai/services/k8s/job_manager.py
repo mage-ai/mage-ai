abfb34309b5ef7c49f1a2698c32c1d4037968f76
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-07-16 23:01:14 -0700

    [xy] Support custom k8s executor job prefix. (#2995)
    * [xy] Support custom k8s executor job prefix.
    
    * [xy] Add unit tests.
    
    * [xy] Update doc.

--- mage_ai/services/k8s/job_manager.py
+++ mage_ai/services/k8s/job_manager.py
@@ -1,6 +1,6 @@
 import os
 import time
-from typing import Dict
+from typing import Dict, Union
 
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
@@ -52,7 +52,7 @@ class JobManager():
     def run_job(
         self,
         command: str,
-        k8s_config=None,
+        k8s_config: Union[K8sExecutorConfig, Dict] = None,
     ):
         if not self.job_exists():
             if type(k8s_config) is dict:
@@ -86,7 +86,7 @@ class JobManager():
         self,
         command: str,
         k8s_config: K8sExecutorConfig = None,
-    ):
+    ) -> client.V1Job:
         # Configureate Pod template container
         mage_server_container_spec = self.pod_config.spec.containers[0]
 
@@ -140,7 +140,7 @@ class JobManager():
             body=job,
             namespace=self.namespace,
         )
-        self._print("Job created. status='%s'" % str(api_response.status))
+        self._print(f"Job created. status='{api_response.status}'")
 
     def delete_job(self):
         try:
