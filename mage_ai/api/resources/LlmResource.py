ad14724fb4c5edbbf9489f2adfe585fe91e70b55
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-07-17 18:05:48 -0700

    [xy] Update API

--- mage_ai/api/resources/LlmResource.py
+++ mage_ai/api/resources/LlmResource.py
@@ -5,7 +5,7 @@ from mage_ai.api.resources.GenericResource import GenericResource
 class LlmResource(GenericResource):
     @classmethod
     def create(self, payload, user, **kwargs):
-        response = Generator.generate(payload.get('use_case'), payload.get('payload'))
+        response = Generator.generate(payload.get('use_case'), payload.get('request'))
 
         return self(dict(
             use_case=payload.get('use_case'),
