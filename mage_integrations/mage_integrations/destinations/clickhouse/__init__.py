cd4eebf9bbd687efdc1148882973daa8bd23ae41
Luishfs <luish1980@hotmail.com>
2023-07-18 00:06:09 -0300

    added clickouse integration

---
+++ mage_integrations/mage_integrations/destinations/clickhouse/__init__.py
@@ -0,0 +1,22 @@
+import argparse
+import sys
+
+from mage_integrations.destinations.base import Destination
+from mage_integrations.destinations.clickhouse.target_clickhouse.target import (
+    TargetClickhouse,
+)
+
+
+class Clickhouse(Destination):
+    def process(self, input_buffer) -> None:
+        TargetClickhouse(config=self.config,logger=self.logger).listen_override(
+            file_input=open(self.input_file_path, 'r'))
+
+
+
+if __name__ == '__main__':
+    destination = Clickhouse(
+        argument_parser=argparse.ArgumentParser(),
+        batch_processing=True,
+    )
+    destination.process(sys.stdin.buffer)
---
+++ mage_integrations/mage_integrations/destinations/clickhouse/target_clickhouse/target.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+
+import json
+import sys
+import typing as t
+from collections import Counter, defaultdict
+
+from singer_sdk import typing as th
+from singer_sdk.io_base import SingerMessageType
+from target_clickhouse.sinks import ClickhouseSink
+
+from mage_integrations.destinations.target import SQLTarget
+
+
+class TargetClickhouse(SQLTarget):
+    """SQL-based target for Clickhouse."""
+
+    name = "target-clickhouse"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "sqlalchemy_url",
+            th.StringType,
+            secret=True,  # Flag config as protected.
+            description="SQLAlchemy connection string",
+        ),
+    ).to_dict()
+
+    default_sink_class = ClickhouseSink
+
+    def __init__(self, *, config=None, parse_env_config: bool = False,
+                 validate_config: bool = True, logger) -> None:
+
+        self.loggers = logger
+
+        super().__init__(config=config, parse_env_config=parse_env_config,
+                         validate_config=validate_config)
+
+    @property
+    def logger(self):
+        """Get logger.
+
+        Returns:
+            Plugin logger.
+        """
+
+        logger = self.loggers
+
+        return logger
+
+    def listen_override(self, file_input: t.IO[str] | None = None) -> None:
+        if not file_input:
+            file_input = sys.stdin
+
+        self._process_lines_override(file_input)
+        self._process_endofpipe()
+
+    def _process_lines_override(self, file_input: t.IO[str]) -> t.Counter[str]:
+        """Internal method to process jsonl lines from a Singer tap.
+
+        Args:
+            file_input: Readable stream of messages, each on a separate line.
+
+        Returns:
+            A counter object for the processed lines.
+        """
+        self.logger.info(
+            f"Target {self.name} is listening for input from tap.")
+        counter = self._process_lines_internal(file_input)
+
+        line_count = sum(counter.values())
+
+        self.logger.info(
+            f"Target {self.name} completed reading {line_count} lines of input"
+            f"({counter[SingerMessageType.RECORD]} records"
+            f"{counter[SingerMessageType.BATCH]} batch manifests"
+            f"{counter[SingerMessageType.STATE]} state messages)."
+        )
+
+        return counter
+
+    def _process_lines_internal(self, file_input: t.IO[str]) -> t.Counter[str]:
+        """Internal method to process jsonl lines from a Singer tap.
+
+        Args:
+            file_input: Readable stream of messages, each on a separate line.
+
+        Returns:
+            A counter object for the processed lines.
+
+        Raises:
+            json.decoder.JSONDecodeError: raised if any lines are not valid
+        """
+        stats: dict[str, int] = defaultdict(int)
+        for line in file_input.readlines():
+
+            if line.startswith('INFO'):
+                continue
+
+            line_dict = json.loads(line)
+
+            self._assert_line_requires(line_dict, requires={"type"})
+
+            record_type: SingerMessageType = line_dict["type"]
+            if record_type == SingerMessageType.SCHEMA:
+                self._process_schema_message(line_dict)
+
+            elif record_type == SingerMessageType.RECORD:
+                self._process_record_message(line_dict)
+
+            elif record_type == SingerMessageType.ACTIVATE_VERSION:
+                self._process_activate_version_message(line_dict)
+
+            elif record_type == SingerMessageType.STATE:
+                self._process_state_message(line_dict)
+
+            elif record_type == SingerMessageType.BATCH:
+                self._process_batch_message(line_dict)
+
+            else:
+                continue
+
+            stats[record_type] += 1
+
+        return Counter(**stats)
