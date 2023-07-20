de9845466e18fb110cb28c5fe6aac6036bfee608
Xu He <hexu90@gmail.com>
2023-07-19 20:50:49 -0700

    text to pipeline

--- mage_ai/ai/constants.py
+++ mage_ai/ai/constants.py
@@ -5,3 +5,4 @@ class LLMUseCase(str, Enum):
     GENERATE_COMMENT_FOR_BLOCK = 'generate_comment_for_block'
     GENERATE_DOC_FOR_BLOCK = 'generate_doc_for_block'
     GENERATE_DOC_FOR_PIPELINE = 'generate_doc_for_pipeline'
+    GENERATE_BLOCK_WITH_DESCRIPTION = 'generate_block_with_description'
