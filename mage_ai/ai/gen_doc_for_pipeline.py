c72e2d205fc3e2f823a1e84013c6a3ef9290f54a
Xu He <hexu90@gmail.com>
2023-07-20 21:11:02 -0700

    update documentation generation as async

--- mage_ai/ai/gen_doc_for_pipeline.py
+++ mage_ai/ai/gen_doc_for_pipeline.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import typer
 
 from mage_ai.ai.llm_pipeline_wizard import LLMPipelineWizard
@@ -33,11 +35,11 @@ def generate_block_documentation(project_path: str = PROJECT_NAME_DEFAULT,
 def generate_pipeline_documentation(project_path: str = PROJECT_NAME_DEFAULT,
                                     pipeline_uuid: str = PROJECT_NAME_DEFAULT,
                                     print_block_doc: bool = PRINT_BLOCK_DOC_DEFAULT):
-    print(LLMPipelineWizard().generate_pipeline_documentation(
+    print(asyncio.run(LLMPipelineWizard().async_generate_pipeline_documentation(
         pipeline_uuid,
         print_block_doc=print_block_doc,
         project_path=project_path,
-    )['pipeline_doc'])
+    ))['pipeline_doc'])
 
 
 if __name__ == '__main__':
