543199fbf52b3f7774a00fbb85ccdb3608451116
Luishfs <luish1980@hotmail.com>
2023-07-26 14:24:28 -0300

    fixing requirements & adding state support

--- mage_integrations/mage_integrations/destinations/salesforce/__init__.py
+++ mage_integrations/mage_integrations/destinations/salesforce/__init__.py
@@ -9,6 +9,7 @@ from mage_integrations.destinations.salesforce.target_salesforce.target import (
 
 class Salesforce(Destination):
     def process(self, input_buffer) -> None:
+        self.config['state_path'] = self.state_file_path
         TargetSalesforce(config=self.config, logger=self.logger).listen_override(
             file_input=open(self.input_file_path, 'r'))
 
