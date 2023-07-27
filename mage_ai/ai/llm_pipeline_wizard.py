48104397f3659204720409145c20ccd838b62705
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-07-26 17:24:14 -0700

    [xy] Update response format for generate_block_with_description. (#3104)
    * [xy] Update response format for generate_block_with_description.
    
    * [xy] Update method definition.

--- mage_ai/ai/llm_pipeline_wizard.py
+++ mage_ai/ai/llm_pipeline_wizard.py
@@ -141,7 +141,7 @@ class LLMPipelineWizard:
         config['data_source'] = function_args.get(DataSource.__name__, None)
         return block_type, block_language, pipeline_type, config
 
-    async def async_generate_block_with_description(self, block_description: str) -> str:
+    async def async_generate_block_with_description(self, block_description: str) -> dict:
         messages = [{"role": "user", "content": block_description}]
         response = await openai.ChatCompletion.acreate(
             model="gpt-3.5-turbo-0613",
@@ -154,11 +154,16 @@ class LLMPipelineWizard:
             function_args = json.loads(response_message["function_call"]["arguments"])
             block_type, block_language, pipeline_type, config = self.__load_template_params(
                 function_args)
-            return fetch_template_source(
+            return dict(
                 block_type=block_type,
-                config=config,
+                configuration=config,
+                content=fetch_template_source(
+                    block_type=block_type,
+                    config=config,
+                    language=block_language,
+                    pipeline_type=pipeline_type,
+                ),
                 language=block_language,
-                pipeline_type=pipeline_type,
             )
         else:
             logger.error("Failed to interpret the description as a block template.")
