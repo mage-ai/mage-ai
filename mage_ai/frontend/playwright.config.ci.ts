2d93aeed0aada66cba1fca99f4a2fea6f3a6d543
Eric Tse <erictse@gmail.com>
2023-07-26 20:58:38 -0700

    Use 2 Playwright configs: develop tests locally or run them in GitHub CI

--- mage_ai/frontend/playwright.config.ci.ts
+++ mage_ai/frontend/playwright.config.ci.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:6789',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
@@ -40,8 +40,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn run dev',
+    command: 'python3 mage_ai/cli/main.py start test_project',
+    cwd: '../..',
     reuseExistingServer: !process.env.CI,
-    url: 'http://localhost:3000',
+    url: 'http://localhost:6789',
   },
 });
