1685c21854526f90aae71e40cd4ab441b820b127
dy46 <david@mage.ai>
2023-08-02 22:12:52 -0700

    [dy] Update unit test

--- mage_ai/tests/data_preparation/test_repo_manager.py
+++ mage_ai/tests/data_preparation/test_repo_manager.py
@@ -9,42 +9,43 @@ from mage_ai.tests.base_test import DBTestCase
 
 
 class RepoManagerTest(DBTestCase):
-    def test_variables_dir_from_config_dict(self):
+    def test_variables_dir(self):
+        if os.getenv(MAGE_DATA_DIR_ENV_VAR):
+            del os.environ[MAGE_DATA_DIR_ENV_VAR]
         config_dict = dict(
             variables_dir='variables_dir_from_config_dict',
         )
 
-        test1 = RepoConfig(config_dict=config_dict)
+        test1 = RepoConfig(repo_path=self.repo_path, config_dict=config_dict)
         self.assertEqual(
             test1.variables_dir,
             os.path.join(self.repo_path, 'variables_dir_from_config_dict'),
         )
         shutil.rmtree(test1.variables_dir)
 
-    def test_variables_dir_from_project_metadata(self):
         metadata_dict = dict(
             variables_dir='variables_dir_from_metadata',
         )
-        with open(os.path.join(self.repo_path, 'metadata.yaml'), 'w') as f:
+        test_repo_path = os.path.join(self.repo_path, 'repo_manager_test')
+        os.makedirs(test_repo_path)
+        with open(os.path.join(test_repo_path, 'metadata.yaml'), 'w') as f:
             yaml.dump(metadata_dict, f)
-        test2 = RepoConfig(config_dict=dict())
+        test2 = RepoConfig(repo_path=test_repo_path)
         self.assertEqual(
             test2.variables_dir,
-            os.path.join(self.repo_path, 'variables_dir_from_metadata'),
+            os.path.join(test_repo_path, 'variables_dir_from_metadata'),
         )
         shutil.rmtree(test2.variables_dir)
 
-    def test_variables_dir_from_default(self):
-        test3 = RepoConfig(repo_path='non_existing_path')
+        test3 = RepoConfig(repo_path=os.path.join(self.repo_path, 'non_existing_path'))
         self.assertEqual(
             test3.variables_dir,
-            os.path.join(os.getcwd(), 'non_existing_path')
+            os.path.join(self.repo_path, 'non_existing_path')
         )
         shutil.rmtree(test3.variables_dir)
 
-    def test_variables_dir_from_env_var(self):
         os.environ[MAGE_DATA_DIR_ENV_VAR] = 'variables_dir_from_env_var'
-        test4 = RepoConfig()
+        test4 = RepoConfig(repo_path=self.repo_path)
         self.assertEqual(
             test4.variables_dir,
             os.path.join(self.repo_path, 'variables_dir_from_env_var'),
