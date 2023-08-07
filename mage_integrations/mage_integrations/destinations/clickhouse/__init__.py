dce942c010996be2c0c678542c25d3dd7d9c7229
Luishfs <luish1980@hotmail.com>
2023-08-07 15:20:49 -0300

    Added clickhouse state file location (#3201)

--- mage_integrations/mage_integrations/destinations/clickhouse/__init__.py
+++ mage_integrations/mage_integrations/destinations/clickhouse/__init__.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+
 from clickhouse_sqlalchemy import make_session
 from sqlalchemy import create_engine
 
@@ -11,6 +12,7 @@ from mage_integrations.destinations.clickhouse.target_clickhouse.target import (
 
 class Clickhouse(Destination):
     def _process(self, input_buffer) -> None:
+        self.config['state_path'] = self.state_file_path
         TargetClickhouse(config=self.config, logger=self.logger).listen_override(
             file_input=open(self.input_file_path, 'r'))
 
