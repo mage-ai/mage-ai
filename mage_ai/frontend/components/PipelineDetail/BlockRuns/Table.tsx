2e020172d04c2234b03de6786d1c998e7c6a0ae6
Johnson Kwok <johnson@mage.ai>
2023-08-11 20:03:53 -0700

    [jk] Update formatting of BlockRuns table

--- mage_ai/frontend/components/PipelineDetail/BlockRuns/Table.tsx
+++ mage_ai/frontend/components/PipelineDetail/BlockRuns/Table.tsx
@@ -88,22 +88,22 @@ function BlockRunsTable({
     },
   );
 
-  const columnFlex = [null, 1, 3, 2, null, null, null];
+  const columnFlex = [1, 2, 2, 1, 1, null, null];
   const columns = [
     {
-      uuid: 'Date',
-    },
-    {
       uuid: 'Status',
-    },
-    {
-      uuid: 'Trigger',
     },
     {
       uuid: 'Block',
     },
     {
-      uuid: 'Completed',
+      uuid: 'Trigger',
+    },
+    {
+      uuid: 'Created at',
+    },
+    {
+      uuid: 'Completed at',
     },
     {
       uuid: 'Logs',
@@ -156,13 +156,6 @@ function BlockRunsTable({
 
         const rows = [
           <Text
-            default
-            key={`${id}_created_at`}
-            monospace
-          >
-            {createdAt}
-          </Text>,
-          <Text
             danger={RunStatus.FAILED === status}
             default={RunStatus.CANCELLED === status}
             info={RunStatus.INITIAL === status}
@@ -174,16 +167,6 @@ function BlockRunsTable({
             {status}
           </Text>,
           <NextLink
-            as={`/pipelines/${pipelineUUID}/triggers/${pipelineScheduleId}`}
-            href={'/pipelines/[pipeline]/triggers/[...slug]'}
-            key={`${id}_trigger`}
-            passHref
-          >
-            <Link bold sameColorAsText>
-              {pipelineScheduleName}
-            </Link>
-          </NextLink>,
-          <NextLink
             as={`/pipelines/${pipelineUUID}/edit?block_uuid=${blockUUID}`}
             href={'/pipelines/[pipeline]/edit'}
             key={`${id}_block_uuid`}
@@ -191,7 +174,6 @@ function BlockRunsTable({
           >
             <Link
               bold
-              sameColorAsText
               verticalAlignContent
             >
               <Circle
@@ -203,7 +185,7 @@ function BlockRunsTable({
                 square
               />
               <Spacing mr={1} />
-              <Text monospace>
+              <Text monospace sky>
                 {blockUUID}{streamID && ': '}{streamID && (
                   <Text default inline monospace>
                     {streamID}
@@ -216,12 +198,31 @@ function BlockRunsTable({
               </Text>
             </Link>
           </NextLink>,
+          <NextLink
+            as={`/pipelines/${pipelineUUID}/triggers/${pipelineScheduleId}`}
+            href={'/pipelines/[pipeline]/triggers/[...slug]'}
+            key={`${id}_trigger`}
+            passHref
+          >
+            <Link bold sky>
+              {pipelineScheduleName}
+            </Link>
+          </NextLink>,
+          <Text
+            default
+            key={`${id}_created_at`}
+            monospace
+            small
+          >
+            {createdAt}
+          </Text>,
           <Text
             default
             key={`${id}_completed_at`}
             monospace
+            small
           >
-            {completedAt || '-'}
+            {completedAt?.slice(0, 19) || '-'}
           </Text>,
           <Button
             default
