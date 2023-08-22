1d47a10d206404087d45480bc92744b49d5cebaf
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-08-21 17:56:03 -0700

    [xy] Fix BigQuery datetime bookmark column.

--- mage_integrations/mage_integrations/sources/bigquery/__init__.py
+++ mage_integrations/mage_integrations/sources/bigquery/__init__.py
@@ -2,6 +2,7 @@ from typing import List
 
 from mage_integrations.connections.bigquery import BigQuery as BigQueryConnection
 from mage_integrations.sources.base import main
+from mage_integrations.sources.constants import COLUMN_FORMAT_DATETIME
 from mage_integrations.sources.sql.base import Source
 
 
@@ -33,6 +34,11 @@ FROM {dataset}.INFORMATION_SCHEMA.COLUMNS
             table_names = ', '.join([f"'{n}'" for n in streams])
             query += f'\nWHERE TABLE_NAME IN ({table_names})'
         return query
+
+    def column_type_mapping(self, column_type: str, column_format: str = None) -> str:
+        if COLUMN_FORMAT_DATETIME == column_format:
+            return 'DATETIME'
+        return super().column_type_mapping(column_type, column_format)
 
     def update_column_names(self, columns: List[str]) -> List[str]:
         return [f'`{column}`' for column in columns]
