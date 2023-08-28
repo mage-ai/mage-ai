e083647bad15924d97899f81f01b967dcedd5cb0
David Yang <davidyang1996@gmail.com>
2023-08-28 08:43:04 -0700

    [dy] Restrict file creation through the API (#3351)
    * [dy] Update file and folder methods
    
    * [dy] Add file path tests
    
    * [dy] Support lower versions of python
    
    * [dy] Add to FileContentResource
    
    * [dy] Add get_absolute_path
    
    * [dy] Fix error name
    
    * [dy] Move all checks to the API

--- mage_ai/api/resources/FileContentResource.py
+++ mage_ai/api/resources/FileContentResource.py
@@ -4,7 +4,8 @@ from mage_ai.api.errors import ApiError
 from mage_ai.api.resources.GenericResource import GenericResource
 from mage_ai.cache.block_action_object import BlockActionObjectCache
 from mage_ai.data_preparation.models.block import Block
-from mage_ai.data_preparation.models.file import File
+from mage_ai.data_preparation.models.errors import FileNotInProjectError
+from mage_ai.data_preparation.models.file import File, ensure_file_is_in_project
 from mage_ai.orchestration.db import safe_db_query
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.shared.array import find
@@ -15,6 +16,12 @@ class FileContentResource(GenericResource):
     @safe_db_query
     def member(self, pk, user, **kwargs):
         file_path = urllib.parse.unquote(pk)
+        try:
+            ensure_file_is_in_project(file_path)
+        except FileNotInProjectError:
+            error = ApiError.RESOURCE_INVALID.copy()
+            error.update(message=f'File at path: {file_path} is not in the project directory.')
+            raise ApiError(error)
         file = File.from_path(file_path, get_repo_path())
         return self(file, user, **kwargs)
 
--- mage_ai/data_preparation/models/errors.py
+++ mage_ai/data_preparation/models/errors.py
@@ -3,3 +3,7 @@ from mage_ai.errors.base import MageBaseException
 
 class FileExistsError(MageBaseException):
     pass
+
+
+class FileNotInProjectError(MageBaseException):
+    pass
