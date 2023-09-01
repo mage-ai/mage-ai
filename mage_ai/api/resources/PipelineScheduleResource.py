8ca47f5eeb5bc137a13f8f342c4e7f8193b15d0f
Johnson Kwok <78053898+johnson-mage@users.noreply.github.com>
2023-09-01 16:39:27 -0700

    [jk] Auto-update triggers in code when making changes in UI (#3395)
    * [jk] Update trigger as code when updating pipeline schedule
    
    * [jk] Bulk update triggers in code from Pipelines dashboard
    
    * [jk] Add unit test
    
    * [jk] Update unit test
    
    * [jk] Update unit test
    
    * [jk] Update unit test
    
    * [jk] Add comment
    
    * [jk] Remove trigger from triggers.yaml config if it is saved as code
    
    * [jk] Do not overwrite envs attribute on updated trigger
    
    * [jk] Fix key error
    
    * [jk] Update docs
    
    * [jk] Clean up code
    
    * [jk] Remove unused import
    
    * [jk] Update unit test

--- mage_ai/api/resources/PipelineScheduleResource.py
+++ mage_ai/api/resources/PipelineScheduleResource.py
@@ -4,13 +4,22 @@ import uuid
 from sqlalchemy.orm import selectinload
 
 from mage_ai.api.resources.DatabaseResource import DatabaseResource
+from mage_ai.data_preparation.models.triggers import (
+    Trigger,
+    add_or_update_trigger_for_pipeline_and_persist,
+    remove_trigger,
+)
 from mage_ai.orchestration.db import db_connection, safe_db_query
 from mage_ai.orchestration.db.models.schedules import (
     EventMatcher,
     PipelineSchedule,
     pipeline_schedule_event_matcher_association_table,
 )
-from mage_ai.orchestration.db.models.tags import Tag, TagAssociation, TagAssociationWithTag
+from mage_ai.orchestration.db.models.tags import (
+    Tag,
+    TagAssociation,
+    TagAssociationWithTag,
+)
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.shared.hash import merge_dict
 
@@ -216,7 +225,24 @@ class PipelineScheduleResource(DatabaseResource):
 
             self.tag_associations_updated = tag_associations_updated + tag_associations_to_keep
 
-        super().update(payload)
+        resource = super().update(payload)
+        updated_model = resource.model
+        trigger = Trigger(
+            name=updated_model.name,
+            pipeline_uuid=updated_model.pipeline_uuid,
+            schedule_interval=updated_model.schedule_interval,
+            schedule_type=updated_model.schedule_type,
+            settings=updated_model.settings,
+            sla=updated_model.sla,
+            start_time=updated_model.start_time,
+            status=updated_model.status,
+            variables=updated_model.variables,
+        )
+        add_or_update_trigger_for_pipeline_and_persist(
+            trigger,
+            updated_model.pipeline_uuid,
+            update_only_if_exists=True,
+        )
 
         return self
 
@@ -225,6 +251,13 @@ class PipelineScheduleResource(DatabaseResource):
             return self.tag_associations
         else:
             return self.tag_associations_updated
+
+    @safe_db_query
+    def delete(self, **kwargs):
+        remove_trigger(self.model.name, self.model.pipeline_uuid)
+        self.model.delete()
+
+        return self
 
 
 def __load_tag_associations(resource):
