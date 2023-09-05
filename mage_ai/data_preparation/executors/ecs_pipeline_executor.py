d660d92e5e7ea6584cc40639b3f825f02c75c47a
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-09-05 00:32:30 -0700

    [xy] Fix ECS pipeline executor.

--- mage_ai/data_preparation/executors/ecs_pipeline_executor.py
+++ mage_ai/data_preparation/executors/ecs_pipeline_executor.py
@@ -1,16 +1,16 @@
 from typing import Dict
 
-from mage_ai.data_preparation.executors.block_executor import BlockExecutor
+from mage_ai.data_preparation.executors.pipeline_executor import PipelineExecutor
 from mage_ai.services.aws.ecs import ecs
 from mage_ai.shared.hash import merge_dict
 
 
-class EcsPipelineExecutor(BlockExecutor):
-    def __init__(self, pipeline, block_uuid: str, execution_partition: str = None):
-        super().__init__(pipeline, block_uuid, execution_partition=execution_partition)
-        self.executor_config = self.pipeline.repo_config.ecs_config
-        if self.block.executor_config is not None:
-            self.executor_config = merge_dict(self.executor_config, self.block.executor_config)
+class EcsPipelineExecutor(PipelineExecutor):
+    def __init__(self, pipeline, execution_partition: str = None):
+        super().__init__(pipeline, execution_partition=execution_partition)
+        self.executor_config = self.pipeline.repo_config.ecs_config or dict()
+        if self.pipeline.executor_config is not None:
+            self.executor_config = merge_dict(self.executor_config, self.pipeline.executor_config)
 
     def execute(
         self,
