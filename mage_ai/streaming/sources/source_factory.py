961991521c99803062d583a7bf0bd90c0a06b29c
Mathias Ziebarth <mathias.ziebarth@iosb.fraunhofer.de>
2023-09-11 08:51:18 +0200

    influxdb: flake8 fix order

--- mage_ai/streaming/sources/source_factory.py
+++ mage_ai/streaming/sources/source_factory.py
@@ -10,6 +10,14 @@ class SourceFactory:
             from mage_ai.streaming.sources.amazon_sqs import AmazonSqsSource
 
             return AmazonSqsSource(config, **kwargs)
+        elif connector_type == SourceType.AZURE_EVENT_HUB:
+            from mage_ai.streaming.sources.azure_event_hub import AzureEventHubSource
+
+            return AzureEventHubSource(config, **kwargs)
+        elif connector_type == SourceType.GOOGLE_CLOUD_PUBSUB:
+            from mage_ai.streaming.sources.google_cloud_pubsub import GoogleCloudPubSubSource
+
+            return GoogleCloudPubSubSource(config, **kwargs)
         elif connector_type == SourceType.INFLUXDB:
             from mage_ai.streaming.sources.influxdb import InfluxDbSource
 
@@ -22,14 +30,6 @@ class SourceFactory:
             from mage_ai.streaming.sources.kinesis import KinesisSource
 
             return KinesisSource(config, **kwargs)
-        elif connector_type == SourceType.AZURE_EVENT_HUB:
-            from mage_ai.streaming.sources.azure_event_hub import AzureEventHubSource
-
-            return AzureEventHubSource(config, **kwargs)
-        elif connector_type == SourceType.GOOGLE_CLOUD_PUBSUB:
-            from mage_ai.streaming.sources.google_cloud_pubsub import GoogleCloudPubSubSource
-
-            return GoogleCloudPubSubSource(config, **kwargs)
         elif connector_type == SourceType.RABBITMQ:
             from mage_ai.streaming.sources.rabbitmq import RabbitMQSource
 
