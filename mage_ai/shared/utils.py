fd632852c12c167b2638456d98769f07b19c0d5a
Luishfs <luish1980@hotmail.com>
2023-09-11 16:22:23 -0300

    Added timestamp_precision check

--- mage_ai/shared/utils.py
+++ mage_ai/shared/utils.py
@@ -105,8 +105,8 @@ def convert_python_type_to_trino_type(python_type, usr_data_types):
         return 'BOOLEAN'
     elif python_type is datetime and usr_data_types is None:
         return 'TIMESTAMP'
-    elif python_type is datetime and usr_data_types is not None:
-        return f'TIMESTAMP({usr_data_types.get("timestamp_precision")})'
+    elif python_type is datetime and usr_data_types.get('timestamp_precision') is not None:
+        return f"TIMESTAMP({usr_data_types.get('timestamp_precision')})"
     return 'VARCHAR'
 
 
