1b65f26553aac0b92cec970bf68759c27d1a67c4
tommydangerous <quantumventuress@gmail.com>
2023-09-10 21:12:09 -0700

    [td] Add block layout to overview

--- mage_ai/frontend/components/Dashboard/constants.ts
+++ mage_ai/frontend/components/Dashboard/constants.ts
@@ -1,4 +1,4 @@
-import { CalendarRounded, Smiley, WeekDots } from '@oracle/icons';
+import { CalendarRounded, NavDashboard, Smiley, WeekDots } from '@oracle/icons';
 import { TabType } from '@oracle/components/Tabs/ButtonTabs';
 import { TIME_PERIOD_DISPLAY_MAPPING, TimePeriodEnum } from '@utils/date';
 import { capitalize } from '@utils/string';
@@ -17,6 +17,12 @@ export const TAB_MONTH = {
   Icon: CalendarRounded,
   label: () => capitalize(TIME_PERIOD_DISPLAY_MAPPING[TimePeriodEnum.MONTH]),
   uuid: TimePeriodEnum.MONTH,
+};
+
+export const TAB_DASHBOARD = {
+  Icon: NavDashboard,
+  label: () => 'Dashboard',
+  uuid: 'Dashboard',
 };
 
 export const TIME_PERIOD_TABS: TabType[] = [
