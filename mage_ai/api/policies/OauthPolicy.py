d1b16afbddc676fe066157a18c9b03fcb68d976c
David Yang <davidyang1996@gmail.com>
2023-09-11 09:59:08 -0700

    [dy] Support github auth for different workspaces (#3456)
    * [dy] Initial commit
    
    * [dy] Black formatting

--- mage_ai/api/policies/OauthPolicy.py
+++ mage_ai/api/policies/OauthPolicy.py
@@ -8,51 +8,95 @@ class OauthPolicy(BasePolicy):
     pass
 
 
-OauthPolicy.allow_actions([
-    constants.CREATE,
-], scopes=[
-    OauthScope.CLIENT_PRIVATE,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_actions(
+    [
+        constants.CREATE,
+        constants.UPDATE,
+    ],
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
 
 
-OauthPolicy.allow_actions([
-    constants.DETAIL,
-], scopes=[
-    OauthScope.CLIENT_PRIVATE,
-    OauthScope.CLIENT_PUBLIC,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_actions(
+    [
+        constants.DETAIL,
+    ],
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+        OauthScope.CLIENT_PUBLIC,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
 
 
-OauthPolicy.allow_read(OauthPresenter.default_attributes, scopes=[
-    OauthScope.CLIENT_PRIVATE,
-], on_action=[
-    constants.CREATE,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_read(
+    OauthPresenter.default_attributes,
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+    ],
+    on_action=[
+        constants.CREATE,
+        constants.UPDATE,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
 
 
-OauthPolicy.allow_read(OauthPresenter.default_attributes, scopes=[
-    OauthScope.CLIENT_PRIVATE,
-    OauthScope.CLIENT_PUBLIC,
-], on_action=[
-    constants.DETAIL,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_read(
+    OauthPresenter.default_attributes,
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+        OauthScope.CLIENT_PUBLIC,
+    ],
+    on_action=[
+        constants.DETAIL,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
 
 
-OauthPolicy.allow_write([
-    'provider',
-    'token',
-], scopes=[
-    OauthScope.CLIENT_PRIVATE,
-], on_action=[
-    constants.CREATE,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_write(
+    [
+        'provider',
+        'token',
+    ],
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+    ],
+    on_action=[
+        constants.CREATE,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
 
 
-OauthPolicy.allow_query([
-    'redirect_uri',
-], on_action=[
-    constants.DETAIL,
-], scopes=[
-    OauthScope.CLIENT_PRIVATE,
-    OauthScope.CLIENT_PUBLIC,
-], condition=lambda policy: policy.has_at_least_viewer_role())
+OauthPolicy.allow_write(
+    [
+        'action_type',
+    ],
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+    ],
+    on_action=[
+        constants.UPDATE,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
+
+
+OauthPolicy.allow_query(
+    [
+        'redirect_uri',
+    ],
+    on_action=[
+        constants.DETAIL,
+    ],
+    scopes=[
+        OauthScope.CLIENT_PRIVATE,
+        OauthScope.CLIENT_PUBLIC,
+    ],
+    condition=lambda policy: policy.has_at_least_viewer_role(),
+)
