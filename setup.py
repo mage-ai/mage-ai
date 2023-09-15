bc38f6e556ee2af911d3e901e8dcdebf6d7129fe
Xu He <hexu90@gmail.com>
2023-09-15 15:25:53 -0700

    [xh] Integrate DuckDB IO class and SQL block (#3463)
    * duck db
    
    * clean up PR
    
    * address comments
    
    * more dep to extras
    
    * rename to DuckDB

--- setup.py
+++ setup.py
@@ -131,6 +131,7 @@ setuptools.setup(
             'dbt-sqlserver==1.3.1',
             'dbt-trino==1.4.0',
             'dbt-clickhouse==1.4.0',
+            'duckdb==0.8.1',
             'elasticsearch==8.9.0',
             'google-api-core==2.11.0',
             'google-api-python-client==2.70.0',
