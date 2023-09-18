f25035ad0998322dee5b872c7142415c9fcd9a64
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-09-18 11:47:06 -0700

    [xy] Update unit tests.

--- mage_ai/streaming/sinks/kafka.py
+++ mage_ai/streaming/sinks/kafka.py
@@ -5,6 +5,7 @@ from enum import Enum
 from typing import Dict, List
 
 from kafka import KafkaProducer
+
 from mage_ai.shared.config import BaseConfig
 from mage_ai.streaming.constants import DEFAULT_BATCH_SIZE, DEFAULT_TIMEOUT_MS
 from mage_ai.streaming.sinks.base import BaseSink
