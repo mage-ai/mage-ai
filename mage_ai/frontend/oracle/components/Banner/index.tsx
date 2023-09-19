963239f186664c32cca8bacad71da9ac575f251f
Johnson Kwok <johnson@mage.ai>
2023-09-19 14:03:09 -0700

    [jk] Add Banner component

---
+++ mage_ai/frontend/oracle/components/Banner/index.tsx
@@ -0,0 +1,85 @@
+import FlexContainer, { JUSTIFY_SPACE_BETWEEN_PROPS } from '../FlexContainer';
+import Link from '@oracle/elements/Link';
+import Spacing from '@oracle/elements/Spacing';
+import Text from '@oracle/elements/Text';
+import { BannerContainerStyle, BannerStyle } from './index.style';
+import { ChevronRight } from '@oracle/icons';
+import { ICON_SIZE_MEDIUM } from '@oracle/styles/units/icons';
+
+type BannerProps = {
+  children?: any;
+  linkProps?: {
+    href?: string;
+    label?: string;
+  }
+  onHideClick?: () => void;
+  textProps?: {
+    message?: string;
+    warning?: boolean;
+  }
+};
+
+function Banner({
+  children,
+  linkProps,
+  onHideClick,
+  textProps,
+}: BannerProps) {
+  const { message, warning } = textProps || {};
+  const { href, label: linkLabel } = linkProps || {};
+
+  return (
+    <BannerContainerStyle>
+      <FlexContainer {...JUSTIFY_SPACE_BETWEEN_PROPS}>
+        <BannerStyle>
+          {children
+            ? children
+            : (
+              <FlexContainer {...JUSTIFY_SPACE_BETWEEN_PROPS}>
+                <Text
+                  large
+                  warning={warning}
+                >
+                  {message}
+                </Text>
+                {href &&
+                  <Spacing ml={3}>
+                    <FlexContainer {...JUSTIFY_SPACE_BETWEEN_PROPS}>
+                      <Link
+                        bold
+                        href={href}
+                        large
+                        openNewWindow
+                        sameColorAsText
+                      >
+                        {linkLabel}
+                      </Link>
+
+                      <Spacing ml="4px" />
+
+                      <ChevronRight size={ICON_SIZE_MEDIUM} />
+                    </FlexContainer>
+                  </Spacing>
+                }
+              </FlexContainer>
+            )
+          }
+        </BannerStyle>
+
+        {onHideClick && (
+          <Spacing ml={2}>
+            <Link
+              bold
+              large
+              onClick={onHideClick}
+            >
+              Hide
+            </Link>
+          </Spacing>
+        )}
+      </FlexContainer>
+    </BannerContainerStyle>
+  );
+}
+
+export default Banner;
