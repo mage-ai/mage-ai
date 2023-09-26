afab673da41bd7160d774105b9c014d126d1bdf0
Rares Popa <rares.popa@ni.com>
2023-09-26 11:23:44 +0300

    Reworked implementation to comply with test architecture

--- mage_ai/orchestration/triggers/time_trigger.py
+++ mage_ai/orchestration/triggers/time_trigger.py
@@ -4,7 +4,7 @@ from mage_ai.orchestration.pipeline_scheduler import check_sla, schedule_all
 
 
 class TimeTrigger(ABC):
-    def __init__(self, trigger_interval) -> None:
+    def __init__(self, trigger_interval: int = 10) -> None:
         self.trigger_interval = trigger_interval
 
     def run(self) -> None:
