10fe2429d1901c8e626e1a14933c06be5858cd60
David Yang <davidyang1996@gmail.com>
2023-09-28 16:12:26 -0700

    [dy] Handle secret key and uuid whitespace (#3621)
    * [dy] Handle whitespace for secret key and uuid
    
    * [dy] Add unit test

--- mage_ai/orchestration/db/models/secrets.py
+++ mage_ai/orchestration/db/models/secrets.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, String, Text, UniqueConstraint
+from typing import Optional
 
+from sqlalchemy import Column, String, Text, UniqueConstraint, or_
+
+from mage_ai.orchestration.db import safe_db_query
 from mage_ai.orchestration.db.models.base import BaseModel
 
 
@@ -11,3 +14,14 @@ class Secret(BaseModel):
     repo_name = Column(String(255))
     key_uuid = Column(String(255), nullable=True)
     __table_args__ = (UniqueConstraint('name', 'key_uuid', name='name_key_uuid_uc'),)
+
+    @classmethod
+    @safe_db_query
+    def get_secret(cls, name: str, key_uuid: str) -> Optional['Secret']:
+        return cls.query.filter(
+            cls.name == name,
+            or_(
+                cls.key_uuid == key_uuid,
+                cls.key_uuid == key_uuid.strip(),
+            ),
+        ).one_or_none()
