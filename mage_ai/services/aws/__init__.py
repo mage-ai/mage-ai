b9320a684f90a1d25bf251bc49dbf9b5f047db3d
dy46 <david@mage.ai>
2023-10-05 11:13:45 -0700

    [dy] Add get secret value db safe

--- mage_ai/services/aws/__init__.py
+++ mage_ai/services/aws/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from mage_ai.data_preparation.shared.secrets import get_secret_value
+from mage_ai.data_preparation.shared.secrets import get_secret_value_db_safe
 
 AWS_REGION_NAME = 'AWS_REGION_NAME'
 AWS_DEFAULT_REGION = 'AWS_DEFAULT_REGION'
@@ -13,9 +13,9 @@ def get_aws_region_name():
     if region_name is None:
         region_name = os.getenv(AWS_DEFAULT_REGION)
     if region_name is None:
-        region_name = get_secret_value(AWS_REGION_NAME)
+        region_name = get_secret_value_db_safe(AWS_REGION_NAME)
     if region_name is None:
-        region_name = get_secret_value(AWS_DEFAULT_REGION)
+        region_name = get_secret_value_db_safe(AWS_DEFAULT_REGION)
     if region_name is None:
         region_name = 'us-west-2'
     return region_name
@@ -24,14 +24,14 @@ def get_aws_region_name():
 def get_aws_access_key_id():
     aws_access_key_id = os.getenv(AWS_ACCESS_KEY_ID)
     if aws_access_key_id is None:
-        aws_access_key_id = get_secret_value(AWS_ACCESS_KEY_ID)
+        aws_access_key_id = get_secret_value_db_safe(AWS_ACCESS_KEY_ID)
     return aws_access_key_id
 
 
 def get_aws_secret_access_key():
     aws_secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY)
     if aws_secret_access_key is None:
-        aws_secret_access_key = get_secret_value(AWS_SECRET_ACCESS_KEY)
+        aws_secret_access_key = get_secret_value_db_safe(AWS_SECRET_ACCESS_KEY)
     return aws_secret_access_key
 
 
