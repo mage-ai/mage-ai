879bb0ad792e545adec7af14d3f4fcbed7ea9b83
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-10-06 02:24:19 -0700

    [xy] Speed up io mssql export method (#3697)
    * [xy] Use fast_executemany in io mssql.
    
    * [xy] Fix connection issue.
    
    * [xy] Update doc.

--- mage_ai/io/sql.py
+++ mage_ai/io/sql.py
@@ -105,7 +105,8 @@ class BaseSQL(BaseSQLConnection):
         db_dtypes: List[str],
         dtypes: List[str],
         full_table_name: str,
-        buffer: Union[IO, None] = None
+        buffer: Union[IO, None] = None,
+        **kwargs,
     ) -> None:
         raise Exception('Subclasses must override this method.')
 
@@ -215,6 +216,7 @@ class BaseSQL(BaseSQLConnection):
         allow_reserved_words: bool = False,
         unique_conflict_method: str = None,
         unique_constraints: List[str] = None,
+        **kwargs,
     ) -> None:
         """
         Exports dataframe to the connected database from a Pandas data frame. If table doesn't
@@ -254,6 +256,7 @@ class BaseSQL(BaseSQLConnection):
             if index:
                 df = df.reset_index()
 
+            # Clean dataframe
             dtypes = infer_dtypes(df)
             df = clean_df_for_export(df, self.clean, dtypes)
 
@@ -266,6 +269,16 @@ class BaseSQL(BaseSQLConnection):
             dtypes = infer_dtypes(df)
 
         def __process():
+            if not query_string and kwargs.get('fast_execute', True) and \
+                    hasattr(self, 'upload_dataframe_fast') and callable(self.upload_dataframe_fast):
+                self.upload_dataframe_fast(
+                    df,
+                    schema_name,
+                    table_name,
+                    if_exists=if_exists,
+                )
+                return
+
             buffer = StringIO()
             table_exists = self.table_exists(schema_name, table_name)
 
@@ -324,6 +337,7 @@ class BaseSQL(BaseSQLConnection):
                         allow_reserved_words=allow_reserved_words,
                         unique_conflict_method=unique_conflict_method,
                         unique_constraints=unique_constraints,
+                        **kwargs,
                     )
             self.conn.commit()
 
