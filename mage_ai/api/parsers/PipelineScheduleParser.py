f226b06486b0f4a93c483c0745409f184c4f1781
tommydangerous <quantumventuress@gmail.com>
2023-10-08 19:51:08 -0700

    [td] Only allow specific pipeline schedule variables to be created

--- mage_ai/api/parsers/PipelineScheduleParser.py
+++ mage_ai/api/parsers/PipelineScheduleParser.py
@@ -8,6 +8,8 @@ from mage_ai.api.policies.PipelineSchedulePolicy import (
     READABLE_ATTRIBUTES_FOR_PIPELINE_INTERACTIONS,
     WRITABLE_ATTRIBUTES_FOR_PIPELINE_INTERACTIONS,
 )
+from mage_ai.data_preparation.models.project import Project
+from mage_ai.data_preparation.models.project.constants import FeatureUUID
 from mage_ai.shared.hash import extract
 
 
@@ -23,8 +25,39 @@ async def parse_read_update(parser, _value: Dict, **kwargs) -> Dict:
     return {}
 
 
-async def parse_write(parser, value: Dict, **kwargs) -> Dict:
-    return extract(value, WRITABLE_ATTRIBUTES_FOR_PIPELINE_INTERACTIONS)
+async def parse_write(
+    parser,
+    value: Dict,
+    authorize_status: AuthorizeStatusType = None,
+    **kwargs,
+) -> Dict:
+    if AuthorizeStatusType.FAILED == authorize_status:
+        parsed_value = extract(value or {}, WRITABLE_ATTRIBUTES_FOR_PIPELINE_INTERACTIONS)
+    else:
+        parsed_value = (value or {}).copy()
+
+    pipeline = parser.policy.parent_model()
+    if not pipeline:
+        pipeline = parser.policy.resource.pipeline
+
+    if Project(pipeline.repo_config if pipeline else None).is_feature_enabled(
+        FeatureUUID.INTERACTIONS,
+    ):
+        pipeline_interaction = (parser.policy.result_set().context.data or {}).get(
+            'pipeline_interactions',
+            {},
+        ).get(pipeline.uuid)
+
+        if pipeline_interaction:
+            variables = parsed_value.get('variables')
+            if pipeline_interaction and variables:
+                variables_allowed = await pipeline_interaction.variables()
+                parsed_value['variables'] = extract(
+                    variables or {},
+                    (variables_allowed or {}).keys(),
+                )
+
+    return parsed_value
 
 
 async def parse_write_update(parser, value: Dict, **kwargs) -> Dict:
@@ -66,6 +99,7 @@ PipelineScheduleParser.parse_write(
     ],
     on_authorize_status=[
         AuthorizeStatusType.FAILED,
+        AuthorizeStatusType.SUCCEEDED,
     ],
     scopes=[
         OauthScopeType.CLIENT_PRIVATE,
