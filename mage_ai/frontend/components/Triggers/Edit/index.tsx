dcbd34be708d76342e98962e2582660fc945eb37
dy46 <david@mage.ai>
2023-10-10 16:12:31 -0700

    [dy] Add toggle for request headers in UI

--- mage_ai/frontend/components/Triggers/Edit/index.tsx
+++ mage_ai/frontend/components/Triggers/Edit/index.tsx
@@ -135,6 +135,7 @@ function Edit({
   const [eventMatchers, setEventMatchers] = useState<EventMatcherType[]>([]);
   const [overwriteVariables, setOverwriteVariables] = useState<boolean>(false);
   const [enableSLA, setEnableSLA] = useState<boolean>(false);
+  const [useHeaderUrl, setUseHeaderUrl] = useState<boolean>(false);
 
   const [settings, setSettings] = useState<PipelineScheduleSettingsType>(null);
   const [runtimeVariables, setRuntimeVariables] = useState<{ [ variable: string ]: string }>({});
@@ -1326,7 +1327,7 @@ function Edit({
   ]);
 
   const apiMemo = useMemo(() => {
-    const url = getTriggerApiEndpoint(pipelineSchedule);
+    const url = getTriggerApiEndpoint(pipelineSchedule, useHeaderUrl);
 
     return (
       <>
@@ -1365,36 +1366,52 @@ function Edit({
               withCopyIcon
             />
           </Spacing>
-        </Spacing>
-
-        <Spacing mb={2} mt={5} px={PADDING_UNITS}>
-          <Headline>
-            Headers
-          </Headline>
-
-          <Text muted>
-            You will need to include the following headers in your request to authenticate
-            with the server.
-          </Text>
-
+          
           <Spacing mt={UNITS_BETWEEN_ITEMS_IN_SECTIONS}>
-            <CopyToClipboard
-              copiedText={`Content-Type: application/json
-Authorization: Bearer ${pipelineSchedule?.token}
-`}
-              withCopyIcon
-            >
-              <CodeBlock
-                language="json"
-                small
-                source={`
-    Content-Type: application/json
-    Authorization: Bearer ${pipelineSchedule?.token}
-`}
-              />
-            </CopyToClipboard>
+            <FlexContainer alignItems="center">
+              <Spacing mr={2}>
+                <ToggleSwitch
+                  checked={useHeaderUrl}
+                  onCheck={() => setUseHeaderUrl(!useHeaderUrl)}
+                />
+              </Spacing>
+              <Text default monospace>
+                Supply token through request headers
+              </Text>
+            </FlexContainer>
           </Spacing>
         </Spacing>
+        
+        {useHeaderUrl && (
+          <Spacing mb={2} mt={5} px={PADDING_UNITS}>
+            <Headline>
+              Headers
+            </Headline>
+
+            <Text muted>
+              You will need to include the following headers in your request to authenticate
+              with the server.
+            </Text>
+
+            <Spacing mt={UNITS_BETWEEN_ITEMS_IN_SECTIONS}>
+              <CopyToClipboard
+                copiedText={`Content-Type: application/json
+  Authorization: Bearer ${pipelineSchedule?.token}
+  `}
+                withCopyIcon
+              >
+                <CodeBlock
+                  language="json"
+                  small
+                  source={`
+      Content-Type: application/json
+      Authorization: Bearer ${pipelineSchedule?.token}
+  `}
+                />
+              </CopyToClipboard>
+            </Spacing>
+          </Spacing>
+        )}
 
         <Spacing mb={2} mt={5} px={PADDING_UNITS}>
           <Headline>
@@ -1446,10 +1463,22 @@ Authorization: Bearer ${pipelineSchedule?.token}
             <CodeBlock
               language="bash"
               small
-              source={`
+              source={useHeaderUrl ? `
     curl -X POST ${url} \\
       --header 'Content-Type: application/json' \\
       --header 'Authorization: Bearer ${pipelineSchedule?.token}' \\
+      --data '
+    {
+      "pipeline_run": {
+        "variables": {
+          "key1": "value1",
+          "key2": "value2"
+        }
+      }
+    }'
+  ` : `
+    curl -X POST ${url} \\
+      --header 'Content-Type: application/json' \\
       --data '
     {
       "pipeline_run": {
@@ -1465,7 +1494,13 @@ Authorization: Bearer ${pipelineSchedule?.token}
         </Spacing>
       </>
     );
-  }, [pipelineSchedule, triggerDescriptionRowEl, triggerNameRowEl]);
+  }, [
+    pipelineSchedule,
+    setUseHeaderUrl,
+    triggerDescriptionRowEl,
+    triggerNameRowEl,
+    useHeaderUrl,
+  ]);
 
   const saveButtonDisabled = !scheduleType || (
     ScheduleTypeEnum.TIME === scheduleType
