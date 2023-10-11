13d68632c74885cb88dc30a30b47523681c4c9de
eoguz <emin.oguz@hepsiburada.com>
2023-10-11 12:15:45 +0300

    MongoDB CDC is added

--- mage_ai/streaming/sources/mongodb.py
+++ mage_ai/streaming/sources/mongodb.py
@@ -23,23 +23,39 @@ class MongoDBConfig(BaseConfig):
 class MongoSource(BaseSource):
     config_class = MongoDBConfig
 
+    def init_client(self):
+        connection_str = self.config.connection_str
+        database = self.config.database
+        collection = self.config.collection
+        batch_size = self.config.batch_size
+        pipeline = self.config.pipeline
+        operation_time = self.config.operation_time
+        start_after = self.config.start_after
+        self._print(f"Starting to MongoDB Stream for {database}")
+        self._print(f"You can use this collection {collection}")
+        self._print(f"You can use {connection_str}")
+        self._print(f"You can use {batch_size}")
+        self._print(f"You can use {pipeline}")
+        self._print(f"You can use {operation_time}")
+        self._print(f"You can use {start_after}")
+
     def read(self, handler: Callable):
         pass
 
     def batch_read(self, handler: Callable):
         self._print("Start getting message for MongoDB streaming.")
         try:
-            client = MongoClient(self.config_class.connection_str)
-            db = client.get_database(self.config_class.database)
+            client = MongoClient(self.config.connection_str)
+            db = client.get_database(self.config.database)
             parsed_messages = []
             if self.config_class.collection:
-                watch_args = {'batch_size': self.config_class.batch_size}
+                watch_args = {'batch_size': self.config.batch_size}
                 if self.config_class.pipeline:
-                    watch_args['pipeline'] = self.config_class.pipeline
+                    watch_args['pipeline'] = self.config.pipeline
                 if self.config_class.operation_time:
-                    watch_args['operation_time'] = self.config_class.operation_time
+                    watch_args['operation_time'] = self.config.operation_time
                 if self.config_class.start_after:
-                    watch_args['start_after'] = self.config_class.start_after
+                    watch_args['start_after'] = self.config.start_after
                 with db[self.config_class.collection].watch(**watch_args) as stream:
                     for change in stream:
                         parsed_messages.append(change)
