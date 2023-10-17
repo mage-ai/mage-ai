773a49b03648ca893215617f6061cff7288fef8a
Hans Lemm <hcrlemm@gmail.com>
2023-10-17 11:53:24 +0200

    feat: allow markdown on slack notifications

--- mage_ai/services/slack/slack.py
+++ mage_ai/services/slack/slack.py
@@ -1,10 +1,16 @@
-from mage_ai.services.slack.config import SlackConfig
 import json
+
 import requests
+
+from mage_ai.services.slack.config import SlackConfig
 
 
 def send_slack_message(config: SlackConfig, message: str) -> None:
+    payload = {
+        "blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": message}}]
+    }
+
     requests.post(
         config.webhook_url,
-        json.dumps(dict(text=message)),
+        json.dumps(payload),
     )
