88700536d685c92e2a0f0a34cf2eac9d8d617d0c
David Yang <davidyang1996@gmail.com>
2023-10-17 11:02:20 -0500

    [dy] Add auto termination frontend (#3751)
    * [dy] Update workspace creation modal
    
    * [dy] Fix lint
    
    * [dy] Update

--- mage_ai/frontend/pages/manage/index.tsx
+++ mage_ai/frontend/pages/manage/index.tsx
@@ -4,6 +4,7 @@ import { useMutation } from 'react-query';
 import Button from '@oracle/elements/Button';
 import ClickOutside from '@oracle/components/ClickOutside';
 import ConfigureWorkspace from '@components/workspaces/ConfigureWorkspace';
+import ErrorsType from '@interfaces/ErrorsType';
 import FlexContainer from '@oracle/components/FlexContainer';
 import FlyoutMenu from '@oracle/components/FlyoutMenu';
 import KeyboardShortcutButton from '@oracle/elements/Button/KeyboardShortcutButton';
@@ -25,13 +26,15 @@ import { onSuccess } from '@api/utils/response';
 import { useModal } from '@context/Modal';
 
 function MoreActions({
+  clusterType,
   fetchWorkspaces,
   instance,
-  clusterType,
+  setErrors,
 }: {
+  clusterType: string;
   fetchWorkspaces: any;
   instance: InstanceType;
-  clusterType: string;
+  setErrors: (errors: ErrorsType) => void;
 }) {
   const refMoreActions = useRef(null);
   const [showMoreActions, setShowMoreActions] = useState<boolean>();
@@ -55,14 +58,10 @@ function MoreActions({
             fetchWorkspaces();
             setShowMoreActions(false);
           },
-          onErrorCallback: ({
-            error: {
-              errors,
-              message,
-            },
-          }) => {
-            console.log(errors, message);
-          },
+          onErrorCallback: (response, errors) => setErrors({
+            errors,
+            response,
+          }),
         },
       ),
     },
@@ -77,14 +76,10 @@ function MoreActions({
             fetchWorkspaces();
             setShowMoreActions(false);
           },
-          onErrorCallback: ({
-            error: {
-              errors,
-              message,
-            },
-          }) => {
-            console.log(errors, message);
-          },
+          onErrorCallback: (response, errors) => setErrors({
+            errors,
+            response,
+          }),
         },
       ),
     },
@@ -244,6 +239,7 @@ function MoreActions({
 
 function WorkspacePage() {
   const { data: dataStatus } = api.statuses.list();
+  const [errors, setErrors] = useState<ErrorsType>(null);
   const clusterType = useMemo(
     () => dataStatus?.statuses?.[0]?.instance_type || 'ecs',
     [dataStatus],
@@ -277,6 +273,8 @@ function WorkspacePage() {
     fetchWorkspaces,
   ], {
     background: true,
+    disableClickOutside: true,
+    disableEscape: true,
     uuid: 'configure_workspace',
   });
 
@@ -288,7 +286,9 @@ function WorkspacePage() {
           label: () => 'Workspaces',
         },
       ]}
+      errors={errors}
       pageName={WorkspacesPageNameEnum.WORKSPACES}
+      setErrors={setErrors}
       subheaderChildren={
         <KeyboardShortcutButton
           background={BUTTON_GRADIENT}
@@ -379,6 +379,7 @@ function WorkspacePage() {
               fetchWorkspaces={fetchWorkspaces}
               instance={instance}
               key="more_actions"
+              setErrors={setErrors}
             />,
           ];
         })}
