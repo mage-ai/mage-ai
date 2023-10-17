19a6afbb29f88d6d12870bf4b9d98145c0b09fe0
David Yang <davidyang1996@gmail.com>
2023-10-17 16:56:35 -0500

    [dy] Fix circular dependency (#3783)

--- mage_ai/services/aws/__init__.py
+++ mage_ai/services/aws/__init__.py
@@ -1,6 +1,5 @@
 import os
-
-from mage_ai.data_preparation.shared.secrets import get_secret_value_db_safe
+from typing import Optional
 
 AWS_REGION_NAME = 'AWS_REGION_NAME'
 AWS_DEFAULT_REGION = 'AWS_DEFAULT_REGION'
@@ -8,39 +7,38 @@ AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
 AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
 
 
-def get_aws_region_name():
+def get_aws_value_from_secrets(name: str) -> Optional[str]:
+    try:
+        from mage_ai.data_preparation.shared.secrets import get_secret_value_db_safe
+        return get_secret_value_db_safe(name, suppress_warning=True)
+    except Exception:
+        return None
+
+
+def get_aws_region_name() -> Optional[str]:
     region_name = os.getenv(AWS_REGION_NAME)
     if region_name is None:
         region_name = os.getenv(AWS_DEFAULT_REGION)
     if region_name is None:
-        region_name = get_secret_value_db_safe(AWS_REGION_NAME, suppress_warning=True)
+        region_name = get_aws_value_from_secrets(AWS_REGION_NAME)
     if region_name is None:
-        region_name = get_secret_value_db_safe(
-            AWS_DEFAULT_REGION,
-            suppress_warning=True,
-        )
+        region_name = get_aws_value_from_secrets(AWS_DEFAULT_REGION)
     if region_name is None:
         region_name = 'us-west-2'
     return region_name
 
 
-def get_aws_access_key_id():
+def get_aws_access_key_id() -> Optional[str]:
     aws_access_key_id = os.getenv(AWS_ACCESS_KEY_ID)
     if aws_access_key_id is None:
-        aws_access_key_id = get_secret_value_db_safe(
-            AWS_ACCESS_KEY_ID,
-            suppress_warning=True,
-        )
+        aws_access_key_id = get_aws_value_from_secrets(AWS_ACCESS_KEY_ID)
     return aws_access_key_id
 
 
-def get_aws_secret_access_key():
+def get_aws_secret_access_key() -> Optional[str]:
     aws_secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY)
     if aws_secret_access_key is None:
-        aws_secret_access_key = get_secret_value_db_safe(
-            AWS_SECRET_ACCESS_KEY,
-            suppress_warning=True,
-        )
+        aws_secret_access_key = get_aws_value_from_secrets(AWS_SECRET_ACCESS_KEY)
     return aws_secret_access_key
 
 
@@ -49,11 +47,17 @@ def get_aws_boto3_client(aws_service):
     from botocore.config import Config
 
     config = Config(region_name=get_aws_region_name())
+    kwargs = dict()
 
-    client = boto3.client(
+    aws_access_key_id = get_aws_access_key_id()
+    if aws_access_key_id:
+        kwargs['aws_access_key_id'] = aws_access_key_id
+    aws_secret_access_key = get_aws_secret_access_key()
+    if aws_secret_access_key:
+        kwargs['aws_secret_access_key'] = aws_secret_access_key
+
+    return boto3.client(
         aws_service,
-        aws_access_key_id=get_aws_access_key_id(),
-        aws_secret_access_key=get_aws_secret_access_key(),
         config=config,
+        **kwargs,
     )
-    return client
