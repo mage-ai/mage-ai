6ccc44b857e997c040070295fcf87fb9291bde54
eofs <316612+eofs@users.noreply.github.com>
2023-10-21 11:44:19 +0300

    Linter fixes

--- mage_integrations/mage_integrations/destinations/redshift/utils.py
+++ mage_integrations/mage_integrations/destinations/redshift/utils.py
@@ -1,11 +1,14 @@
+import json
+from typing import Dict, List
+
 from mage_integrations.destinations.constants import (
     COLUMN_FORMAT_DATETIME,
     COLUMN_TYPE_OBJECT,
-    COLUMN_TYPE_STRING
+    COLUMN_TYPE_STRING,
 )
-from mage_integrations.destinations.sql.utils import convert_column_type as convert_column_type_orig
-from typing import Dict, List
-import json
+from mage_integrations.destinations.sql.utils import (
+    convert_column_type as convert_column_type_orig,
+)
 
 
 def convert_array(v: List, column_type_dict: Dict):
@@ -19,6 +22,7 @@ def convert_column_type(
 ) -> str:
     if COLUMN_TYPE_OBJECT == column_type:
         return 'VARCHAR(65535)'
-    if COLUMN_TYPE_STRING == column_type and COLUMN_FORMAT_DATETIME == column_settings.get('format'):
-        return 'TIMESTAMPTZ'
+    elif COLUMN_TYPE_STRING == column_type:
+        if COLUMN_FORMAT_DATETIME == column_settings.get('format'):
+            return 'TIMESTAMPTZ'
     return convert_column_type_orig(column_type, column_settings, **kwargs)
