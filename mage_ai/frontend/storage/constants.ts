2ec43d588d45a3adec3b9abed3d0f7ac4cf6e053
tommydangerous <quantumventuress@gmail.com>
2023-10-23 12:29:08 -0700

    sroll fix

---
+++ mage_ai/frontend/storage/constants.ts
@@ -0,0 +1,27 @@
+export const LOCAL_STORAGE_KEY_AUTOMATICALLY_NAME_BLOCKS = 'automatically_name_blocks';
+
+export const LOCAL_STORAGE_KEY_PIPELINE_EDIT_BEFORE_TAB_SELECTED =
+  'pipeline_edit_before_tab_selected';
+
+export const LOCAL_STORAGE_KEY_PIPELINE_EDIT_HIDDEN_BLOCKS =
+  'pipeline_edit_hidden_blocks';
+
+export const LOCAL_STORAGE_KEY_PIPELINE_EDIT_BLOCK_OUTPUT_LOGS =
+  'pipeline_edit_block_output_logs';
+
+export const LOCAL_STORAGE_KEY_SETUP_AI_LATER = 'setup_ai_later';
+
+export const LOCAL_STORAGE_KEY_AUTO_SCROLL_LOGS = 'auto_scroll_logs';
+
+export const LOCAL_STORAGE_KEY_DISPLAY_LOCAL_TIMEZONE = 'display_local_timezone';
+
+export const LOCAL_STORAGE_KEY_HIDE_PUBLIC_DEMO_WARNING = 'hide_public_demo_warning';
+
+export const LOCAL_STORAGE_KEY_DISPLAY_LOCAL_SERVER_TIME =
+  'display_local_server_time';
+
+export const LOCAL_STORAGE_KEY_INCLUDE_SERVER_TIME_SECONDS =
+  'include_server_time_seconds';
+
+export const LOCAL_STORAGE_KEY_PIPELINE_EDITOR_SIDE_BY_SIDE_ENABLED = 'pipeline_editor_side_by_side_enabled';
+export const LOCAL_STORAGE_KEY_PIPELINE_EDITOR_SIDE_BY_SIDE_SCROLL_TOGETHER = 'pipeline_editor_side_by_side_scroll_together';
