0824297f364ea3b28b1bdea0bdb85500356c7c8c
Annie Cheng <annieancheng@gmail.com>
2023-10-26 20:08:57 -0700

    [ac] Improve app header styling (#3849)
    * [ac] Improve app header styling
    
    * [ac] Make update button yellow

--- mage_ai/frontend/components/ServerTimeDropdown/ServerTimeButton.tsx
+++ mage_ai/frontend/components/ServerTimeDropdown/ServerTimeButton.tsx
@@ -1,6 +1,7 @@
 import { memo, useEffect, useRef } from 'react';
 
 import Text from '@oracle/elements/Text';
+import { BORDER_RADIUS_SMALL } from '@oracle/styles/units/borders';
 import { ButtonProps } from '@oracle/elements/Button';
 import { ButtonStyle } from './index.style';
 
@@ -31,12 +32,13 @@ function ServerTimeButton({
       {...props}
       active={active}
       borderLess
+      borderRadius={BORDER_RADIUS_SMALL}
       compact
       highlightOnHoverAlt
       ref={buttonRef}
       transparent
     >
-      <Text inline monospace>
+      <Text inline monospace small>
         {`${time} ${timeZone}`}
       </Text>
     </ButtonStyle>
