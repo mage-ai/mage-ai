e211cb67d66ece7d84af943895d6598e342855e6
Xu He <hexu90@gmail.com>
2023-10-29 22:05:30 -0700

    address comments

--- mage_ai/tests/ai/test_ai_functions.py
+++ mage_ai/tests/ai/test_ai_functions.py
@@ -15,30 +15,30 @@ EXPECTED_LOAD_BLOCK = '{"code": "load mysql"}'
 EXPECTED_FILTER_BLOCK = '{"code": "filter record"}'
 EXPECTED_EXPORT_BLOCK = '{"code": "export postgres"}'
 MOCK_TRANSFORMER_CLASSIFICATION = {
-    "role": "assistant",
-    "function_call": {
-        "name": "classify_description",
-        "arguments": "{\n  \"BlockType\": \"BlockType__transformer\",\n  "
-        "\"BlockLanguage\": \"BlockLanguage__python\",\n  "
-        "\"ActionType\": \"ActionType__filter\"\n}"
+    'role': 'assistant',
+    'function_call': {
+        'name': 'classify_description',
+        'arguments': '{\n  "BlockType": "BlockType__transformer",\n  '
+        '"BlockLanguage": "BlockLanguage__python",\n  '
+        '"ActionType": "ActionType__filter"\n}'
     }
 }
 MOCK_LOADER_FUNCTION_CLASSIFICATION = {
-    "role": "assistant",
-    "function_call": {
-        "name": "classify_description",
-        "arguments": "{\n  \"BlockType\": \"BlockType__data_loader\",\n  "
-        "\"BlockLanguage\": \"BlockLanguage__python\",\n  "
-        "\"DataSource\": \"DataSource__mysql\"\n}"
+    'role': 'assistant',
+    'function_call': {
+        'name': 'classify_description',
+        'arguments': '{\n  "BlockType": "BlockType__data_loader",\n  '
+        '"BlockLanguage": "BlockLanguage__python",\n  '
+        '"DataSource": "DataSource__mysql"\n}'
     }
 }
 MOCK_EXPORTERS_FUNCTION_CLASSIFICATION = {
-    "role": "assistant",
-    "function_call": {
-        "name": "classify_description",
-        "arguments": "{\n  \"BlockType\": \"BlockType__data_exporter\",\n  "
-        "\"BlockLanguage\": \"BlockLanguage__python\",\n  "
-        "\"DataSource\": \"DataSource__postgres\"\n}"
+    'role': 'assistant',
+    'function_call': {
+        'name': 'classify_description',
+        'arguments': '{\n  "BlockType": "BlockType__data_exporter",\n  '
+        '"BlockLanguage": "BlockLanguage__python",\n  '
+        '"DataSource": "DataSource__postgres"\n}'
     }
 }
 
@@ -74,47 +74,47 @@ class AIFunctionTest(TestCase):
                 self.__set_expected_future_result(MOCK_TRANSFORMER_CLASSIFICATION),
                 self.__set_expected_future_result(MOCK_EXPORTERS_FUNCTION_CLASSIFICATION)])
 
-        pipeine_description = "load data from mysql, filter out "
-        "records column age < 18, export it to postgres"
+        pipeine_description = 'load data from mysql, filter out \
+            records column age < 18, export it to postgres'
         blocks = asyncio.run(self.wizard.async_generate_pipeline_from_description(
             pipeine_description))
-        loader_block = blocks.get("1")
-        self.assertEqual(loader_block.get("block_type"), 'data_loader')
-        self.assertEqual(loader_block.get("configuration").get("data_source"), 'mysql')
-        transformer_block = blocks.get("2")
-        self.assertEqual(transformer_block.get("block_type"), 'transformer')
-        self.assertEqual(transformer_block.get("configuration").get("action_type"), 'filter')
-        self.assertTrue("1" in transformer_block.get("upstream_blocks"))
-        exporter_block = blocks.get("3")
-        self.assertEqual(exporter_block.get("block_type"), 'data_exporter')
-        self.assertEqual(exporter_block.get("configuration").get("data_source"), 'postgres')
-        self.assertTrue("2" in exporter_block.get("upstream_blocks"))
+        loader_block = blocks.get('1')
+        self.assertEqual(loader_block.get('block_type'), 'data_loader')
+        self.assertEqual(loader_block.get('configuration').get('data_source'), 'mysql')
+        transformer_block = blocks.get('2')
+        self.assertEqual(transformer_block.get('block_type'), 'transformer')
+        self.assertEqual(transformer_block.get('configuration').get('action_type'), 'filter')
+        self.assertTrue('1' in transformer_block.get('upstream_blocks'))
+        exporter_block = blocks.get('3')
+        self.assertEqual(exporter_block.get('block_type'), 'data_exporter')
+        self.assertEqual(exporter_block.get('configuration').get('data_source'), 'postgres')
+        self.assertTrue('2' in exporter_block.get('upstream_blocks'))
 
     def test_async_generate_block_with_description(self):
         self.wizard._LLMPipelineWizard__async_identify_function_parameters = Mock(
             return_value=self.__set_expected_future_result(MOCK_TRANSFORMER_CLASSIFICATION))
         upstream_blocks = [1, 2, 3]
-        customized_code = {"code": "Test code"}
+        customized_code = {'code': 'Test code'}
         self.wizard._LLMPipelineWizard__async_llm_call = MagicMock(
             return_value=self.__set_expected_future_result(customized_code))
         block = asyncio.run(self.wizard.async_generate_block_with_description(
-            "test block generation", upstream_blocks))
-        self.assertEqual(block.get("block_type"), 'transformer')
-        self.assertEqual(block.get("configuration").get("action_type"), 'filter')
-        self.assertListEqual(upstream_blocks, block.get("upstream_blocks"))
+            'test block generation', upstream_blocks))
+        self.assertEqual(block.get('block_type'), 'transformer')
+        self.assertEqual(block.get('configuration').get('action_type'), 'filter')
+        self.assertListEqual(upstream_blocks, block.get('upstream_blocks'))
 
     def test_async_generate_comment_for_block(self):
-        comment_line = "Return the sum of 1 + 1"
-        function_name = "calculator_function"
-        function_comments = {f"{function_name}": comment_line}
+        comment_line = 'Return the sum of 1 + 1'
+        function_name = 'calculator_function'
+        function_comments = {f'{function_name}': comment_line}
         self.wizard._LLMPipelineWizard__async_llm_call = MagicMock(
             return_value=self.__set_expected_future_result(function_comments))
         block = asyncio.run(self.wizard.async_generate_comment_for_block(
-            f"def {function_name}(self): \n    return 1+1"))
+            f'def {function_name}(self): \n    return 1+1'))
         self.assertTrue(comment_line in block)
 
     def test_async_generate_doc_for_block(self):
-        expected_value = "documentation"
+        expected_value = 'documentation'
         self.wizard._LLMPipelineWizard__async_llm_call = MagicMock(
             return_value=self.__set_expected_future_result(expected_value))
 
@@ -123,11 +123,11 @@ class AIFunctionTest(TestCase):
         self.assertEqual(expected_value, block_doc)
 
     def test_async_generate_doc_for_pipeline(self):
-        expected_value = "documentation"
+        expected_value = 'documentation'
         self.wizard._LLMPipelineWizard__async_llm_call = MagicMock(
             return_value=self.__set_expected_future_result(expected_value))
 
         doc_dict = asyncio.run(self.wizard.async_generate_doc_for_pipeline(
             self.pipeline.uuid))
-        self.assertEqual(expected_value, doc_dict.get("pipeline_doc"))
-        self.assertTrue(expected_value in doc_dict.get("block_docs"))
+        self.assertEqual(expected_value, doc_dict.get('pipeline_doc'))
+        self.assertTrue(expected_value in doc_dict.get('block_docs'))
