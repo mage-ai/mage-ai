4780b3887da62d185a88cfc5ec9f46442bee6a17
Johnson Kwok <johnson@mage.ai>
2023-11-02 15:02:52 -0700

    [jk] Add back pipelinesFiltered code

--- mage_ai/frontend/pages/pipelines/index.tsx
+++ mage_ai/frontend/pages/pipelines/index.tsx
@@ -205,6 +205,10 @@ function PipelineListPage() {
   const pipelines: PipelineType[] = useMemo(
     () => {
       let pipelinesFiltered = filterPipelinesBySearchText(data?.pipelines || []);
+      if (q?.[PipelineQueryEnum.TAG]) {
+        pipelinesFiltered = pipelinesFiltered
+          .filter(({ tags }) => tags.some(t => (q[PipelineQueryEnum.TAG]).includes(t)));
+      }
 
       return pipelinesFiltered;
     },
