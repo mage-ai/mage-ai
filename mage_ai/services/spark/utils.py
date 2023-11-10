a8bdf3b1e04947022a272a870fb445bdf7075cf0
tommydangerous <quantumventuress@gmail.com>
2023-11-10 14:13:00 -0800

    fix

--- mage_ai/services/spark/utils.py
+++ mage_ai/services/spark/utils.py
@@ -1,18 +1,21 @@
 from mage_ai.data_preparation.repo_manager import get_repo_config
-from mage_ai.services.spark.constants import ComputeService, SparkMaster
+from mage_ai.server.active_kernel import get_active_kernel_name
+from mage_ai.server.kernels import KernelName
+from mage_ai.services.spark.constants import ComputeServiceUUID, SparkMaster
+from mage_ai.shared.utils import is_spark_env
 
 
-def get_compute_service(repo_config=None) -> ComputeService:
+def get_compute_service(repo_config=None) -> ComputeServiceUUID:
     if not repo_config:
         repo_config = get_repo_config()
 
     if not repo_config:
         return None
 
-    if repo_config.spark_config:
-        if repo_config.emr_config:
-            return ComputeService.AWS_EMR
-        elif SparkMaster.LOCAL.value == repo_config.spark_config.get('spark_master'):
-            return ComputeService.STANDALONE_CLUSTER
+    if repo_config.emr_config and get_active_kernel_name() == KernelName.PYSPARK:
+        return ComputeServiceUUID.AWS_EMR
+    elif is_spark_env() and repo_config.spark_config and \
+            SparkMaster.LOCAL.value == repo_config.spark_config.get('spark_master'):
+        return ComputeServiceUUID.STANDALONE_CLUSTER
 
     return None
