f921edacf0713f596dbb7cb9080b9fa15b6e13a4
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-11-15 15:26:27 -0800

    [xy] Reenqueue the job if queue is empty. (#3996)

--- mage_ai/tests/orchestration/queue/test_process_queue.py
+++ mage_ai/tests/orchestration/queue/test_process_queue.py
@@ -1,6 +1,12 @@
+from unittest.mock import patch
+
 from mage_ai.orchestration.queue.config import QueueConfig
 from mage_ai.orchestration.queue.process_queue import JobStatus, ProcessQueue
 from mage_ai.tests.base_test import TestCase
+
+
+def run_block():
+    print('test run block')
 
 
 class ProcessQueueTests(TestCase):
@@ -17,17 +23,24 @@ class ProcessQueueTests(TestCase):
         self.queue.job_dict['block_run_3'] = JobStatus.COMPLETED
         self.queue.job_dict['block_run_4'] = JobStatus.CANCELLED
         self.queue.clean_up_jobs()
-        self.assertEqual(self.queue.job_dict['block_run_1'], JobStatus.QUEUED)
+        # queue is empty, thus 'block_run_1' is not in queue
+        self.assertFalse('block_run_1' in self.queue.job_dict)
         self.assertEqual(self.queue.job_dict['block_run_2'], 100)
         self.assertFalse('block_run_3' in self.queue.job_dict)
         self.assertFalse('block_run_4' in self.queue.job_dict)
 
-    def test_has_job(self):
+    @patch('mage_ai.orchestration.queue.process_queue.poll_job_and_execute')
+    def test_has_job(self, mock_poll_job_and_execute):
         self.queue.job_dict['block_run_1'] = JobStatus.QUEUED
         self.queue.job_dict['block_run_2'] = 100
         self.queue.job_dict['block_run_3'] = JobStatus.COMPLETED
         self.queue.job_dict['block_run_4'] = JobStatus.CANCELLED
+        # Queue is empty, thus return False
+        self.assertFalse(self.queue.has_job('block_run_1'))
+        # After enqueueing the job, the has_job method returns True
+        self.queue.enqueue('block_run_1', run_block)
         self.assertTrue(self.queue.has_job('block_run_1'))
+
         self.assertTrue(self.queue.has_job('block_run_2'))
         self.assertFalse(self.queue.has_job('block_run_3'))
         self.assertFalse(self.queue.has_job('block_run_4'))
