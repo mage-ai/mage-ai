882818ffb39c9dfe938d7fd22ebe315a660878fd
Michael Freeman <mfreeman451@gmail.com>
2023-11-15 13:15:56 -0600

    🧪 fixing nats test

--- mage_ai/tests/streaming/sources/test_nats_js.py
+++ mage_ai/tests/streaming/sources/test_nats_js.py
@@ -1,4 +1,4 @@
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import patch
 
 from mage_ai.streaming.sources.nats_js import NATSSource
 from mage_ai.tests.base_test import TestCase
@@ -6,54 +6,27 @@ from mage_ai.tests.base_test import TestCase
 
 class NATSTests(TestCase):
     def setUp(self):
-        self.nats_config = dict(
-            server_url='nats://localhost:4222',
-            subject='test_subject',
-            consumer_name='test_consumer',
-        )
-
-    @patch('nats.connect', new_callable=AsyncMock)
-    @patch('nats.aio.client.Client.jetstream', new_callable=AsyncMock)
-    @patch('nats.aio.client.JetStreamContext.add_stream', new_callable=AsyncMock)
-    @patch('nats.aio.client.JetStreamContext.pull_subscribe', new_callable=AsyncMock)
-    def test_full_workflow(
-        self,
-        mock_pull_subscribe,
-        mock_add_stream,
-        mock_jetstream,
-        mock_connect
-    ):
-        mock_pull_subscribe.return_value = MagicMock(fetch=AsyncMock())
-        source = NATSSource(self.nats_config)
-        source.init_client()
-        mock_connect.assert_called_once()
-        mock_jetstream.assert_called_once()
-        mock_add_stream.assert_called_once()
-        mock_pull_subscribe.assert_called_once()
+        self.nats_config = {
+            'server_url': 'nats://localhost:4222',
+            'subject': 'test_subject',
+            'consumer_name': 'test_consumer',
+        }
 
     def test_init(self):
         with patch.object(NATSSource, 'init_client') as mock_init_client:
             NATSSource(self.nats_config)
             mock_init_client.assert_called_once()
 
-    def test_batch_read(self):
-        mock_handler = MagicMock()
-        with patch.object(NATSSource, 'fetch_messages', return_value=['msg1', 'msg2']):
-            source = NATSSource(self.nats_config)
-            source.batch_read(mock_handler)
-            mock_handler.assert_called_once_with(['msg1', 'msg2'])
+    def test_init_invalid_config(self):
+        invalid_config = {
+            'subject': 'test_subject',
+            'consumer_name': 'test_consumer',
+        }
 
-    def test_read(self):
-        mock_handler = MagicMock()
-        with patch.object(
-            NATSSource,
-            'fetch_messages',
-            side_effect=[
-                ['msg1'],
-                [],
-                Exception('Done')
-            ]
-        ):
-            source = NATSSource(self.nats_config)
-            source.read(mock_handler)
-            mock_handler.assert_called_once_with('msg1')
+        with patch.object(NATSSource, 'init_client') as mock_init_client:
+            with self.assertRaises(Exception) as context:
+                NATSSource(invalid_config)
+            error_msg = "missing 1 required positional argument: 'server_url'"
+            error_msg += "This is a long error message"
+            self.assertIn(error_msg, str(context.exception))
+            self.assertEqual(mock_init_client.call_count, 0)
