fbd25b9f842a8c8a7fa7ac4de26dba4387ae1780
dy46 <david@mage.ai>
2023-11-16 09:38:07 -0800

    [dy] Disable flake workspace test

--- mage_ai/tests/api/endpoints/test_workspaces.py
+++ mage_ai/tests/api/endpoints/test_workspaces.py
@@ -4,10 +4,7 @@ from mage_ai.cluster_manager.constants import ClusterType
 from mage_ai.cluster_manager.kubernetes.workload_manager import WorkloadManager
 from mage_ai.cluster_manager.workspace.base import Workspace
 from mage_ai.data_preparation.repo_manager import get_repo_config
-from mage_ai.tests.api.endpoints.mixins import (
-    BaseAPIEndpointTest,
-    build_list_endpoint_tests,
-)
+from mage_ai.tests.api.endpoints.mixins import BaseAPIEndpointTest
 
 
 class WorkspaceAPIEndpointTest(BaseAPIEndpointTest):
@@ -41,15 +38,15 @@ class WorkspaceAPIEndpointTest(BaseAPIEndpointTest):
             await super().build_test_list_endpoint(*args, **kwargs)
 
 
-build_list_endpoint_tests(
-    WorkspaceAPIEndpointTest,
-    list_count=1,
-    resource='workspaces',
-    result_keys_to_compare=[
-        'name',
-        'namespace',
-        'project_uuid',
-        'instance',
-        'service_account_name',
-    ],
-)
+# build_list_endpoint_tests(
+#     WorkspaceAPIEndpointTest,
+#     list_count=1,
+#     resource='workspaces',
+#     result_keys_to_compare=[
+#         'name',
+#         'namespace',
+#         'project_uuid',
+#         'instance',
+#         'service_account_name',
+#     ],
+# )
