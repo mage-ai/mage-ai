1701cac2893978d8f40696e2f0fb40334477e1b4
Rares Popa <rares.popa@ni.com>
2023-12-07 19:02:20 +0200

    Added backend functionality for pipeline zip import

--- mage_ai/data_preparation/models/errors.py
+++ mage_ai/data_preparation/models/errors.py
@@ -9,5 +9,17 @@ class FileNotInProjectError(MageBaseException):
     pass
 
 
+class FileWriteError(MageBaseException):
+    pass
+
+
 class SerializationError(MageBaseException):
+    pass
+
+
+class PipelineZipTooLargeError(MageBaseException):
+    pass
+
+
+class InvalidPipelineZipError(MageBaseException):
     pass
