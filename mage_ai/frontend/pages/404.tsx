691e05ce18781ffa2e71a50115dc217eb3618061
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-13 07:32:10 +0700

    feat: count down from 5 before redirecting

--- mage_ai/frontend/pages/404.tsx
+++ mage_ai/frontend/pages/404.tsx
@@ -1,18 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { redirectToUrl } from '@utils/url';
-import { toast } from 'react-toastify';
+import Headline from '@oracle/elements/Headline';
+import Text from '@oracle/elements/Text';
+import Spacing from '@oracle/elements/Spacing';
 
 function NotFoundPage() {
+  const [secondsToRedirect, setSecondsToRedirect] = useState(5);
   useEffect(() => {
-    toast.error('The previous page does not exist.');
-    redirectToUrl('/pipelines');
-  }, []);
+    const i = setInterval(() => {
+      if (secondsToRedirect > 0) {
+        setSecondsToRedirect(secondsToRedirect - 1);
+      } else {
+        redirectToUrl('/pipelines');
+      }
+    }, 1000);
+
+    return () => clearInterval(i);
+  }, [secondsToRedirect]);
 
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, that page does not exist.</p>
-    </>
+    <main
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        marginTop: '16rem',
+      }}
+    >
+      <Spacing mb={1}>
+        <Headline level={1}>404 - Page Not Found</Headline>
+      </Spacing>
+      <Text>You will be redirected to the Pipelines dashboard in {secondsToRedirect} seconds.</Text>
+    </main>
   );
 }
 
