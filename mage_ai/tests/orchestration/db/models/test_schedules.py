d28a64a4b688db4aad60278fe742d2bafd99e4fc
dy46 <david@mage.ai>
2023-12-15 11:44:51 -0800

    [dy] Add __init__.py file

--- mage_ai/tests/orchestration/db/models/test_schedules.py
+++ mage_ai/tests/orchestration/db/models/test_schedules.py
@@ -108,7 +108,7 @@ class PipelineScheduleTests(DBTestCase):
                 'test_pipeline',
                 pipeline_schedule_id=pipeline_schedule.id,
             )
-        self.assertEqual(pipeline_schedule.pipeline_runs_count, 5)
+        self.assertEqual(pipeline_schedule.pipeline_runs_count, 8)
 
     def test_in_progress_runs_count(self):
         pipeline_schedule = PipelineSchedule.create(
