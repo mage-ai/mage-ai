5a397cf7a26836b54a470a6d14b59900c97918fa
Rares Popa <rares.popa@ni.com>
2023-12-21 16:03:36 +0200

    Added env vars for default credentials

--- mage_ai/server/default_credential_initializer.py
+++ mage_ai/server/default_credential_initializer.py
@@ -1,11 +1,12 @@
 from mage_ai.authentication.passwords import create_bcrypt_hash, generate_salt
 from mage_ai.orchestration.db.models.oauth import User
+from mage_ai.settings import DEFAULT_ADMIN_EMAIL, DEFAULT_ADMIN_PASSWORD
 
 def get_default_credentials(default_role):
     password_salt = generate_salt()
     return User.create(
-        email='admin@example.com',
-        password_hash=create_bcrypt_hash('admin', password_salt),
+        email=DEFAULT_ADMIN_EMAIL,
+        password_hash=create_bcrypt_hash(DEFAULT_ADMIN_PASSWORD, password_salt),
         password_salt=password_salt,
         roles_new=[default_role],
         username='admin',
