13ecabb39135904ad71344d7b7f5805da77ecd39
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2024-01-04 03:26:58 +0800

    [xy] Decrease image size by removing qdrant libraries (#4277)
    * [xy] Comment out qdrant libraries.
    
    * [xy] Update doc.

--- mage_ai/data_preparation/templates/data_exporters/qdrant.py
+++ mage_ai/data_preparation/templates/data_exporters/qdrant.py
@@ -5,7 +5,6 @@ from pandas import DataFrame
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.qdrant import Qdrant
-from sentence_transformers import SentenceTransformer
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
