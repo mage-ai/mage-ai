a7a3c6f90dabfb2ad8d0043c5eef24742dbcec39
wangxiaoyou1993 <xiaoyou@mage.ai>
2024-01-09 18:05:21 +0800

    [xy] Fix unit tests.

--- mage_ai/tests/data_preparation/models/test_project.py
+++ mage_ai/tests/data_preparation/models/test_project.py
@@ -32,6 +32,8 @@ class ProjectTest(ProjectPlatformMixin, AsyncDBTestCase):
                 self.assertEqual(project.version, VERSION)
                 self.assertFalse(project.root_project)
 
+                mock_get_repo_config.assert_not_called()
+                project.repo_config
                 mock_get_repo_config.assert_called_once_with(
                     repo_path=base_repo_path(),
                     root_project=False,
@@ -45,13 +47,14 @@ class ProjectTest(ProjectPlatformMixin, AsyncDBTestCase):
             'mage_ai.data_preparation.models.project.get_repo_config',
         ) as mock_get_repo_config:
             project = Project(root_project=True)
-
             self.assertEqual(project.name, 'test')
             self.assertEqual(project.repo_path, base_repo_path())
             self.assertEqual(project.settings, None)
             self.assertEqual(project.version, VERSION)
             self.assertTrue(project.root_project)
 
+            mock_get_repo_config.assert_not_called()
+            project.repo_config
             mock_get_repo_config.assert_called_once_with(
                 repo_path=base_repo_path(),
                 root_project=True,
