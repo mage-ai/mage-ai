e61f1f8c86db1d0fc10772a56ffb7d33882a9289
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-01-11 03:28:59 +0700

    feat: add test for deletion

--- mage_ai/frontend/tests/pipelines.spec.ts
+++ mage_ai/frontend/tests/pipelines.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from './base';
 
-test('create pipeline from overview page', async ({ page }) => {
+test('create_and_delete pipeline from "/overview"', async ({ page }) => {
   test.setTimeout(60000);
 
   await page.goto('/overview');
@@ -10,12 +10,22 @@ test('create pipeline from overview page', async ({ page }) => {
 
   const pathStr = await page.evaluate(() => document.location.pathname);
   const path = pathStr.split('/'); // ['','pipelines','<pipeline-id>','edit']
+  const pipelineName = path[2];
   expect(path[1]).toBe('pipelines');
   expect(path[3]).toBe('edit');
   await expect(page.locator('[id="__next"]')).toContainText(path[2]);
-
   await page.getByRole('link', { name: 'Pipelines' }).click();
-  await page.goto('/pipelines');
-  await page.goto('/pipelines?_limit=30');
-  await expect(page.getByRole('link', { name: path[2] })).toBeVisible();
+
+  page.on('dialog', async dialog => {
+    if (dialog.message() === `Are you sure you want to delete pipeline ${pipelineName}?`) {
+      await dialog.accept();
+    } else {
+      throw new Error(`Unexpected dialog: ${dialog.message()}`);
+    }
+  });
+  await expect(page.getByRole('cell', { name: pipelineName })).toBeVisible({ timeout: 10000 });
+  await page.getByRole('cell', { name: pipelineName }).click({ button: 'right' });
+  await expect(page.getByRole('menuitem', { name: 'Delete' })).toBeVisible();
+  await page.getByRole('menuitem', { name: 'Delete' }).click();
+  await expect(page.getByRole('cell', { name: pipelineName })).toBeHidden();
 });
