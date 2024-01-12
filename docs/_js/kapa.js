bb2f8d02d6d3417d9da83b8a2b02e43006edf2ce
Matt <59450879+mattppal@users.noreply.github.com>
2024-01-12 14:28:41 -0800

    rm js

--- docs/_js/kapa.js
+++
@@ -1,9 +0,0 @@
-<script async
-    src="https://widget.kapa.ai/kapa-widget.bundle.js"
-    data-website-id="62549f2a-0f02-4afa-ac65-35c21202699c"
-    data-project-name="Mage"
-    data-project-color="#4B4DDE"
-    data-project-logo="https://avatars.githubusercontent.com/u/69371472?s=280&v=4"
-    data-modal-disclaimer="This is a custom LLM for Mage with access to documentation (docs.mage.ai), open GitHub issues (github.com/mage-ai/mage-ai/issues), open pull requests (github.com/mage-ai/mage-ai/pulls) and previous forum questions on Slack (mageai.slack.com)"
-    data-modal-example-questions="How do I upgrade a Mage version?,How do I cancel a pipeline run?"
-></script>
