6c4ca0e18974a9ecb1e3cbf491fd8657c2a6de52
tommydangerous <wtf2>
2024-01-13 13:54:47 -0800

    fix

--- mage_ai/services/search/block_action_objects.py
+++ mage_ai/services/search/block_action_objects.py
@@ -25,27 +25,27 @@ async def search(query: str, ratio: float = None, limit: int = None) -> List:
     for object_type, mapping in cache.load_all_data().items():
         for uuid, block_action_object in mapping.items():
             text = get_searchable_text(object_type, block_action_object)
-            arr = [fuzz.token_sort_ratio(query, t) for t in text]
-            if arr:
-                score = max(arr)
+            if not text:
+                continue
 
-                if score >= (ratio / 2):
-                    DX_PRINTER.info(
-                        uuid,
-                        object_type=object_type,
-                        score=score,
-                        text=text,
-                    )
-
-                if score < ratio:
-                    continue
-
-                results.append(dict(
-                    block_action_object=block_action_object,
+            score = max([fuzz.token_sort_ratio(query, t) for t in text])
+            if score >= (ratio / 2):
+                DX_PRINTER.info(
+                    uuid,
                     object_type=object_type,
                     score=score,
-                    uuid=uuid,
-                ))
+                    text=text,
+                )
+
+            if score < ratio:
+                continue
+
+            results.append(dict(
+                block_action_object=block_action_object,
+                object_type=object_type,
+                score=score,
+                uuid=uuid,
+            ))
 
     DX_PRINTER.info(f'Cache: {len(results)}')
 
