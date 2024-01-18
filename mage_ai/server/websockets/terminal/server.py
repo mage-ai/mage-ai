bb713453d6271f445d5fda78d8d96947e5070539
DANGerous <quantumventuress@gmail.com>
2024-01-18 12:07:04 -0800

    [td] Remove terminal colors because the commands freeze pip installed Mage (#4398)
    Co-authored-by: tommydangerous <wtf2>

--- mage_ai/server/websockets/terminal/server.py
+++
@@ -1,7 +0,0 @@
-from mage_ai.server.terminal_server import TerminalWebsocketServer
-from mage_ai.server.websockets.base import BaseHandler
-from mage_ai.server.websockets.constants import Channel
-
-
-class Terminal(TerminalWebsocketServer, BaseHandler):
-    channel = Channel.TERMINAL
