05dfa56d2da6bb5b35569abdfba4d07dc3dbdc18
wangxiaoyou1993 <xiaoyou@mage.ai>
2024-01-25 01:38:34 -0800

    [xy] Fix some streams

--- mage_integrations/mage_integrations/sources/chargebee/streams/coupons.py
+++ mage_integrations/mage_integrations/sources/chargebee/streams/coupons.py
@@ -15,8 +15,8 @@ class CouponsStream(BaseChargebeeStream):
     SCHEMA = 'plan_model/coupons'
     SORT_BY = 'created_at'
 
-    def __init__(self, config, state, catalog, client):
-        BaseChargebeeStream.__init__(self, config, state, catalog, client)
+    def __init__(self, config, state, catalog, client, logger=None):
+        BaseChargebeeStream.__init__(self, config, state, catalog, client, logger=logger)
         if self.config.get('item_model'):
             self.SCHEMA = 'item_model/coupons'
 
