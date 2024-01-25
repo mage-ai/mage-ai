c9290744d10d57034ece3c1a28d8804d740fe7e5
tommydangerous <wtf2>
2024-01-24 17:15:09 -0800

    support io_config jinja

--- mage_ai/api/resources/DataProviderResource.py
+++ mage_ai/api/resources/DataProviderResource.py
@@ -4,6 +4,7 @@ import aiofiles
 import yaml
 
 from mage_ai.api.resources.GenericResource import GenericResource
+from mage_ai.data_preparation.models.block.content import template_render
 from mage_ai.io.base import DataSource
 from mage_ai.orchestration.db import safe_db_query
 from mage_ai.settings.repo import get_repo_path
@@ -47,7 +48,7 @@ class DataProviderResource(GenericResource):
         if os.path.exists(file_path):
             async with aiofiles.open(file_path, mode='r') as file:
                 content = await file.read()
-                profiles = list(yaml.safe_load(content).keys()) if content else []
+                profiles = list(yaml.safe_load(template_render(content)).keys()) if content else []
 
         collection = [dict(
             id=DATA_PROVIDERS_NAME[ds.value],
