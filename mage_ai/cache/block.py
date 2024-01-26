a2f0c75d44038fd104d440593698ad069559dd64
Johnson Kwok <johnson@mage.ai>
2024-01-26 12:52:47 -0800

    [jk] Clean up code

--- mage_ai/cache/block.py
+++ mage_ai/cache/block.py
@@ -169,10 +169,8 @@ class BlockCache(BaseCache):
             ) -> bool:
                 pipeline_dict = cache_dict.get('pipeline') or {}
 
-                return pipeline_dict.get('uuid') != pipeline_uuid or (
-                    pipeline_dict.get('uuid') == pipeline_uuid and
-                    pipeline_dict.get('repo_path') != repo_path
-                )
+                return pipeline_dict.get('uuid') != pipeline_uuid \
+                    or pipeline_dict.get('repo_path') != repo_path
 
             pipelines_arr = list(filter(__filter, pipelines_arr))
             pipelines_arr.append(build_pipeline_dict(
@@ -216,10 +214,8 @@ class BlockCache(BaseCache):
 
         def __filter(cache_dict: Dict, pipeline_uuid=pipeline_uuid, repo_path=repo_path) -> bool:
             pipeline_dict = cache_dict.get('pipeline') or {}
-            return pipeline_dict.get('uuid') != pipeline_uuid or (
-                pipeline_dict.get('uuid') == pipeline_uuid and
-                pipeline_dict.get('repo_path') != repo_path
-            )
+            return pipeline_dict.get('uuid') != pipeline_uuid \
+                or pipeline_dict.get('repo_path') != repo_path
 
         pipelines = (mapping.get(key) or {}).get('pipelines', [])
 
