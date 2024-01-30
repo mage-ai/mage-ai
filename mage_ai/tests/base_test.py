43003fa3fff5cdf34f684262d26ca8d21274a5d2
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2024-01-30 11:28:05 -0800

    [xy] Fix multi project flag. (#4490)
    * [xy] Fix multi project flag.
    
    * [xy] Update tests.

--- mage_ai/tests/base_test.py
+++ mage_ai/tests/base_test.py
@@ -9,7 +9,6 @@ from faker import Faker
 from mage_ai.data_preparation.repo_manager import init_project_uuid
 from mage_ai.orchestration.db import TEST_DB, db_connection
 from mage_ai.orchestration.db.database_manager import database_manager
-from mage_ai.settings.platform.constants import set_project_platform_activated_flag
 from mage_ai.settings.repo import get_variables_dir, set_repo_path
 from mage_ai.shared.logger import LoggingLevel
 
@@ -73,7 +72,6 @@ class DBTestCase(unittest.TestCase):
         if not Path(self.repo_path).exists():
             Path(self.repo_path).mkdir()
         init_project_uuid()
-        set_project_platform_activated_flag()
         database_manager.run_migrations(log_level=LoggingLevel.ERROR)
         db_connection.start_session()
 
@@ -115,7 +113,6 @@ class TestCase(unittest.TestCase):
         if not Path(self.repo_path).exists():
             Path(self.repo_path).mkdir()
         init_project_uuid()
-        set_project_platform_activated_flag()
 
     @classmethod
     def tearDownClass(self):
