a2e06feb1b2d168b441c44380ee22f283dacdded
wangxiaoyou1993 <xiaoyou@mage.ai>
2024-02-14 03:06:15 -0800

    [xy] Restrict langchain_community version.

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ setuptools.setup(
     extras_require={
         'ai': [
             'astor>=0.8.1',
-            'langchain>=0.0.222, <0.1.7',
+            'langchain>=0.0.222',
+            'langchain_community<0.0.20',
             'openai>=0.27.8, <1.0.0',
         ],
         'azure': [
@@ -165,7 +166,8 @@ setuptools.setup(
             'influxdb_client==1.36.1',
             'kafka-python==2.0.2',
             'kubernetes>=28.1.0',
-            'langchain>=0.0.222, <0.1.7',
+            'langchain>=0.0.222',
+            'langchain_community<0.0.20',
             'ldap3==2.9.1',
             'nats-py==2.6.0',
             'nkeys~=0.1.0',
