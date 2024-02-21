f7ded59b40275568b8a6215704b6b7fbbe5a0fb6
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2024-02-21 00:00:05 -0800

    [xy] Combine schedulers. (#4614)
    * [xy] Combine schedulers.
    
    * [xy] Update unit tests.

--- mage_ai/orchestration/pipeline_scheduler.py
+++ mage_ai/orchestration/pipeline_scheduler.py
@@ -1,6 +1,1 @@
-from mage_ai.settings.platform.constants import project_platform_activated
-
-if project_platform_activated():
-    from mage_ai.orchestration.pipeline_scheduler_project_platform import *
-else:
-    from mage_ai.orchestration.pipeline_scheduler_original import *
+from mage_ai.orchestration.pipeline_scheduler_original import *  # noqa: F401, F403
