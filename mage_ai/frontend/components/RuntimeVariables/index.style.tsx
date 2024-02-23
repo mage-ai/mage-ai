059eab1f888462511b4c37d0634be622d5a76278
Johnson Kwok <johnson@mage.ai>
2024-02-23 11:23:13 -0800

    [jk] Fix double scrollbar on pipeline's Triggers page

--- mage_ai/frontend/components/RuntimeVariables/index.style.tsx
+++ mage_ai/frontend/components/RuntimeVariables/index.style.tsx
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 
 import dark from '@oracle/styles/themes/dark';
+import { PADDING } from '@oracle/styles/units/spacing';
 
-export const ContainerStyle = styled.div<{ height: number }>`
+export const ContainerStyle = styled.div<{
+  height: number;
+  includePadding?: boolean;
+  overflow?: boolean;
+}>`
   display: flex;
   flex-direction: column;
   height: ${props => props.height}px;
   border-bottom: 1px solid ${dark.borders.medium};
+
+  ${({ includePadding }) => includePadding && `
+    padding: ${PADDING}px;
+  `}
+
+  ${({ overflow }) => overflow && `
+    overflow: auto;
+  `}
 `;
