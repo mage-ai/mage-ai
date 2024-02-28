3293f7df654a65cbf23c889de22b9ebd50a8db31
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-02-28 20:28:00 +0700

    feat: remove beta from assert visible text + revert visible timeout

--- mage_ai/frontend/tests/pages.spec.ts
+++ mage_ai/frontend/tests/pages.spec.ts
@@ -5,7 +5,7 @@ test('ensure all main pages load properly', async ({ page }) => {
     await page.locator('div[class*="indexstyle__VerticalNavigationStyleComponent"] > div').hover();
     await page.getByRole('link', { name: name }).first().click();
     await page.waitForLoadState();
-    await expect(page.getByText(name, { exact: true }).first()).toBeVisible({ timeout: VISIBLE_TIMEOUT });
+    await expect(page.getByText(name, { exact: true }).first()).toBeVisible();
   }
 
   await navigateToAndWaitTilLoaded('Overview');
@@ -17,6 +17,6 @@ test('ensure all main pages load properly', async ({ page }) => {
   await navigateToAndWaitTilLoaded('Templates');
   await navigateToAndWaitTilLoaded('Version control');
   await navigateToAndWaitTilLoaded('Terminal');
-  await navigateToAndWaitTilLoaded('Global hooks (beta)');
-  await navigateToAndWaitTilLoaded('Compute management (beta)');
+  await navigateToAndWaitTilLoaded('Global hooks');
+  await navigateToAndWaitTilLoaded('Compute management');
 });
