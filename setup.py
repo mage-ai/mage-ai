52049a041e5838989c26721054af7b028fe4eb09
wangxiaoyou1993 <xiaoyou@mage.ai>
2024-02-27 22:27:04 -0800

    [xy] Lock langchain version.

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ setuptools.setup(
             'influxdb_client==1.36.1',
             'kafka-python==2.0.2',
             'kubernetes>=28.1.0',
-            'langchain>=0.0.222',
+            'langchain==0.1.6',
             'langchain_community<0.0.20',
             'ldap3==2.9.1',
             'nats-py==2.6.0',
