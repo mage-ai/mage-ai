717006dd531f2c21e5a642ea6df6e0164fa0185a
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2024-03-11 14:56:05 -0700

    [xy] Remove Redshift stl insert query. (#4735)

--- mage_integrations/mage_integrations/destinations/redshift/__init__.py
+++ mage_integrations/mage_integrations/destinations/redshift/__init__.py
@@ -159,23 +159,10 @@ WHERE TABLE_NAME = '{table_name}' AND TABLE_SCHEMA = '{schema_name}'
                 drop_old_table_command,
             ]
 
-        if not self.is_redshift_serverless:
-            commands.append(
-                '\n'.join([
-                    'WITH last_queryid_for_table AS (',
-                    '    SELECT query, MAX(si.starttime) OVER () as last_q_stime, si.starttime as stime',   # noqa: E501
-                    '    FROM stl_insert si, SVV_TABLE_INFO sti',
-                    f'    WHERE sti.table_id=si.tbl AND sti."table"=\'{table_name}\'',
-                    ')',
-                    'SELECT SUM(rows) FROM stl_insert si, last_queryid_for_table lqt ',
-                    'WHERE si.query=lqt.query AND lqt.last_q_stime=stime',
-                ])
-            )
-        else:
-            # stl_insert table is not supported in Redshift Serverless
-            commands.append(
-                f'SELECT {len(records)} AS row_count'
-            )
+        # Not query data from stl_insert table anymore since it's inefficient.
+        commands.append(
+            f'SELECT {len(records)} AS row_count'
+        )
         return commands
 
     def full_table_name(self, schema_name: str, table_name: str, prefix: str = '') -> str:
