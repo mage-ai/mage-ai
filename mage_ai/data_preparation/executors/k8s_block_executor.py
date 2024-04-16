da00280d30030f8b3b787d9390d70995a87712c1
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2024-04-16 14:33:18 -0700

    [xy] Support interpolating trigger name in k8s job name. (#4947)
    * [xy] Support interpolating trigger name in k8s job name.
    
    * [xy] Fix name

--- mage_ai/data_preparation/executors/k8s_block_executor.py
+++ mage_ai/data_preparation/executors/k8s_block_executor.py
@@ -4,10 +4,13 @@ from jinja2 import Template
 
 from mage_ai.data_preparation.executors.block_executor import BlockExecutor
 from mage_ai.data_preparation.shared.utils import get_template_vars
+from mage_ai.orchestration.db import safe_db_query
+from mage_ai.orchestration.db.models.schedules import BlockRun
 from mage_ai.services.k8s.config import K8sExecutorConfig
 from mage_ai.services.k8s.constants import DEFAULT_NAMESPACE
 from mage_ai.services.k8s.job_manager import JobManager as K8sJobManager
 from mage_ai.shared.hash import merge_dict
+from mage_ai.shared.utils import clean_name
 
 
 class K8sBlockExecutor(BlockExecutor):
@@ -15,10 +18,13 @@ class K8sBlockExecutor(BlockExecutor):
 
     def __init__(self, pipeline, block_uuid: str, execution_partition: str = None, **kwargs):
         super().__init__(pipeline, block_uuid, execution_partition=execution_partition)
-        self.executor_config = self.pipeline.repo_config.k8s_executor_config or dict()
+        self.executor_config_dict = self.pipeline.repo_config.k8s_executor_config or dict()
         if self.block.executor_config is not None:
-            self.executor_config = merge_dict(self.executor_config, self.block.executor_config)
-        self.executor_config = K8sExecutorConfig.load(config=self.executor_config)
+            self.executor_config_dict = merge_dict(
+                self.executor_config_dict,
+                self.block.executor_config,
+            )
+        self.executor_config = K8sExecutorConfig.load(config=self.executor_config_dict)
 
     def _execute(
         self,
@@ -26,10 +32,7 @@ class K8sBlockExecutor(BlockExecutor):
         global_vars: Dict = None,
         **kwargs,
     ) -> None:
-        if not self.executor_config.job_name_prefix:
-            job_name_prefix = 'data-prep'
-        else:
-            job_name_prefix = self.executor_config.job_name_prefix
+        job_name_prefix = self._get_job_name_prefix(block_run_id)
 
         if self.executor_config.namespace:
             namespace = Template(self.executor_config.namespace).render(
@@ -54,3 +57,23 @@ class K8sBlockExecutor(BlockExecutor):
             cmd,
             k8s_config=self.executor_config,
         )
+
+    @safe_db_query
+    def _get_job_name_prefix(
+        self,
+        block_run_id,
+    ):
+        if not self.executor_config.job_name_prefix:
+            job_name_prefix = 'data-prep'
+        else:
+            job_name_prefix = self.executor_config.job_name_prefix
+        if not block_run_id:
+            return job_name_prefix
+
+        if '{trigger_name}' in job_name_prefix:
+            block_run = BlockRun.query.get(block_run_id)
+            trigger = block_run.pipeline_run.pipeline_schedule
+            job_name_prefix = job_name_prefix.format(
+                trigger_name=clean_name(trigger.name).replace('_', '-'))
+
+        return job_name_prefix
--- mage_ai/data_preparation/executors/k8s_pipeline_executor.py
+++ mage_ai/data_preparation/executors/k8s_pipeline_executor.py
@@ -6,19 +6,25 @@ from jinja2 import Template
 from mage_ai.data_preparation.executors.pipeline_executor import PipelineExecutor
 from mage_ai.data_preparation.models.pipeline import Pipeline
 from mage_ai.data_preparation.shared.utils import get_template_vars
+from mage_ai.orchestration.db import safe_db_query
+from mage_ai.orchestration.db.models.schedules import PipelineRun
 from mage_ai.services.k8s.config import K8sExecutorConfig
 from mage_ai.services.k8s.constants import DEFAULT_NAMESPACE
 from mage_ai.services.k8s.job_manager import JobManager as K8sJobManager
 from mage_ai.shared.hash import merge_dict
+from mage_ai.shared.utils import clean_name
 
 
 class K8sPipelineExecutor(PipelineExecutor):
     def __init__(self, pipeline: Pipeline, execution_partition: str = None):
         super().__init__(pipeline, execution_partition=execution_partition)
-        self.executor_config = self.pipeline.repo_config.k8s_executor_config or dict()
+        self.executor_config_dict = self.pipeline.repo_config.k8s_executor_config or dict()
         if self.pipeline.executor_config is not None:
-            self.executor_config = merge_dict(self.executor_config, self.pipeline.executor_config)
-        self.executor_config = K8sExecutorConfig.load(config=self.executor_config)
+            self.executor_config_dict = merge_dict(
+                self.executor_config_dict,
+                self.pipeline.executor_config,
+            )
+        self.executor_config = K8sExecutorConfig.load(config=self.executor_config_dict)
 
     def cancel(
         self,
@@ -63,10 +69,7 @@ class K8sPipelineExecutor(PipelineExecutor):
     ) -> K8sJobManager:
         if global_vars is None:
             global_vars = dict()
-        if not self.executor_config.job_name_prefix:
-            job_name_prefix = 'data-prep'
-        else:
-            job_name_prefix = self.executor_config.job_name_prefix
+        job_name_prefix = self._get_job_name_prefix(pipeline_run_id)
 
         if self.executor_config.namespace:
 
@@ -83,3 +86,23 @@ class K8sPipelineExecutor(PipelineExecutor):
             logging_tags=kwargs.get('tags', dict()),
             namespace=namespace,
         )
+
+    @safe_db_query
+    def _get_job_name_prefix(
+        self,
+        pipeline_run_id,
+    ):
+        if not self.executor_config.job_name_prefix:
+            job_name_prefix = 'data-prep'
+        else:
+            job_name_prefix = self.executor_config.job_name_prefix
+        if not pipeline_run_id:
+            return job_name_prefix
+
+        if '{trigger_name}' in job_name_prefix:
+            pipeline_run = PipelineRun.query.get(pipeline_run_id)
+            trigger = pipeline_run.pipeline_schedule
+            job_name_prefix = job_name_prefix.format(
+                trigger_name=clean_name(trigger.name).replace('_', '-'))
+
+        return job_name_prefix
