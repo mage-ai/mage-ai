4241596894fd7cda3b8c8623c2201279c482a398
mager <supreme@mage.ai>
2024-05-02 07:10:15 -0700

    [td] Improved module reloading

--- mage_ai/utils/code.py
+++ mage_ai/utils/code.py
@@ -1,90 +1,42 @@
 import importlib
-import re
-from typing import Tuple
+import pkgutil
+import sys
+from typing import Set
 
-from mage_ai.autocomplete.utils import extract_all_imports
 from mage_ai.settings.repo import get_repo_path
 
 
+def reload_module_and_submodules(module_name: str, reloaded_modules: Set[str]) -> None:
+    if module_name in sys.modules and module_name not in reloaded_modules:
+        try:
+            print(f"Attempting to reload: {module_name}...")  # Indicate reloading attempt
+            module = importlib.reload(sys.modules[module_name])
+            reloaded_modules.add(module_name)
+            print(f"Successfully reloaded: {module_name}")
+        except Exception as e:
+            print(f"Failed to reload: {module_name}. Error: {e}")
+
+        if hasattr(module, "__path__"):  # If the module is a package, reload its submodules
+            print(f"Checking submodules of: {module_name}...")
+            for loader, name, is_pkg in pkgutil.walk_packages(module.__path__):
+                full_name = module.__name__ + '.' + name
+                print(f"Attempting to reload submodule: {full_name}...")
+                reload_module_and_submodules(full_name, reloaded_modules)
+
+
 def reload_all_repo_modules(content: str) -> None:
-    parts = get_repo_path().split('/')
-    project_name = parts[-1]
+    print("Starting the module reloading process...")
+    repo_path_parts = get_repo_path().split('/')
+    project_name = repo_path_parts[-1]
+    reloaded_modules = set()
 
-    for line in extract_all_imports(content):
-        if f'import {project_name}' not in line and f'from {project_name}' not in line:
-            continue
+    for line in content.splitlines():
+        if f'import {project_name}' in line or f'from {project_name}' in line:
+            line_parts = line.replace('import ', '').replace('from ', '').split()
+            module_name = line_parts[0]
+            reload_module_and_submodules(module_name, reloaded_modules)
 
-        regex = re.compile(r'from ([\w.]+) import|import ([\w.]+)')
-        matches = re.findall(regex, line)
-
-        if len(matches) >= 1:
-            m1, m2 = matches[0]
-            try:
-                importlib.reload(importlib.import_module(m1 or m2))
-            except Exception:
-                pass
-
-
-def extract_decorated_function(code: str, decorated_function_name: str) -> Tuple:
-    spans = []
-
-    span_current_start = None
-    span_current_def = None
-
-    lines = code.split('\n')
-    number_of_lines = len(lines)
-
-    for idx, line in enumerate(lines):
-        if line and \
-                span_current_start is not None and \
-                span_current_def is not None:
-
-            if line.startswith('@') or line.startswith('def '):
-                spans.append((
-                    span_current_start,
-                    idx - 1,
-                ))
-                span_current_start = None
-                span_current_def = None
-
-        if line and \
-                span_current_start is not None and \
-                span_current_def is None and \
-                line.startswith('def '):
-
-            span_current_def = idx
-
-        if line and \
-                span_current_start is None and \
-                line.startswith(f'@{decorated_function_name}'):
-
-            span_current_start = idx
-
-        if span_current_start is not None and \
-                span_current_def is not None and \
-                idx == number_of_lines - 1:
-
-            spans.append((span_current_start, idx))
-
-    lines_left = []
-
-    spans_len = len(spans)
-    for idx, span in enumerate(spans):
-        s, e = span
-
-        if idx == 0 and s >= 1:
-            lines_left.append('\n'.join(lines[:s]))
-
-        s_n = None
-        # Last span
-        if idx == spans_len - 1:
-            s_n = -1
-        else:
-            s_n, e_n = spans[idx + 1]
-
-        lines_left.append('\n'.join(lines[e:s_n]))
-
-    return (
-        ['\n'.join(lines[s:e]) for s, e in spans],
-        lines_left,
+    # After all reloading attempts
+    print(
+        f'Reloading process completed. Total modules/submodules reloaded: {len(reloaded_modules)}',
     )
