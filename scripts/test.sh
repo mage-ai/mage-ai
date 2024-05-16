524a60ffe4375ad964c6f4df142d912a7e2faa8d
mager <supreme@mage.ai>
2024-05-16 13:40:09 -0700

    more

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,23 @@
-HOST='' \
-PORT='' \
-PROJECT='' \
-docker compose exec app yarn type
+#!/bin/bash
+
+git diff --name-only master...HEAD | \
+  grep -v '^mage_ai/server/frontend_dist/' | \
+  grep -v '^mage_ai/server/frontend_dist_base_path_template/' | \
+  grep -E '\.(js|jsx|ts|tsx)$' | \
+  xargs prettier --config mage_ai/frontend/.prettierrc --write
+
+git diff --name-only master...HEAD | \
+  grep -v '^mage_ai/server/frontend_dist/' | \
+  grep -v '^mage_ai/server/frontend_dist_base_path_template/' | \
+  grep -E '\.(js|jsx|ts|tsx)$' | \
+  xargs eslint_d --fix --config mage_ai/frontend/.eslintrc.js
+
+git diff --name-only master...HEAD | \
+  grep -v '^mage_ai/server/frontend_dist/' | \
+  grep -v '^mage_ai/server/frontend_dist_base_path_template/' | \
+  xargs poetry run pre-commit run --show-diff-on-failure --files
+
+# HOST='' \
+# PORT='' \
+# PROJECT='' \
+# docker compose exec app yarn type
