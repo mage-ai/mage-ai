2cf80ddd2f0f0b82ee65552c1c773f650f950302
mager <supreme@mage.ai>
2024-05-26 13:52:04 -0700

    summarizer tests

--- mage_ai/data_preparation/models/block/dynamic/counter.py
+++ mage_ai/data_preparation/models/block/dynamic/counter.py
@@ -1,14 +1,21 @@
-from typing import Any, Iterable, List, Optional
+from typing import Any, Iterable, List, Optional, Sequence
 
 from mage_ai.data.tabular.reader import read_metadata
 from mage_ai.data_preparation.models.block.dynamic.constants import (
     CHILD_DATA_VARIABLE_UUID,
 )
+from mage_ai.data_preparation.models.block.dynamic.utils import (
+    is_dynamic_block,
+    is_dynamic_block_child,
+    should_reduce_output,
+)
 from mage_ai.data_preparation.models.variables.cache import VariableAggregateCache
 from mage_ai.data_preparation.models.variables.constants import (
     VariableAggregateDataType,
+    VariableAggregateSummaryGroupType,
 )
-from mage_ai.data_preparation.models.variables.summarizer import (  # dynamic_block_index_paths,
+from mage_ai.data_preparation.models.variables.summarizer import (
+    dynamic_block_index_paths,
     get_aggregate_summary_info,
     get_part_uuids,
 )
@@ -18,13 +25,21 @@ class DynamicItemCounter:
     def __init__(
         self,
         block: Any,
-        variable_uuid: Optional[str] = None,
+        dynamic_block_index: Optional[int] = None,
         partition: Optional[str] = None,
+        variable_uuid: Optional[str] = None,
     ):
         self.block = block
+        self.dynamic_block_index = dynamic_block_index
         self.variable_uuid = variable_uuid or CHILD_DATA_VARIABLE_UUID
         self.partition = partition
+
+        self.dynamic = is_dynamic_block(self.block)
+        self.dynamic_child = is_dynamic_block_child(self.block)
+        self.reduce_output = should_reduce_output(self.block)
+
         self._output = None
+        self._summary_information = None
 
     @property
     def variable_manager(self):
@@ -35,10 +50,51 @@ class DynamicItemCounter:
         return self.block.pipeline
 
     @property
+    def output(self):
+        if self._output is not None:
+            return self._output
+
+        self._output = self.variable.read_data()
+        return self._output
+
+    @property
     def variable(self):
         return self.variable_manager.get_variable_object(
             self.pipeline.uuid, self.block.uuid, self.variable_uuid, partition=self.partition
         )
+
+    def item_count(self) -> int:
+        return 0
+
+
+class DynamicBlockItemCounter(DynamicItemCounter):
+    @property
+    def summary_information(self) -> Optional[VariableAggregateCache]:
+        if self._summary_information is not None:
+            return self._summary_information
+
+        self._summary_information = get_aggregate_summary_info(
+            self.block.variable_manager,
+            pipeline_uuid=self.block.pipeline.uuid,
+            block_uuid=self.block.uuid,
+            variable_uuid=CHILD_DATA_VARIABLE_UUID,
+            data_type=VariableAggregateDataType.STATISTICS,
+            partition=self.partition,
+        )
+        return self._summary_information
+
+    @property
+    def part_uuids(self) -> Optional[Sequence]:
+        return get_part_uuids(self.variable)
+
+    @property
+    def num_rows_from_parquet_metadata(self) -> Optional[int]:
+        metadata = read_metadata(self.variable.variable_path)
+        if metadata:
+            num_rows = metadata.get('num_rows') or 0
+            if num_rows is not None and str(num_rows).isdigit():
+                return int(str(num_rows))
+        return 0
 
     def item_count(self) -> int:
         """
@@ -81,76 +137,93 @@ class DynamicItemCounter:
 
         return 0
 
+
+class DynamicChildItemCounter(DynamicItemCounter):
     @property
     def summary_information(self) -> Optional[VariableAggregateCache]:
-        return get_aggregate_summary_info(
+        if self._summary_information is not None:
+            return self._summary_information
+
+        self._summary_information = get_aggregate_summary_info(
             self.block.variable_manager,
             pipeline_uuid=self.block.pipeline.uuid,
             block_uuid=self.block.uuid,
             variable_uuid=CHILD_DATA_VARIABLE_UUID,
             data_type=VariableAggregateDataType.STATISTICS,
+            group_type=VariableAggregateSummaryGroupType.DYNAMIC,
             partition=self.partition,
         )
+        return self._summary_information
 
     @property
-    def part_uuids(self) -> Optional[List[str]]:
-        return get_part_uuids(self.variable)
+    def part_uuids(self) -> Optional[Sequence]:
+        return dynamic_block_index_paths(self.variable)
 
-    @property
-    def num_rows_from_parquet_metadata(self) -> Optional[int]:
-        metadata = read_metadata(self.variable.variable_path)
-        if metadata:
-            num_rows = metadata.get('num_rows') or 0
-            if num_rows is not None and str(num_rows).isdigit():
-                return int(str(num_rows))
+    def item_count(self) -> int:
+        """
+        1. Similar to counting the number of parts,
+        count the number of dynamic child block directories
+            block_uuid/
+                0/
+                    output_0/
+                        filename.extension
+        2. If dynamic child block reduces output, the count is 1
+        """
+        if should_reduce_output(self.block):
+            return 1
+
+        if self.part_uuids is not None:
+            return len(self.part_uuids)
+
+        if self.output is not None and isinstance(self.output, Iterable):
+            return sum(1 for _ in self.output)
+
         return 0
 
+
+class DynamicDuoItemCounter(DynamicItemCounter):
     @property
-    def output(self):
-        if self._output is not None:
-            return self._output
+    def indexes(self) -> List[int]:
+        dynamic_child_counter = DynamicChildItemCounter(
+            self.block,
+            partition=self.partition,
+            variable_uuid=self.variable_uuid,
+        )
+        return [i for i in range(dynamic_child_counter.item_count())]
 
-        self._output = self.variable.read_data()
-        return self._output
+    def item_count(self) -> int:
+        """
+        If block is dynamic and a dynamic child,
+        the count is the sum of dynamic_output_item_count() across all dynamic child blocks:
+            0/
+                -> [1, 2, 3]
+            1/
+                -> [4, 5]
+            2/
+                -> [6, 7, 8]
+        Count: 8
 
+        After a block completes, the statistics.json must be created, aggregated, and mapped:
 
-def dynamic_child_output_item_count() -> int:
-    """
-    1. Similar to counting the number of parts, count the number of dynamic child block directories
         block_uuid/
             0/
                 output_0/
-                    filename.extension
-    2. If dynamic child block reduces output, the count is 1
-    """
-    pass
-
-
-def multi_dynamic_output_item_count() -> int:
-    """
-    If block is dynamic and a dynamic child,
-    the count is the sum of dynamic_output_item_count() across all dynamic child blocks:
-        0/
-            -> [1, 2, 3]
-        1/
-            -> [4, 5]
-        2/
-            -> [6, 7, 8]
-    Count: 8
-
-    After a block completes, the statistics.json must be created, aggregated, and mapped:
-
-    block_uuid/
-        0/
-            output_0/
-                0/
-                    data.parquet
+                    0/
+                        data.parquet
+                        statistics.json
+                    1/
+                output_1/
+            1/
+                output_0/
+                    data.json
                     statistics.json
-                1/
-            output_1/
-        1/
-            output_0/
-                data.json
-                statistics.json
-    """
-    pass
+        """
+        return sum([
+            DynamicBlockItemCounter(
+                self.block,
+                dynamic_block_index=dynamic_block_index,
+                partition=self.partition,
+                variable_uuid=self.variable_uuid,
+            ).item_count()
+            for dynamic_block_index in self.indexes
+        ])
