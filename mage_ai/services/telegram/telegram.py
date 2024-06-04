0086e39e234cba7d4a35e8c053df400d536bce59
Ayumi08 <ayuminakayama265@gmail.com>
2024-06-03 21:52:37 -0700

    fixed issue #4877

--- mage_ai/services/telegram/telegram.py
+++ mage_ai/services/telegram/telegram.py
@@ -4,12 +4,10 @@ from mage_ai.services.telegram.config import TelegramConfig
 
 
 def send_telegram_message(config: TelegramConfig, message: str, title: str) -> None:
-    summary = """
-    <b>{title}</b>
-
-
-    {message}
-    """.format(title=title, message=message)
+    summary = (
+        "<b>{title}</b>\n\n"
+        "{message}"
+    ).format(title=title, message=message)
 
     payload = {
         "text": summary,
