bc9e122f463df3b1c6a7585048bd3bf1095467ac
mager <supreme@mage.ai>
2024-06-11 06:34:48 -0700

    fix

--- mage_ai/frontend/pages/_app.tsx
+++ mage_ai/frontend/pages/_app.tsx
@@ -11,13 +11,13 @@ import 'react-toastify/dist/ReactToastify.min.css';
 function MainApp({ version, ...props }: { mode: ModeEnum; version?: string } & AppProps) {
   if (version === 'v2') {
     const AppV2 = dynamic(() => import('@components/NextAppV2'));
-
+    // @ts-ignore
     return <AppV2 {...props} />;
   }
 
   // @ts-ignore
   const AppV1 = dynamic(() => import('@components/NextAppV1'));
-
+  // @ts-ignore
   return <AppV1 {...props} />;
 }
 
