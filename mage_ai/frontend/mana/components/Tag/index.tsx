2ddfee94f5990174df788e0e7fb642848d91e5fb
mager <supreme@mage.ai>
2024-06-11 06:29:46 -0700

    fix

--- mage_ai/frontend/mana/components/Tag/index.tsx
+++ mage_ai/frontend/mana/components/Tag/index.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 
-type TagProps = {
-  children: React.ReactNode;
+import styled from 'styled-components';
+import { PaddingVerticalEnum } from '@mana/themes/interactive';
+import { baseXs } from '../../styles/typography';
+
+type StyleProps = {
   inverted?: boolean;
   passthrough?: boolean;
   secondary?: boolean;
 };
 
-function Tag({ children, inverted, passthrough, secondary }: TagProps) {
-  const tagClasses = [
-    styles.tag,
-    inverted ? styles['tag--inverted'] : '',
-    secondary ? styles['tag--secondary'] : '',
-    passthrough ? styles['tag--passthrough'] : '',
-  ].join(' ');
+const TagStyled = styled.div<StyleProps>`
+  ${baseXs}
 
-  return <div className={tagClasses}>{children}</div>;
+  background-color: ${({ inverted, secondary, theme }) =>
+    inverted
+      ? theme.colors.whiteLo
+      : secondary
+        ? theme.backgrounds.button.base.default
+        : theme.colors.whiteHi};
+  border-radius: ${({ theme }) => theme.borders.radius.round};
+  color: ${({ inverted, theme }) =>
+    inverted ? theme.fonts.color.text.inverted : theme.fonts.color.text.base};
+  cursor: inherit;
+  display: inline-block;
+  font-family: ${({ theme }) => theme.fonts.family.base.semiBold};
+  padding: 4px ${PaddingVerticalEnum.XS};
+  pointer-events: ${({ passthrough }) => (passthrough ? 'inherit' : 'auto')};
+`;
+
+function Tag({ children, ...props }: { children: React.ReactNode } & StyleProps) {
+  return <TagStyled {...props}>{children}</TagStyled>;
 }
 
 export default Tag;
