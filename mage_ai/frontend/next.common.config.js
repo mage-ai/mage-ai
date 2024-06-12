be09345ec71a5bbed669841f0adf2ea6de0a6832
mager <supreme@mage.ai>
2024-06-12 01:25:19 -0700

    fix

--- mage_ai/frontend/next.common.config.js
+++ mage_ai/frontend/next.common.config.js
@@ -1,16 +1,5 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const removeImports = require('next-remove-imports')();
-const path = require('path');
-const webpack = require('webpack');
-
-const editorFiles = [
-  '@codingame/monaco-vscode-monarch-service-override/index.js',
-  '@codingame/monaco-vscode-python-default-extension/index.js',
-  'monaco-editor-wrapper/dist/index.js',
-  'monaco-editor-wrapper/dist/workerFactory.js',
-  'monaco-editor/esm/vs/editor/editor.main.js',
-];
 
 module.exports = removeImports({
   eslint: {
@@ -29,10 +18,6 @@ module.exports = removeImports({
   // This does not happen in production builds.
   reactStrictMode: String(process.env.NEXT_PUBLIC_REACT_STRICT_MODE) !== '0',
   webpack: (config, { isServer }) => {
-    if (!config.optimization) {
-      config.optimization = {};
-    }
-
     if (!isServer) {
       config.plugins.push(
         new MonacoWebpackPlugin({
@@ -55,78 +40,6 @@ module.exports = removeImports({
         },
         test: /\.worker\.ts$/,
       });
-
-      // This makes the app take forever to load on localhost:3000
-      // config.module.rules.push({
-      //   include: [
-      //     path.resolve(__dirname, 'node_modules/monaco-editor'),
-      //     path.resolve(__dirname, 'node_modules/react-dom'),
-      //     path.resolve(__dirname, 'node_modules/elkjs'),
-      //   ],
-      //   test: /\.(js|mjs|jsx|ts|tsx)$/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: [['next/babel']],
-      //       compact: true,
-      //     },
-      //   },
-      // });
-
-      if (process.env.IGNORE_EDITOR_NODE_MODULES) {
-        // editorFiles.forEach(file => {
-        //   config.plugins.push(
-        //     new webpack.IgnorePlugin({
-        //       resourceRegExp: new RegExp(file),
-        //     }),
-        //   );
-        // });
-      } else {
-        // These files are already minimized, they are just very large e.g. +20mbs
-        // const includeRegex = new RegExp(
-        //   editorFiles.map(file => file.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'),
-        // );
-        // config.optimization.minimizer = [
-        //   new TerserPlugin({
-        //     exclude: /node_modules\/next\/dist\/compiled\/terser\/bundle\.min\.js/,
-        //     include: includeRegex,
-        //     terserOptions: {
-        //       format: {
-        //         comments: false,
-        //       },
-        //     },
-        //     extractComments: false,
-        //   }),
-        // ];
-        // // Optionally add aliases to make sure modules point to the right paths
-        // const alias = {
-        //   'monaco-editor/esm/vs/editor/editor.main.js': path.resolve(
-        //     __dirname,
-        //     'node_modules/monaco-editor/esm/vs/editor/editor.main.js',
-        //   ),
-        //   'monaco-editor-wrapper/dist/index.js': path.resolve(
-        //     __dirname,
-        //     'node_modules/monaco-editor-wrapper/dist/index.js',
-        //   ),
-        //   'monaco-editor-wrapper/dist/workerFactory.js': path.resolve(
-        //     __dirname,
-        //     'node_modules/monaco-editor-wrapper/dist/workerFactory.js',
-        //   ),
-        //   '@codingame/monaco-vscode-python-default-extension/index.js': path.resolve(
-        //     __dirname,
-        //     'node_modules/@codingame/monaco-vscode-python-default-extension/index.js',
-        //   ),
-        //   '@codingame/monaco-vscode-monarch-service-override/index.js': path.resolve(
-        //     __dirname,
-        //     'node_modules/@codingame/monaco-vscode-monarch-service-override/index.js',
-        //   ),
-        // };
-        // // Update Webpack's resolve.alias property
-        // config.resolve.alias = {
-        //   ...config.resolve.alias,
-        //   ...alias,
-        // };
-      }
     }
 
     return config;
