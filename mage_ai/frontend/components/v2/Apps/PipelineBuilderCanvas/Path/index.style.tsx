0365ac1dc4a25675eefe1bea9b83df20bd4dd580
mager <supreme@mage.ai>
2024-06-16 21:56:23 -0700

    fix

--- mage_ai/frontend/components/v2/Apps/PipelineBuilderCanvas/Path/index.style.tsx
+++
@@ -1,7 +0,0 @@
-import styles from '@styles/scss/layout/Canvas/Paths/ConnectionsPath.module.scss';
-import { withStyles } from '@mana/hocs/withStyles';
-
-export const ConnectionsPathStyled = withStyles(styles, {
-  HTMLTag: 'svg',
-  classNames: ['connections-path'],
-});
