f7f999b81ddd3154f7b359f98405fe6f008227f8
mager <supreme@mage.ai>
2024-06-16 22:01:56 -0700

    fix

---
+++ mage_ai/frontend/components/v2/Canvas/Nodes/Block/index.tsx
@@ -0,0 +1,39 @@
+import { PortSubtypeEnum } from '../../types';
+
+type AsideType = {
+  after?: {
+    Icon?: string;
+    color?: string;
+  };
+  before?: {
+    Icon?: string;
+    color?: string;
+  };
+};
+
+type ConnectionsType = {
+  type: PortSubtypeEnum;
+};
+
+type BadgeType = {
+  color?: string;
+  label?: string;
+};
+
+type BlockNodeProps = {
+  titleConfig: {
+    asides?: {
+      after?: AsideType;
+      before?: AsideType;
+    };
+    badge?: BadgeType;
+    connections?: ConnectionType[];
+    label?: string;
+  };
+};
+
+function BlockNode({}: BlockNodeProps) {
+
+}
+
+export default BlockNode;
--- mage_ai/frontend/components/v2/Canvas/index.tsx
+++
@@ -1,6 +0,0 @@
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
-export function Canvas({ children }: { children?: React.ReactNode }) {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
-}
