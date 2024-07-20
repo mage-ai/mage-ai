2f403539f01cc7a7f16bf5ba06f8d2ef794741e7
mager <supreme@mage.ai>
2024-07-19 17:44:22 -0700

    build fe

--- scripts/frontend/export.sh
+++ scripts/frontend/export.sh
@@ -3,8 +3,9 @@
 # Get the current working directory
 CWD=$(pwd)/mage_ai/frontend
 
-# bash "$CWD/mage_ai/frontend/scripts/clean.sh" "$CWD"
+bash "$CWD/mage_ai/frontend/scripts/clean.sh" "$CWD"
 
 export NODE_ENV=production
 
-NODE_ENV=$NODE_ENV yarn --cwd "$CWD" export_prod
+NODE_ENV=$NODE_ENV yarn --cwd "$CWD" next build
+NODE_ENV=$NODE_ENV yarn --cwd "$CWD" next export -o ../server/frontend_dist
