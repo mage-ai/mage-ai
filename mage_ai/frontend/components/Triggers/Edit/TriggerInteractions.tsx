477b4d8f18225c80cecd6c622a7d9247459031bd
Johnson Kwok <johnson@mage.ai>
2024-09-06 11:45:17 -0700

    [jk] Fix typo for custom frequency in trigger review

--- mage_ai/frontend/components/Triggers/Edit/TriggerInteractions.tsx
+++ mage_ai/frontend/components/Triggers/Edit/TriggerInteractions.tsx
@@ -34,7 +34,7 @@ import { indexBy } from '@utils/array';
 import { shouldDisplayLocalTimezone } from '@components/settings/workspace/utils';
 import { useWindowSize } from '@utils/sizes';
 
-type TriggerInteractions = {
+type TriggerInteractionsType = {
   containerRef: any;
   date?: Date;
   interactions: InteractionType[];
@@ -77,7 +77,7 @@ function TriggerInteractions({
   time,
   triggerTypes,
   variables,
-}) {
+}: TriggerInteractionsType) {
   const interactionsMapping = useMemo(() => indexBy(interactions || [], ({ uuid }) => uuid), [
     interactions,
   ]);
@@ -97,7 +97,7 @@ function TriggerInteractions({
         uuid: blockUUID,
       } = block || {
         uuid: null,
-      }
+      };
 
       const blockInteractions = blockInteractionsMapping?.[blockUUID] || [];
       const hasBlockInteractions = blockInteractions?.length >= 1;
@@ -158,7 +158,7 @@ function TriggerInteractions({
         uuid: blockUUID,
       } = block || {
         uuid: null,
-      }
+      };
 
       const blockInteractions = blockInteractionsMapping?.[blockUUID] || [];
 
@@ -176,7 +176,7 @@ function TriggerInteractions({
           }: InteractionVariableType = variable;
 
           const value = variables?.[variableUUID];
-          const missingValue = typeof value === 'undefined'
+          const missingValue = typeof value === 'undefined';
           const invalid = required && missingValue;
           const Icon = INTERACTION_VARIABLE_VALUE_TYPE_ICON_MAPPING?.[types[0]] || Alphabet;
 
@@ -346,9 +346,12 @@ function TriggerInteractions({
                     monospace
                     muted={!pipelineSchedule?.schedule_interval}
                   >
-                    {pipelineSchedule?.schedule_interval && capitalizeRemoveUnderscoreLower(
-                      pipelineSchedule?.schedule_interval?.substring(1) || '',
-                    )}
+                    {pipelineSchedule?.schedule_interval
+                      && capitalizeRemoveUnderscoreLower(pipelineSchedule?.schedule_interval?.[0] === '@'
+                        ? (pipelineSchedule?.schedule_interval?.substring(1) || '')
+                        : (pipelineSchedule?.schedule_interval || ''),
+                      )
+                    }
                     {!pipelineSchedule?.schedule_interval && 'This is required'}
                   </Text>
                 </Spacing>,
