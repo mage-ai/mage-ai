17f590be0d5eecf98909cd149381a80e481e71d5
Sujith Kumar S <31705464+sujiplr@users.noreply.github.com>
2024-09-07 13:01:57 +0530

    Added the SSL handling for SASL
    Added the SSL handling for SASL

---
+++ mage_repo/transformers/avro_messages.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+
+
+@transformer
+def transform(messages: List[Dict], *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Args:
+        messages: List of messages in the stream.
+
+    Returns:
+        Transformed messages
+    """
+    # Specify your transformation logic here
+    for ms in messages:
+        print(ms)
+
+    return messages
