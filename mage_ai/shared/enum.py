499fb3de690598e345c67db4d38ec447c96123a5
Henry Wu <csharplus@gmail.com>
2024-10-12 19:05:53 -0700

    Revert IntEnum related changes

--- mage_ai/shared/enum.py
+++ mage_ai/shared/enum.py
@@ -1,8 +1,11 @@
 try:
     # breaking change introduced in python 3.11
-    from enum import Enum, IntEnum, StrEnum  # noqa: F401
+    from enum import Enum, IntEnum, StrEnum
 except ImportError:  # pragma: no cover
-    from enum import Enum, IntEnum  # noqa: F401 # pragma: no cover
+    from enum import Enum  # pragma: no cover
+
+    class IntEnum(int, Enum):  # pragma: no cover
+        pass  # pragma: no cover
 
     class StrEnum(str, Enum):  # pragma: no cover
         pass  # pragma: no cover
