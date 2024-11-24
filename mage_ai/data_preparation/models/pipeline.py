c0bb71901deb3bae41ee497290ed9ea105a59df9
Henry Wu <42320565+csharplus@users.noreply.github.com>
2024-11-23 22:54:15 -0800

    [hw] Correct the SSL settings in the nats configuration template (#5579)
    # Description
    
    Address the issue reported in
    https://github.com/mage-ai/mage-ai/issues/5556.
    
    # How Has This Been Tested?
    <!-- Please describe the tests that you ran to verify your changes.
    Provide instructions so we can reproduce.
    -->
    
    - [X] Checked the configuration settings against the fields in its class
    definition
    
    # Checklist
    - [ ] The PR is tagged with proper labels (bug, enhancement, feature,
    documentation)
    - [x] I have performed a self-review of my own code
    - [ ] I have added unit tests that prove my fix is effective or that my
    feature works
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    
    cc:
    <!-- Optionally mention someone to let them know about this pull request
    -->
    @wangxiaoyou1993

--- mage_ai/data_preparation/models/pipeline.py
+++ mage_ai/data_preparation/models/pipeline.py
@@ -2331,7 +2331,7 @@ class Pipeline:
     ):
         # Introduce a small delay to prevent multiple changes from generating
         # identical timestamps for the pipeline YAML file
-        time.sleep(0.0003)
+        time.sleep(0.0004)
 
         blocks_current = sorted([b.uuid for b in self.blocks_by_uuid.values()])
 
