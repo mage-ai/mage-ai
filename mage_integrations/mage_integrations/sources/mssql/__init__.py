ae7176a7fc7b9fa9cac4113e587b241a4ef7f6ce
sugimiyanto <sugimiyanto@puc.com>
2024-12-18 23:12:11 +0800

    Add MSSQL custom data integration type handling

--- mage_integrations/mage_integrations/sources/mssql/__init__.py
+++ mage_integrations/mage_integrations/sources/mssql/__init__.py
@@ -1,11 +1,25 @@
 from typing import List
 
 import dateutil
+from singer.schema import Schema
 
 from mage_integrations.connections.mssql import MSSQL as MSSQLConnection
 from mage_integrations.sources.base import main
-from mage_integrations.sources.constants import COLUMN_FORMAT_DATETIME
+from mage_integrations.sources.catalog import Catalog, CatalogEntry
+from mage_integrations.sources.constants import (
+    COLUMN_FORMAT_DATETIME,
+    COLUMN_TYPE_BOOLEAN,
+    COLUMN_TYPE_INTEGER,
+    COLUMN_TYPE_NULL,
+    COLUMN_TYPE_NUMBER,
+    COLUMN_TYPE_OBJECT,
+    COLUMN_TYPE_STRING,
+    REPLICATION_METHOD_FULL_TABLE,
+    UNIQUE_CONFLICT_METHOD_UPDATE,
+)
 from mage_integrations.sources.sql.base import Source
+from mage_integrations.sources.utils import get_standard_metadata
+from mage_integrations.utils.dictionary import group_by
 
 
 class MSSQL(Source):
@@ -58,6 +72,92 @@ WHERE  c.table_schema = '{schema}'
             query = f'{query}\nAND c.TABLE_NAME IN ({table_names})'
         return query
 
+    def discover(self, streams: List[str] = None) -> Catalog:
+        query = self.build_discover_query(streams=streams)
+
+        rows = self.build_connection().load(query)
+        groups = group_by(lambda t: t[0], rows)
+
+        streams = []
+        for stream_id, columns_data in groups.items():
+            properties = dict()
+            unique_constraints = []
+            valid_replication_keys = []
+
+            for column_data in columns_data:
+                """
+                Each column data has the following values
+                * TABLE_NAME
+                * COLUMN_DEFAULT
+                * COLUMN_KEY
+                * COLUMN_NAME
+                * DATA_TYPE
+                * IS_NULLABLE
+                """
+                column_key = self.__decode(column_data[2])
+                column_name = self.__decode(column_data[3])
+                column_type = self.__decode(column_data[4].lower())
+                is_nullable = self.__decode(column_data[5])
+
+                column_format = None
+                column_properties = None
+                column_types = []
+
+                if column_key is not None and ('PRI' in column_key or 'UNIQUE' == column_key):
+                    unique_constraints.append(column_name)
+
+                if 'YES' == is_nullable:
+                    column_types.append(COLUMN_TYPE_NULL)
+
+                if 'bool' in column_type or 'bit' in column_type:
+                    column_types.append(COLUMN_TYPE_BOOLEAN)
+                elif 'int' in column_type or 'bigint' in column_type:
+                    column_types.append(COLUMN_TYPE_INTEGER)
+                elif 'numeric' in column_type or 'decimal' in column_type or \
+                        'money' in column_type:
+                    column_types.append(COLUMN_TYPE_NUMBER)
+                elif 'datetime' in column_type or 'date' in column_type:
+                    column_format = COLUMN_FORMAT_DATETIME
+                    column_types.append(COLUMN_TYPE_STRING)
+                elif 'json' in column_type or 'variant' in column_type:
+                    column_properties = {}
+                    column_types.append(COLUMN_TYPE_OBJECT)
+                else:
+                    # binary, text, varchar, uniqueidentifier, etc.
+                    column_types.append(COLUMN_TYPE_STRING)
+
+                properties[column_name] = dict(
+                    properties=column_properties,
+                    format=column_format,
+                    type=column_types,
+                )
+
+            schema = Schema.from_dict(dict(
+                properties=properties,
+                type='object',
+            ))
+            metadata = get_standard_metadata(
+                key_properties=unique_constraints,
+                replication_method=REPLICATION_METHOD_FULL_TABLE,
+                schema=schema.to_dict(),
+                stream_id=stream_id,
+                valid_replication_keys=valid_replication_keys,
+            )
+            catalog_entry = CatalogEntry(
+                key_properties=unique_constraints,
+                metadata=metadata,
+                replication_method=REPLICATION_METHOD_FULL_TABLE,
+                schema=schema,
+                stream=stream_id,
+                tap_stream_id=stream_id,
+                unique_conflict_method=UNIQUE_CONFLICT_METHOD_UPDATE,
+                unique_constraints=unique_constraints,
+            )
+
+            streams.append(catalog_entry)
+
+        return Catalog(streams)
+
     def convert_datetime(self, val):
         if val is None:
             return val
@@ -82,6 +182,14 @@ WHERE  c.table_schema = '{schema}'
 
     def _limit_query_string(self, limit, offset):
         return f'OFFSET {offset} ROWS FETCH NEXT {limit} ROWS ONLY'
+
+    def __decode(self, bytes_or_str):
+        if type(bytes_or_str) is bytes:
+            try:
+                return bytes_or_str.decode('utf-8')
+            except Exception:
+                pass
+        return bytes_or_str
 
 
 if __name__ == '__main__':
