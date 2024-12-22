0f4c1c792194a8a732d2da0ebc0087f5221750c7
Henry Wu <csharplus@gmail.com>
2024-12-22 00:26:00 -0800

    Adjust the version of the oracledb module

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setuptools.setup(
         ],
         'oracle': [
             "oracledb==1.3.1; python_version < '3.12'",
-            "oracledb==1.3.1; python_version >= '3.12'",
+            "oracledb==2.0.0; python_version >= '3.12'",
         ],
         'postgres': [
             'psycopg2==2.9.3',
