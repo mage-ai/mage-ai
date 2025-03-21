68b10e68d5fa8eb31d9d12eece3050253e7ad139
yugasun <yugasun.ai@gmail.com>
2025-03-21 07:55:12 +0000

    feat: support doris source and destination

--- mage_ai/data_integrations/destinations/constants.py
+++ mage_ai/data_integrations/destinations/constants.py
@@ -5,6 +5,7 @@ DESTINATIONS = [
     dict(name='Clickhouse'),
     dict(name='Delta Lake Azure'),
     dict(name='Delta Lake S3'),
+    dict(name='Doris'),
     dict(name='Elasticsearch'),
     dict(name='Google Cloud Storage'),
     dict(name='Kafka'),
