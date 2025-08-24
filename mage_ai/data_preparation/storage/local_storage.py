32c09f67231debb9eee9f14c584e5216c660c8a1
wangxiaoyou1993 <xiaoyou@mage.ai>
2025-08-24 15:25:59 -0700

    [xy] Not set use_pyarrow=True as fallback for reading polars.

--- mage_ai/data_preparation/storage/local_storage.py
+++ mage_ai/data_preparation/storage/local_storage.py
@@ -121,7 +121,10 @@ class LocalStorage(BaseStorage):
         return pd.read_parquet(file_path, engine='pyarrow')
 
     def read_polars_parquet(self, file_path: str, **kwargs) -> pl.DataFrame:
-        return pl.read_parquet(file_path, use_pyarrow=True)
+        try:
+            return pl.read_parquet(file_path, use_pyarrow=True)
+        except Exception:
+            return pl.read_parquet(file_path)
 
     def write_csv(self, df: pd.DataFrame, file_path: str) -> None:
         File.create_parent_directories(file_path)
