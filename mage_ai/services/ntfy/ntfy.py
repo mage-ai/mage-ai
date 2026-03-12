b6a3bc20a6450b639345f55255754122246f8229
Daniel Beall <kdbeall@protonmail.com>
2026-03-12 13:12:01 -0700

    Update ntfy

--- mage_ai/services/ntfy/ntfy.py
+++ mage_ai/services/ntfy/ntfy.py
@@ -4,7 +4,7 @@ from mage_ai.services.ntfy.config import NtfyConfig
 
 
 def send_ntfy_message(config: NtfyConfig, message: str, title: str) -> None:
-    requests.post(config.url,
+    requests.post(config.webhook_url,
         data=message,
         headers={
             "Title": title,
