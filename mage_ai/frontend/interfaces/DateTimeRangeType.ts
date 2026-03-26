5be3eba509f9e9d64ed6eec162046ea23e615ca4
Chaz Biroan <chaz.biroan@ireulion.com>
2026-03-25 18:44:49 -0700

    [cb] Prettier & eslint conformance for DateTimeRangeType

--- mage_ai/frontend/interfaces/DateTimeRangeType.ts
+++ mage_ai/frontend/interfaces/DateTimeRangeType.ts
@@ -1,37 +1,37 @@
 export enum DateTimeRangeQueryEnum {
-    START = 'start_timestamp',
-    END = 'end_timestamp',
+  START = 'start_timestamp',
+  END = 'end_timestamp',
 }
 
 export enum DateTimeRangeEnum {
-    LAST_HOUR = 'Last hour',
-    LAST_DAY = 'Last day',
-    LAST_WEEK = 'Last week',
-    LAST_30_DAYS = 'Last 30 days',
-    CUSTOM_RANGE = 'Custom range',
+  LAST_HOUR = 'Last hour',
+  LAST_DAY = 'Last day',
+  LAST_WEEK = 'Last week',
+  LAST_30_DAYS = 'Last 30 days',
+  CUSTOM_RANGE = 'Custom range',
 }
 
 export const DATE_TIME_RANGES = [
-    DateTimeRangeEnum.LAST_HOUR,
-    DateTimeRangeEnum.LAST_DAY,
-    DateTimeRangeEnum.LAST_WEEK,
-    DateTimeRangeEnum.LAST_30_DAYS,
+  DateTimeRangeEnum.LAST_HOUR,
+  DateTimeRangeEnum.LAST_DAY,
+  DateTimeRangeEnum.LAST_WEEK,
+  DateTimeRangeEnum.LAST_30_DAYS,
 ];
 
 export const DATE_TIME_RANGE_SECOND_INTERVAL_MAPPING = {
-    [DateTimeRangeEnum.LAST_HOUR]: 3600,
-    [DateTimeRangeEnum.LAST_DAY]: 86400,
-    [DateTimeRangeEnum.LAST_WEEK]: 604800,
-    [DateTimeRangeEnum.LAST_30_DAYS]: 2592000,
+  [DateTimeRangeEnum.LAST_HOUR]: 3600,
+  [DateTimeRangeEnum.LAST_DAY]: 86400,
+  [DateTimeRangeEnum.LAST_WEEK]: 604800,
+  [DateTimeRangeEnum.LAST_30_DAYS]: 2592000,
 };
 
 export type DateTimeRangeTimeStamps = {
-    start_timestamp?: number;
-    end_timestamp?: number;
-}
+  start_timestamp?: number;
+  end_timestamp?: number;
+};
 
 export type DateTimeRangeProps = {
-    timestamps: DateTimeRangeTimeStamps,
-    selectedRange: DateTimeRangeEnum;
-    setSelectedRange: (range: DateTimeRangeEnum) => void;
+  timestamps: DateTimeRangeTimeStamps;
+  selectedRange: DateTimeRangeEnum;
+  setSelectedRange: (range: DateTimeRangeEnum) => void;
 };
