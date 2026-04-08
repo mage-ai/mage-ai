82139eb13ef8d6acd027b832572a84881044f856
Tuna.Sonmez <tuna.sonmez@b2metric.com>
2026-04-08 19:01:41 +0300

    fix: make api_key generic and read from .env

--- mage_ai/frontend/api/constants.ts
+++ mage_ai/frontend/api/constants.ts
@@ -1,5 +1,6 @@
-// Make sure this value is the same in mage_ai/settings/__init__.py
-export const OAUTH2_APPLICATION_CLIENT_ID: string = 'zkWlN0PkIKSN0C11CfUHUj84OT5XOJ6tDZ6bDRO2';
+// Make sure this value is the same in mage_ai/settings/server.py
+export const OAUTH2_APPLICATION_CLIENT_ID: string =
+  process.env.NEXT_PUBLIC_API_KEY || 'zkWlN0PkIKSN0C11CfUHUj84OT5XOJ6tDZ6bDRO2';
 
 export enum OperationTypeEnum {
   CREATE = 'create',
--- mage_ai/settings/server.py
+++ mage_ai/settings/server.py
@@ -52,7 +52,10 @@ secrets.token_urlsafe()
 
 Make sure this value is the same in mage_ai/frontend/api/constants.ts
 """
-OAUTH2_APPLICATION_CLIENT_ID = 'zkWlN0PkIKSN0C11CfUHUj84OT5XOJ6tDZ6bDRO2'
+OAUTH2_APPLICATION_CLIENT_ID = os.getenv(
+    'NEXT_PUBLIC_API_KEY',
+    'zkWlN0PkIKSN0C11CfUHUj84OT5XOJ6tDZ6bDRO2',
+)
 
 QUERY_API_KEY = 'api_key'
 
