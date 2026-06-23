365f6e94a63696665f5f2167ada8bc09ec3cc63a
wangxiaoyou1993 <xiaoyou@mage.ai>
2026-06-23 14:21:40 -0700

    Trigger backend CI path

--- mage_ai/server/constants.py
+++ mage_ai/server/constants.py
@@ -9,7 +9,7 @@ DATA_PREP_SERVER_PORT = os.getenv('DATA_PREP_SERVER_PORT', 6789)
 DATAFRAME_OUTPUT_SAMPLE_COUNT = 10
 
 # The last line of this file must be the version number.
-# Dockerfile depends on it because it runs ./scripts/install_mage.sh and uses
+# Dockerfiles depend on it because install steps use
 # the last line to determine the version to install.
 VERSION = \
 '0.9.79'
